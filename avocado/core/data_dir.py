885d688d1b85f4b4553d3659c7277e07d7931c23
Lucas Meneghel Rodrigues <lmr@redhat.com>
2014-03-05 20:48:51 -0300

    avocado.core.data_dir: Update main module attribute names

--- avocado/core/data_dir.py
+++ avocado/core/data_dir.py
@@ -8,28 +8,28 @@ import glob
 import shutil
 
 _ROOT_PATH = os.path.join(sys.modules[__name__].__file__, "..", "..", "..")
-ROOT_DIR = os.path.abspath(_ROOT_PATH)
-TMP_DIR = os.path.join(ROOT_DIR, 'tmp')
+DEFAULT_ROOT_DIR = os.path.abspath(_ROOT_PATH)
+DEFAULT_TMP_DIR = os.path.join(DEFAULT_ROOT_DIR, 'tmp')
 
 
 def get_root_dir():
-    return ROOT_DIR
+    return DEFAULT_ROOT_DIR
 
 
-def get_tmp_dir():
-    if not os.path.isdir(TMP_DIR):
-        os.makedirs(TMP_DIR)
-    return TMP_DIR
+def get_DEFAULT_TMP_DIR():
+    if not os.path.isdir(DEFAULT_TMP_DIR):
+        os.makedirs(DEFAULT_TMP_DIR)
+    return DEFAULT_TMP_DIR
 
 
 def clean_tmp_files():
-    if os.path.isdir(TMP_DIR):
-        hidden_paths = glob.glob(os.path.join(TMP_DIR, ".??*"))
-        paths = glob.glob(os.path.join(TMP_DIR, "*"))
+    if os.path.isdir(DEFAULT_TMP_DIR):
+        hidden_paths = glob.glob(os.path.join(DEFAULT_TMP_DIR, ".??*"))
+        paths = glob.glob(os.path.join(DEFAULT_TMP_DIR, "*"))
         for path in paths + hidden_paths:
             shutil.rmtree(path, ignore_errors=True)
 
 
 if __name__ == '__main__':
-    print "root dir:         " + ROOT_DIR
-    print "tmp dir:          " + TMP_DIR
+    print "root dir:         " + DEFAULT_ROOT_DIR
+    print "tmp dir:          " + DEFAULT_TMP_DIR
