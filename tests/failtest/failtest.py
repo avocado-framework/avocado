2e9fb12fb4b20e5d1bda84e1997b3cf52ffdf4f9
Lucas Meneghel Rodrigues <lmr@redhat.com>
2014-03-07 13:32:55 -0300

    tests: Adding failtest
    Signed-off-by: Lucas Meneghel Rodrigues <lmr@redhat.com>

---
+++ tests/failtest/failtest.py
@@ -0,0 +1,15 @@
+from avocado import test
+from avocado.core import exceptions
+
+
+class failtest(test.Test):
+
+    """
+    Functional test for avocado. Straight up fail the test
+    """
+
+    def action(self, length=1):
+        """
+        Sleep for length seconds.
+        """
+        raise exceptions.TestFail('This test is supposed to fail')
