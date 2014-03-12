4488cc8a5552b8294ccd68c12a32eceae1250de7
Lucas Meneghel Rodrigues <lmr@redhat.com>
2014-03-11 23:42:12 -0300

    tests: Add synctest
    synctest is a simple test wrapper for the synctest test
    suite. It's an example of suite compilation and execution.
    
    Signed-off-by: Lucas Meneghel Rodrigues <lmr@redhat.com>

---
+++ tests/synctest/synctest.py
@@ -0,0 +1,24 @@
+import os
+
+from avocado import test
+from avocado.utils import archive
+from avocado.utils import build
+from avocado.utils import process
+
+
+class synctest(test.Test):
+
+    """
+    Execute the synctest test suite.
+    """
+
+    def setup(self, tarball='synctest.tar.bz2'):
+        tarball_path = self.get_deps_path(tarball)
+        tarball = archive.extract(tarball_path, self.srcdir)
+        self.srcdir = os.path.join(self.srcdir, 'synctest')
+        build.make(self.srcdir)
+
+    def action(self, length=100, loop=10):
+        os.chdir(self.srcdir)
+        cmd = './synctest %s %s' % (length, loop)
+        process.system(cmd)
