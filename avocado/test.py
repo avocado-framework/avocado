7d1f52adce183e6f118e614ea645d3752d12ac18
Lucas Meneghel Rodrigues <lmr@redhat.com>
2014-03-25 17:01:07 -0300

    avocado.test: Remove the run_test() function
    As it is not necessary in avocado anymore.
    
    Signed-off-by: Lucas Meneghel Rodrigues <lmr@redhat.com>

--- avocado/test.py
+++ avocado/test.py
@@ -3,16 +3,13 @@ Contains the base test implementation, used as a base for the actual
 framework tests.
 """
 
-import imp
 import logging
 import os
 import sys
 import time
 import traceback
-from avocado import sysinfo
 from avocado.core import data_dir
 from avocado.core import exceptions
-from avocado.core import output
 from avocado.utils import process
 
 
@@ -198,67 +195,3 @@ class DropinTest(Test):
         except exceptions.CmdError, details:
             self._log_detailed_cmd_info(details.result)
             raise exceptions.TestFail(details)
-
-
-def run_tests(args):
-    test_start_time = time.strftime('%Y-%m-%d-%H.%M.%S')
-    logdir = args.logdir or data_dir.get_logs_dir()
-    debugbase = 'run-%s' % test_start_time
-    debugdir = os.path.join(logdir, debugbase)
-    latestdir = os.path.join(logdir, "latest")
-    if not os.path.isdir(debugdir):
-        os.makedirs(debugdir)
-    try:
-        os.unlink(latestdir)
-    except OSError:
-        pass
-    os.symlink(debugbase, latestdir)
-
-    debuglog = os.path.join(debugdir, "debug.log")
-    loglevel = args.log_level or logging.DEBUG
-
-    output_manager = output.OutputManager()
-
-    output_manager.start_file_logging(debuglog, loglevel)
-
-    urls = args.url.split()
-    total_tests = len(urls)
-    test_dir = data_dir.get_test_dir()
-    output_manager.log_header("DEBUG LOG: %s" % debuglog)
-    output_manager.log_header("TOTAL TESTS: %s" % total_tests)
-    output_mapping = {'PASS': output_manager.log_pass,
-                      'FAIL': output_manager.log_fail,
-                      'TEST_NA': output_manager.log_skip,
-                      'WARN': output_manager.log_warn}
-
-    test_index = 1
-
-    for url in urls:
-        path_attempt = os.path.abspath(url)
-        if os.path.exists(path_attempt):
-            test_class = DropinTest
-            test_instance = test_class(path=path_attempt, base_logdir=debugdir)
-        else:
-            test_module_dir = os.path.join(test_dir, url)
-            f, p, d = imp.find_module(url, [test_module_dir])
-            test_module = imp.load_module(url, f, p, d)
-            f.close()
-            test_class = getattr(test_module, url)
-            test_instance = test_class(name=url, base_logdir=debugdir)
-
-        sysinfo_logger = sysinfo.SysInfo(basedir=test_instance.sysinfodir)
-        test_instance.start_logging()
-        test_instance.setup()
-        sysinfo_logger.start_job_hook()
-        test_instance.run()
-        test_instance.cleanup()
-        test_instance.report()
-        test_instance.stop_logging()
-
-        output_func = output_mapping[test_instance.status]
-        label = "(%s/%s) %s:" % (test_index, total_tests,
-                                 test_instance.tagged_name)
-        output_func(label, test_instance.time_elapsed)
-        test_index += 1
-
-    output_manager.stop_file_logging()
