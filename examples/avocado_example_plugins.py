6400f95c5983d204f7f07356fe42f0b59797ad25
Lucas Meneghel Rodrigues <lmr@redhat.com>
2014-04-16 14:53:21 -0300

    avocado.plugins: Turn the lister into a core plugin
    The lister plugin is useful, as it allows people
    to see present, active/inactive plugins. So split
    it from the examples area.
    
    Signed-off-by: Lucas Meneghel Rodrigues <lmr@redhat.com>

--- examples/avocado_example_plugins.py
+++ examples/avocado_example_plugins.py
@@ -12,8 +12,6 @@
 # Author: Ruda Moura <rmoura@redhat.com>
 
 from avocado.plugins import plugin
-from avocado.plugins.manager import get_plugin_manager
-from avocado.core import output
 
 
 class HelloWorld(plugin.Plugin):
@@ -33,30 +31,3 @@ class HelloWorld(plugin.Plugin):
 
     def hello(self, args):
         print self.__doc__
-
-
-class PluginsList(plugin.Plugin):
-
-    """
-    List all plugins loaded.
-    """
-
-    name = 'plugins_list'
-    enabled = True
-
-    def configure(self, app_parser, cmd_parser):
-        myparser = cmd_parser.add_parser('plugins',
-                                         help='List all plugins loaded')
-        myparser.set_defaults(func=self.list_plugins)
-        self.configured = True
-
-    def list_plugins(self, args):
-        bcolors = output.colors
-        pipe = output.get_paginator()
-        pm = get_plugin_manager()
-        pipe.write(bcolors.header_str('Plugins loaded:'))
-        pipe.write('\n')
-        for plug in pm.plugins:
-            status = "Enabled" if plug.enabled else "Disabled"
-            pipe.write('    %s - %s (%s)\n' % (plug.name, plug.__doc__.strip(),
-                                               status))
