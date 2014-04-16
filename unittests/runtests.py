80f1db0e95dd1187177c59c41974cebc8ea7fe72
Lucas Meneghel Rodrigues <lmr@redhat.com>
2014-04-16 15:29:05 -0300

    unittests/runtests.py: Add missing license header
    Signed-off-by: Lucas Meneghel Rodrigues <lmr@redhat.com>

--- unittests/runtests.py
+++ unittests/runtests.py
@@ -1,6 +1,19 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
 
+# This program is free software; you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation; specifically version 2 of the License.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.
+#
+# See LICENSE for more details.
+#
+# Copyright: 2014 Red Hat
+# Author: Lucas Meneghel Rodrigues <lmr@redhat.com>
+
 __author__ = 'Lucas Meneghel Rodrigues <lmr@redhat.com>'
 
 from nose.selector import Selector
