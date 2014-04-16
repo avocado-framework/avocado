c6c17ee93decf15de3a98e8428d49e6a55a11d10
Ruda Moura <rmoura@redhat.com>
2014-04-16 17:01:02 -0300

    TestResult: add subclass HumanTestResult.
    Signed-off-by: Ruda Moura <rmoura@redhat.com>

--- avocado/result.py
+++ avocado/result.py
@@ -38,26 +38,15 @@ class TestResult(object):
 
     def start_tests(self):
         'Called once before any tests are executed.'
-        self.stream.start_file_logging(self.debuglog, self.loglevel)
-        self.stream.log_header("DEBUG LOG: %s" % self.debuglog)
-        self.stream.log_header("TOTAL TESTS: %s" % self.tests_total)
         self.tests_run += 1
 
     def end_tests(self):
         'Called once after all tests are executed.'
-        self.stream.log_header("TOTAL PASSED: %d" % len(self.passed))
-        self.stream.log_header("TOTAL ERROR: %d" % len(self.errors))
-        self.stream.log_header("TOTAL FAILED: %d" % len(self.failed))
-        self.stream.log_header("TOTAL SKIPPED: %d" % len(self.skipped))
-        self.stream.log_header("TOTAL WARNED: %d" % len(self.warned))
-        self.stream.log_header("ELAPSED TIME: %.2f s" % self.total_time)
-        self.stream.stop_file_logging()
+        pass
 
     def start_test(self, test):
         'Called when the given test is about to be run.'
-        self.test_label = '(%s/%s) %s: ' % (self.tests_run,
-                                            self.tests_total,
-                                            test.tagged_name)
+        pass
 
     def end_test(self, test):
         'Called when the given test has been run.'
@@ -66,27 +55,22 @@ class TestResult(object):
 
     def add_pass(self, test):
         'Called when a test succeed.'
-        self.stream.log_pass(self.test_label, test.time_elapsed)
         self.passed.append(test)
 
     def add_error(self, test):
         'Called when a test got error.'
-        self.stream.log_pass(self.test_label, test.time_elapsed)
         self.errors.append(test)
 
     def add_fail(self, test):
         'Called when a test fails.'
-        self.stream.log_fail(self.test_label, test.time_elapsed)
         self.failed.append(test)
 
     def add_skip(self, test):
         'Called when a test is skipped.'
-        self.stream.log_skip(self.test_label, test.time_elapsed)
         self.skipped.append(test)
 
     def add_warn(self, test):
         'Called when a test warns.'
-        self.stream.log_warn(self.test_label, test.time_elapsed)
         self.warned.append(test)
 
     def check_test(self, test):
@@ -100,3 +84,53 @@ class TestResult(object):
         add = status_map[test.status]
         add(test)
         self.end_test(test)
+
+
+class HumanTestResult(TestResult):
+
+    """
+    Human output Test result class.
+    """
+
+    def start_tests(self):
+        TestResult.start_tests(self)
+        self.stream.start_file_logging(self.debuglog, self.loglevel)
+        self.stream.log_header("DEBUG LOG: %s" % self.debuglog)
+        self.stream.log_header("TOTAL TESTS: %s" % self.tests_total)
+
+    def end_tests(self):
+        self.stream.log_header("TOTAL PASSED: %d" % len(self.passed))
+        self.stream.log_header("TOTAL ERROR: %d" % len(self.errors))
+        self.stream.log_header("TOTAL FAILED: %d" % len(self.failed))
+        self.stream.log_header("TOTAL SKIPPED: %d" % len(self.skipped))
+        self.stream.log_header("TOTAL WARNED: %d" % len(self.warned))
+        self.stream.log_header("ELAPSED TIME: %.2f s" % self.total_time)
+        self.stream.stop_file_logging()
+
+    def start_test(self, test):
+        self.test_label = '(%s/%s) %s: ' % (self.tests_run,
+                                            self.tests_total,
+                                            test.tagged_name)
+
+    def end_test(self, test):
+        TestResult.end_test(self, test)
+
+    def add_pass(self, test):
+        TestResult.add_pass(self, test)
+        self.stream.log_pass(self.test_label, test.time_elapsed)
+
+    def add_error(self, test):
+        TestResult.add_error(self, test)
+        self.stream.log_pass(self.test_label, test.time_elapsed)
+
+    def add_fail(self, test):
+        TestResult.add_fail(self, test)
+        self.stream.log_fail(self.test_label, test.time_elapsed)
+
+    def add_skip(self, test):
+        TestResult.add_skip(self, test)
+        self.stream.log_skip(self.test_label, test.time_elapsed)
+
+    def add_warn(self, test):
+        TestResult.add_warn(self, test)
+        self.stream.log_warn(self.test_label, test.time_elapsed)
