119e55eadae99ddf3abca775959ada59069e33bd
Lucas Meneghel Rodrigues <lmr@redhat.com>
2014-04-16 15:26:44 -0300

    avocado.plugins.xunit: Add missing license header
    Signed-off-by: Lucas Meneghel Rodrigues <lmr@redhat.com>

--- avocado/plugins/xunit.py
+++ avocado/plugins/xunit.py
@@ -1,3 +1,16 @@
+# This program is free software; you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation; specifically version 2 of the License.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.
+#
+# See LICENSE for more details.
+#
+# Copyright: 2014 Red Hat
+# Author: Ruda Moura <rmoura@redhat.com>
+
 from avocado.plugins import plugin
 from avocado.result import TestResult
 
