f9eebcfaff9f07481faad68b5347e73e7522b8a6
Lucas Meneghel Rodrigues <lmr@redhat.com>
2014-04-25 15:00:55 -0300

    avocado.plugins: Add core plugin 'datadir'
    Add a plugin that can list data directories used by
    avocado. This way, we can easily glance what is being
    used according to the settings available.
    
    Signed-off-by: Lucas Meneghel Rodrigues <lmr@redhat.com>

---
+++ avocado/plugins/datadir.py
@@ -0,0 +1,45 @@
+# This program is free software; you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation; either version 2 of the License, or
+# (at your option) any later version.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.
+#
+# See LICENSE for more details.
+#
+# Copyright: Red Hat Inc. 2013-2014
+# Author: Lucas Meneghel Rodrigues <lmr@redhat.com>
+
+from avocado.plugins import plugin
+from avocado.core import output
+from avocado.core import data_dir
+
+
+class DataDirList(plugin.Plugin):
+
+    """
+    Implements the avocado 'datadir' functionality.
+
+    The datadir functionality lists currently used directories used by avocado.
+    """
+
+    name = 'datadir'
+    enabled = True
+
+    def configure(self, app_parser, cmd_parser):
+        myparser = cmd_parser.add_parser('datadir',
+                                         help='List all relevant dirs used by avocado')
+        myparser.set_defaults(func=self.list_data_dirs)
+        self.configured = True
+
+    def list_data_dirs(self, args):
+        bcolors = output.colors
+        pipe = output.get_paginator()
+        pipe.write(bcolors.header_str('Avocado Data Directories:'))
+        pipe.write('\n    base dir:        ' + data_dir.get_base_dir())
+        pipe.write('\n    tests dir:       ' + data_dir.get_test_dir())
+        pipe.write('\n    data dir:        ' + data_dir.get_data_dir())
+        pipe.write('\n    logs dir:        ' + data_dir.get_logs_dir())
+        pipe.write('\n    tmp dir:         ' + data_dir.get_tmp_dir())
