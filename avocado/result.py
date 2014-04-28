29abb784873670441c4c697ba1f945542a4d94df
Lucas Meneghel Rodrigues <lmr@redhat.com>
2014-04-28 11:52:18 -0300

    avocado.result: Docstring fixes and improvements
    Signed-off-by: Lucas Meneghel Rodrigues <lmr@redhat.com>

--- avocado/result.py
+++ avocado/result.py
@@ -38,44 +38,80 @@ class TestResult(object):
         self.warned = []
 
     def start_tests(self):
-        'Called once before any tests are executed.'
+        """
+        Called once before any tests are executed.
+        """
         self.tests_run += 1
 
     def end_tests(self):
-        'Called once after all tests are executed.'
+        """
+        Called once after all tests are executed.
+        """
         pass
 
     def start_test(self, test):
-        'Called when the given test is about to be run.'
+        """
+        Called when the given test is about to run.
+
+        :param test: :class:`avocado.test.Test` instance.
+        """
         pass
 
     def end_test(self, test):
-        'Called when the given test has been run.'
+        """
+        Called when the given test has been run.
+
+        :param test: :class:`avocado.test.Test` instance.
+        """
         self.tests_run += 1
         self.total_time += test.time_elapsed
 
     def add_pass(self, test):
-        'Called when a test succeed.'
+        """
+        Called when a test succeeded.
+
+        :param test: :class:`avocado.test.Test` instance.
+        """
         self.passed.append(test)
 
     def add_error(self, test):
-        'Called when a test got error.'
+        """
+        Called when a test had a setup error.
+
+        :param test: :class:`avocado.test.Test` instance.
+        """
         self.errors.append(test)
 
     def add_fail(self, test):
-        'Called when a test fails.'
+        """
+        Called when a test fails.
+
+        :param test: :class:`avocado.test.Test` instance.
+        """
         self.failed.append(test)
 
     def add_skip(self, test):
-        'Called when a test is skipped.'
+        """
+        Called when a test is skipped.
+
+        :param test: :class:`avocado.test.Test` instance.
+        """
         self.skipped.append(test)
 
     def add_warn(self, test):
-        'Called when a test warns.'
+        """
+        Called when a test had a warning.
+
+        :param test: :class:`avocado.test.Test` instance.
+        """
         self.warned.append(test)
 
     def check_test(self, test):
-        'Called once for a test to check status and report.'
+        """
+        Called once for a test to check status and report.
+
+        :param test: :class:`avocado.test.Test` instance.
+        """
         self.start_test(test)
         status_map = {'PASS': self.add_pass,
                       'ERROR': self.add_error,
@@ -94,12 +130,18 @@ class HumanTestResult(TestResult):
     """
 
     def start_tests(self):
+        """
+        Called once before any tests are executed.
+        """
         TestResult.start_tests(self)
         self.stream.start_file_logging(self.debuglog, self.loglevel)
         self.stream.log_header("DEBUG LOG: %s" % self.debuglog)
         self.stream.log_header("TOTAL TESTS: %s" % self.tests_total)
 
     def end_tests(self):
+        """
+        Called once after all tests are executed.
+        """
         self.stream.log_header("TOTAL PASSED: %d" % len(self.passed))
         self.stream.log_header("TOTAL ERROR: %d" % len(self.errors))
         self.stream.log_header("TOTAL FAILED: %d" % len(self.failed))
@@ -109,29 +151,64 @@ class HumanTestResult(TestResult):
         self.stream.stop_file_logging()
 
     def start_test(self, test):
+        """
+        Called when the given test is about to run.
+
+        :param test: :class:`avocado.test.Test` instance.
+        """
         self.test_label = '(%s/%s) %s: ' % (self.tests_run,
                                             self.tests_total,
                                             test.tagged_name)
 
     def end_test(self, test):
+        """
+        Called when the given test has been run.
+
+        :param test: :class:`avocado.test.Test` instance.
+        """
         TestResult.end_test(self, test)
 
     def add_pass(self, test):
+        """
+        Called when a test succeeded.
+
+        :param test: :class:`avocado.test.Test` instance.
+        """
         TestResult.add_pass(self, test)
         self.stream.log_pass(self.test_label, test.time_elapsed)
 
     def add_error(self, test):
+        """
+        Called when a test had a setup error.
+
+        :param test: :class:`avocado.test.Test` instance.
+        """
         TestResult.add_error(self, test)
         self.stream.log_pass(self.test_label, test.time_elapsed)
 
     def add_fail(self, test):
+        """
+        Called when a test fails.
+
+        :param test: :class:`avocado.test.Test` instance.
+        """
         TestResult.add_fail(self, test)
         self.stream.log_fail(self.test_label, test.time_elapsed)
 
     def add_skip(self, test):
+        """
+        Called when a test is skipped.
+
+        :param test: :class:`avocado.test.Test` instance.
+        """
         TestResult.add_skip(self, test)
         self.stream.log_skip(self.test_label, test.time_elapsed)
 
     def add_warn(self, test):
+        """
+        Called when a test had a warning.
+
+        :param test: :class:`avocado.test.Test` instance.
+        """
         TestResult.add_warn(self, test)
         self.stream.log_warn(self.test_label, test.time_elapsed)
