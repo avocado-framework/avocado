666f78252e9162470882fe073e7ee2e8892a68c8
Lucas Meneghel Rodrigues <lmr@redhat.com>
2014-07-17 14:17:48 -0300

    avocado.virttest.data_dir: Remove bogus print

--- avocado/virttest/data_dir.py
+++ avocado/virttest/data_dir.py
@@ -11,7 +11,6 @@ import shutil
 
 _ROOT_PATH = os.path.join(sys.modules[__name__].__file__, '..', '..', '..')
 ROOT_DIR = os.path.join(os.path.abspath(_ROOT_PATH), 'tests', 'virt')
-print ROOT_DIR
 BASE_BACKEND_DIR = os.path.join(ROOT_DIR, 'backends')
 DATA_DIR = os.path.join(ROOT_DIR, 'shared', 'data')
 DEPS_DIR = os.path.join(ROOT_DIR, 'shared', 'deps')
