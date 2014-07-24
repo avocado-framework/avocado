00f438e55a807c878c0e3e0aa12eac8337f4c900
Cleber Rosa <crosa@redhat.com>
2014-07-24 20:47:59 -0300

    Core generator config: move the implementation to utils/misc.py
    Signed-off-by: Cleber Rosa <crosa@redhat.com>

--- avocado/utils/io.py
+++ avocado/utils/io.py
@@ -17,6 +17,8 @@ Avocado generic IO related functions.
 """
 
 import logging
+import resource
+
 
 log = logging.getLogger('avocado.test')
 
@@ -92,3 +94,67 @@ def write_one_line(filename, line):
     :type line: str
     """
     write_file(filename, line.rstrip('\n') + '\n')
+
+
+class CoreGeneratorConfig(object):
+
+    """
+    Configures the system for automatic `core` file generation
+    """
+
+    def __init__(self):
+        self.previous_core_soft_limit = None
+        self.previous_core_hard_limit = None
+        self.previous_core_pattern = None
+        self.enabled = None
+
+    def enable(self):
+        """
+        Runs all necessary actions to enable core files
+
+        Together with `disable()` this is this class' primary interface
+        """
+        (self.previous_core_soft_limit,
+         self.previous_core_hard_limit) = self.get_limits()
+
+        self.set_limits()
+
+    def disable(self):
+        """
+        Runs all necessary actions to disable core files
+
+        Together with `enable()` this is this class' primary interface
+        """
+        self.set_limits(self.previous_core_soft_limit,
+                        self.previous_core_hard_limit)
+
+        (new_soft_limit,
+         new_hard_limit) = self.get_limits()
+
+        assert self.previous_core_soft_limit == new_soft_limit
+        assert self.previous_core_hard_limit == new_hard_limit
+
+    def get_limits(self):
+        """
+        Get the current value for generated core files size limit
+
+        :returns: the current soft and hard limit, both as integers
+        :rtype: tuple
+        """
+        return resource.getrlimit(resource.RLIMIT_CORE)
+
+    def set_limits(self,
+                   soft=resource.RLIM_INFINITY,
+                   hard=resource.RLIM_INFINITY):
+        """
+        Configures the current user core file size limit
+        """
+        resource.setrlimit(resource.RLIMIT_CORE, (soft, hard))
+
+    def set_core_pattern(self):
+        """
+        Configures the `kernel.core_pattern` system tunable
+
+        So that core dumps are generated within a test data directory
+        """
+        pass
