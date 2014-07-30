a4b1251800037cdd97b2c283baa00a59cf02a733
Cleber Rosa <crosa@redhat.com>
2014-07-29 21:59:46 -0300

    gdbmi: replace gdbmi parser for the one from the pygdb project
    Even though pygdb seems to be dead as a project, the MI language
    parser is useful and much better than what I was cooking.
    
    Signed-off-by: Cleber Rosa <crosa@redhat.com>

--- avocado/gdbmi.py
+++ avocado/gdbmi.py
@@ -16,92 +16,21 @@
 Module that provides communication with GDB via its GDB/MI interpreter
 """
 
-# prefixed with a "^"
-RESULT_STATEMENTS = ["done",
-                     "running",
-                     "connected",
-                     "error",
-                     "exit"]
-
-# prefixed with a "*"
-ASYNC_STATE_STATEMENTS = ["running",
-                          "stopped"]
-
-# prefixed with a "="
-ASYNC_OTHER_STATEMENTS = ["thread-group-added",
-                          "thread-group-removed",
-                          "thread-group-exited",
-                          "thread-created",
-                          "thread-exited",
-                          "thread-selected",
-                          "library-loaded",
-                          "library-unloaded",
-                          "traceframe-changed",
-                          "tsv-created",
-                          "tsv-deleted",
-                          "tsv-modified",
-                          "breakpoint-created",
-                          "breakpoint-modified",
-                          "breakpoint-deleted",
-                          "record-started",
-                          "record-stopped",
-                          "cmd-param-changed",
-                          "memory-changed"]
-
-GDB_RECORD_KEYS = {'^': RESULT_STATEMENTS,
-                   '*': ASYNC_STATE_STATEMENTS,
-                   '=': ASYNC_OTHER_STATEMENTS,
-                   '~': 'console_output',
-                   '@': 'string_output',
-                   '&': 'debugging_messages'}
+from avocado.external import gdbmi_parser
 
 GDB_PROMPT = '(gdb)'
 GDB_EXIT = '^exit'
-
 GDB_BREAK_CONTITIONS = [GDB_PROMPT, GDB_EXIT]
 
-class UnknownKeyError(Exception):
-
-    """
-    Line contains a key that is unknown to the GDB/MI language
-    """
-    def __init__(self, key=None):
-        self.key = key
-
-    def __str__(self):
-        if self.key is not None:
-            msg = 'Key "%s" not recognized by GDB/MI parser' % self.key
-        else:
-            msg = 'Key not recognized by GDB/MI parser'
-        return msg
-
 def parse_line(line):
-    """
-    Parse a line, that is, a line that starts with
-    """
-    key = line[0]
-    if key not in GDB_RECORD_KEYS:
-        raise UnknownKeyError(key)
-
-    value = GDB_RECORD_KEYS[key]
-    if isinstance(value, list):
-        allowed_statements = value
-    else:
-        allowed_statements = []
-
-    line = line[1:]
-    statement, args = line.split(',', 1)
-    if allowed_statements:
-        if statement not in allowed_statements:
-            raise ValueError('Statement not recognized "%s"' % statement)
-
-    return statement, args
+    if not line.endswith('\n'):
+        line = "%s\n" % line
+    return gdbmi_parser.process(line)
 
 def encode_cli_command(command):
     """
     Returns a regular GDB command
 
-    :param command:
-
+    :param command: the regular cli command to send
     """
     return '-interpreter-exec console "%s"' % command
