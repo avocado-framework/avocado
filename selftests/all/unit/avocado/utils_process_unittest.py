5cea521be75f871181924a96bd9ab3181d3338e6
Cleber Rosa <crosa@redhat.com>
2014-08-07 11:24:29 -0300

    GDB: add transparent support to avocado/utils/process.py
    So that any code using our process API can be run through a debugger.
    
    Signed-off-by: Cleber Rosa <crosa@redhat.com>

---
+++ selftests/all/unit/avocado/utils_process_unittest.py
@@ -0,0 +1,60 @@
+#!/usr/bin/python
+
+import unittest
+
+from avocado import runtime
+from avocado.utils import process
+
+
+class TestGDBProcess(unittest.TestCase):
+
+    def setUp(self):
+        self.current_runtime_expr = runtime.GDB_RUN_BINARY_NAMES_EXPR[:]
+
+    def cleanUp(self):
+        runtime.GDB_RUN_BINARY_NAMES_EXPR = self.current_runtime_expr
+
+    def test_should_run_inside_gdb(self):
+        runtime.GDB_RUN_BINARY_NAMES_EXPR = ['foo']
+        self.assertTrue(process.should_run_inside_gdb('foo'))
+        self.assertTrue(process.should_run_inside_gdb('/usr/bin/foo'))
+        self.assertFalse(process.should_run_inside_gdb('/usr/bin/fooz'))
+
+        runtime.GDB_RUN_BINARY_NAMES_EXPR.append('foo:main')
+        self.assertTrue(process.should_run_inside_gdb('foo'))
+        self.assertFalse(process.should_run_inside_gdb('bar'))
+
+        runtime.GDB_RUN_BINARY_NAMES_EXPR.append('bar:main.c:5')
+        self.assertTrue(process.should_run_inside_gdb('bar'))
+        self.assertFalse(process.should_run_inside_gdb('baz'))
+        self.assertTrue(process.should_run_inside_gdb('bar 1 2 3'))
+        self.assertTrue(process.should_run_inside_gdb('/usr/bin/bar 1 2 3'))
+
+    def test_get_sub_process_klass(self):
+        runtime.GDB_RUN_BINARY_NAMES_EXPR = []
+        self.assertIs(process.get_sub_process_klass('/bin/true'),
+                      process.SubProcess)
+
+        runtime.GDB_RUN_BINARY_NAMES_EXPR.append('/bin/false')
+        self.assertIs(process.get_sub_process_klass('/bin/false'),
+                      process.GDBSubProcess)
+        self.assertIs(process.get_sub_process_klass('false'),
+                      process.GDBSubProcess)
+        self.assertIs(process.get_sub_process_klass('true'),
+                      process.SubProcess)
+
+    def test_split_gdb_expr(self):
+        binary, breakpoint = process.split_gdb_expr('foo:debug_print')
+        self.assertEqual(binary, 'foo')
+        self.assertEqual(breakpoint, 'debug_print')
+        binary, breakpoint = process.split_gdb_expr('bar')
+        self.assertEqual(binary, 'bar')
+        self.assertEqual(breakpoint, 'main')
+        binary, breakpoint = process.split_gdb_expr('baz:main.c:57')
+        self.assertEqual(binary, 'baz')
+        self.assertEqual(breakpoint, 'main.c:57')
+        self.assertIsInstance(process.split_gdb_expr('foo'), tuple)
+        self.assertIsInstance(process.split_gdb_expr('foo:debug_print'), tuple)
+
+if __name__ == "__main__":
+    unittest.main()
