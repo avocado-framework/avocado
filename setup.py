cb6033624a0e101f6298d67fd1044231ae6f8ee3
Lucas Meneghel Rodrigues <lmr@redhat.com>
2014-08-07 16:50:32 -0300

    setup.py: Fix setup script
    The tests dir layout has changed, update setup accordingly.
    
    Signed-off-by: Lucas Meneghel Rodrigues <lmr@redhat.com>

--- setup.py
+++ setup.py
@@ -31,35 +31,6 @@ setup(name='avocado',
                 'avocado.utils',
                 'avocado.plugins'],
       data_files=[('/etc/avocado', ['etc/settings.ini']),
-                  ('/usr/share/avocado/tests/doublefail', ['tests/doublefail/doublefail.py']),
-
-                  ('/usr/share/avocado/tests/doublefree', ['tests/doublefree/doublefree.py']),
-                  ('/usr/share/avocado/tests/doublefree/data', ['tests/doublefree/data/doublefree.c']),
-
-                  ('/usr/share/avocado/tests/errortest', ['tests/errortest/errortest.py']),
-                  ('/usr/share/avocado/tests/failtest', ['tests/failtest/failtest.py']),
-
-                  ('/usr/share/avocado/tests/fiotest', ['tests/fiotest/fiotest.py']),
-                  ('/usr/share/avocado/tests/fiotest/data', glob.glob('tests/fiotest/data/*')),
-
-                  ('/usr/share/avocado/tests/gendata', ['tests/gendata/gendata.py']),
-
-                  ('/usr/share/avocado/tests/linuxbuild', ['tests/linuxbuild/linuxbuild.py']),
-                  ('/usr/share/avocado/tests/linuxbuild/data', glob.glob('tests/linuxbuild/data/*')),
-
-                  ('/usr/share/avocado/tests/multiplextest', glob.glob('tests/multiplextest/*')),
-                  ('/usr/share/avocado/tests/skiptest', ['tests/skiptest/skiptest.py']),
-                  ('/usr/share/avocado/tests/sleeptenmin', ['tests/sleeptenmin/sleeptenmin.py']),
-                  ('/usr/share/avocado/tests/sleeptest', glob.glob('tests/sleeptest/*')),
-
-                  ('/usr/share/avocado/tests/synctest', glob.glob('tests/synctest/synctest.py')),
-                  ('/usr/share/avocado/tests/synctest/data', ['tests/synctest/data/synctest.tar.bz2']),
-
-                  ('/usr/share/avocado/tests/timeouttest', ['tests/timeouttest/timeouttest.py']),
-
-                  ('/usr/share/avocado/tests/trinity', ['tests/trinity/trinity.py']),
-                  ('/usr/share/avocado/tests/trinity/data', ['tests/trinity/data/trinity-1.4.tar.bz2']),
-
-                  ('/usr/share/avocado/tests/warntest', ['tests/warntest/warntest.py']),
-                  ('/usr/share/avocado/tests/whiteboard', glob.glob('tests/whiteboard/*'))],
+                  ('/usr/share/avocado/tests/',
+                   glob.glob('tests/*/*'))],
       scripts=['scripts/avocado'])
