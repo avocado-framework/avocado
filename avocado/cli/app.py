9989f8e7a1363bb8e6efaf7752f272b6ea667393
Ruda Moura <rmoura@redhat.com>
2014-08-25 17:33:37 -0300

    avocado.cli.parser: New way to parsing arguments.
    Modify how we parse command line arguments,
    by removing the logic that was inside avocado.cli.app into
    this new module.
    
    Signed-off-by: Ruda Moura <rmoura@redhat.com>

--- avocado/cli/app.py
+++ avocado/cli/app.py
@@ -17,12 +17,9 @@ The core Avocado application.
 """
 
 import os
-import sys
 
-from argparse import ArgumentParser
-
+from avocado.cli.parser import Parser
 from avocado.plugins.manager import get_plugin_manager
-from avocado.version import VERSION
 
 
 class AvocadoApp(object):
@@ -38,41 +35,12 @@ class AvocadoApp(object):
 
         self.external_plugins = external_plugins
         self.plugin_manager = None
-        self.app_parser = ArgumentParser(prog='avocado',
-                                         version=VERSION,
-                                         add_help=False,  # see parent parse
-                                         description='Avocado Test Runner')
-        self.app_parser.add_argument('-V', '--verbose', action='store_true',
-                                     help='print extra debug messages',
-                                     dest='verbose')
-        self.app_parser.add_argument('--logdir', action='store',
-                                     help='Alternate logs directory',
-                                     dest='logdir', default='')
-        self.app_parser.add_argument('--loglevel', action='store',
-                                     help='Debug Level',
-                                     dest='log_level', default='')
-        self.app_parser.add_argument('--plugins', action='store',
-                                     help='Load extra plugins from directory',
-                                     dest='plugins_dir', default='')
-        args, _ = self.app_parser.parse_known_args()
-
-        # Use parent parsing to avoid break output of --help option
-        self.app_parser = ArgumentParser(parents=[self.app_parser])
-
-        # Subparsers where Avocado subcommands are plugged
-        self.cmd_parser = self.app_parser.add_subparsers(
-            title='subcommands',
-            description='valid subcommands',
-            help='subcommand help')
-        self.load_plugin_manager(args.plugins_dir)
-
-        default_args = None
-        if not sys.argv[1:]:
-            default_args = ['--help']
-        args, _ = self.app_parser.parse_known_args(args=default_args)
-
-        self.plugin_manager.activate(args)
-        self.args = self.app_parser.parse_args()
+        self.parser = Parser()
+        self.parser.start()
+        self.load_plugin_manager(self.parser.args.plugins_dir)
+        self.parser.resume()
+        self.plugin_manager.activate(self.parser.args)
+        self.parser.finish()
 
     def load_plugin_manager(self, plugins_dir):
         """Load Plugin Manager.
@@ -83,7 +51,7 @@ class AvocadoApp(object):
         self.plugin_manager.load_plugins(plugins_dir)
         if self.external_plugins:
             self.plugin_manager.add_plugins(self.external_plugins)
-        self.plugin_manager.configure(self.app_parser, self.cmd_parser)
+        self.plugin_manager.configure(self.parser)
 
     def run(self):
-        return self.args.func(self.args)
+        return self.parser.take_action()
