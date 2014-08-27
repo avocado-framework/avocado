c9fd1a47337f2e00f0e80d2aa6428ce29e8b4f13
Lucas Meneghel Rodrigues <lmr@redhat.com>
2014-08-27 20:33:01 -0300

    avocado.utils: Split download and url related libraries
    To avoid a few oddities inside avocado code.
    
    Signed-off-by: Lucas Meneghel Rodrigues <lmr@redhat.com>

---
+++ avocado/utils/aurl.py
@@ -0,0 +1,32 @@
+# This program is free software; you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation; either version 2 of the License, or
+# (at your option) any later version.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.
+#
+# See LICENSE for more details.
+#
+# Copyright: Red Hat Inc. 2013-2014
+# Author: Lucas Meneghel Rodrigues <lmr@redhat.com>
+
+"""
+URL related functions.
+
+The strange name is to avoid accidental naming colisions in code.
+"""
+
+import urlparse
+
+
+def is_url(path):
+    """
+    Return `True` if path looks like an URL.
+
+    :param path: path to check.
+    :rtype: Boolean.
+    """
+    url_parts = urlparse.urlparse(path)
+    return (url_parts[0] in ('http', 'https', 'ftp', 'git'))
