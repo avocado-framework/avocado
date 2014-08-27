b9c6a8e6a91c4a02e166aa1aac13a7e821c1a8da
Lucas Meneghel Rodrigues <lmr@redhat.com>
2014-08-27 18:19:01 -0300

    avocado.utils: Introduce data_factory library
    We frequently need to generate data for framework or
    testing purposes. Introducing data_factory, a library
    dedicated to this purpose.
    
    With this, we can make other parts of avocado to use
    it and supersede old functions used on this regard.
    
    Signed-off-by: Lucas Meneghel Rodrigues <lmr@redhat.com>

---
+++ avocado/utils/data_factory.py
@@ -0,0 +1,84 @@
+# This program is free software; you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation; either version 2 of the License, or
+# (at your option) any later version.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.
+#
+# See LICENSE for more details.
+#
+# Copyright: Red Hat Inc. 2013-2014
+# Author: Lucas Meneghel Rodrigues <lmr@redhat.com>
+
+"""
+Generate data useful for the avocado framework and tests themselves.
+"""
+
+import logging
+import os
+import random
+import string
+import tempfile
+
+_RAND_POOL = random.SystemRandom()
+
+log = logging.getLogger('avocado.test')
+
+
+def generate_random_string(length, ignore_str=string.punctuation,
+                           convert_str=""):
+    """
+    Generate a random string using alphanumeric characters.
+
+    :param length: Length of the string that will be generated.
+    :param ignore_str: Characters that will not include in generated string.
+    :param convert_str: Characters that need to be escaped (prepend "\\").
+
+    :return: The generated random string.
+    """
+    result = ""
+    chars = string.letters + string.digits + string.punctuation
+    if not ignore_str:
+        ignore_str = ""
+    for i in ignore_str:
+        chars = chars.replace(i, "")
+
+    while length > 0:
+        tmp = _RAND_POOL.choice(chars)
+        if convert_str and (tmp in convert_str):
+            tmp = "\\%s" % tmp
+        result += tmp
+        length -= 1
+    return result
+
+
+def make_dir_and_populate(self, basedir='/tmp'):
+    """
+    Create a directory in basedir and populate with a number of files.
+
+    The files just have random text contents.
+
+    :param basedir: Base directory where directory should be generated.
+    :type basedir: str
+    :return: Path of the dir created and populated.
+    :rtype: str
+    """
+    try:
+        path = tempfile.mkdtemp(prefix='trinity-victims',
+                                dir=basedir)
+        n_files = _RAND_POOL.randint(100, 150)
+        for _ in xrange(n_files):
+            fd, _ = tempfile.mkstemp(dir=self.victims_path, text=True)
+            str_length = _RAND_POOL.randint(30, 50)
+            n_lines = _RAND_POOL.randint(5, 7)
+            for _ in xrange(n_lines):
+                os.write(fd, generate_random_string(str_length))
+            os.close(fd)
+    except OSError, details:
+        log.error("Failed to generate dir in '%s' and populate: %s" %
+                  (basedir, details))
+        return None
+
+    return path
