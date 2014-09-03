44371c24687925ce472775a21918659a7a206de6
Lucas Meneghel Rodrigues <lmr@redhat.com>
2014-09-03 01:38:09 -0300

    avocado: Sanitize namespace
    It is not necessary to internally import the libraries like
    we've been doing so far.
    
    Signed-off-by: Lucas Meneghel Rodrigues <lmr@redhat.com>

--- avocado/__init__.py
+++ avocado/__init__.py
@@ -12,16 +12,6 @@
 # Copyright: Red Hat Inc. 2013-2014
 # Author: Lucas Meneghel Rodrigues <lmr@redhat.com>
 
-import aexpect
-import cli
-import core
-
-import job
-import test
-import version
-import linux
-
-import logging.config
 
 DEFAULT_LOGGING = {
     'version': 1,
@@ -59,5 +49,5 @@ DEFAULT_LOGGING = {
     }
 }
 
-
-logging.config.dictConfig(DEFAULT_LOGGING)
+from logging import config
+config.dictConfig(DEFAULT_LOGGING)
