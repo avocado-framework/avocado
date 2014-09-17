699f04df7a9c636c6385b50a989ac4f653ec86a6
Rudá Moura <rmoura@redhat.com>
2014-09-17 12:36:38 -0300

    multiplexer: Reintroduce command line options for filters.
    Reintroduce command line options --filter-only and --filter-out
    to control the global filters for multiplexing, both by the test runner
    and the multiplex plugin.
    
    Group multiplex command line options together in the group
    "multiplex" inside the test runner.
    
    Examples:
    
    avocado run --help
    ... other options ...
    multiplex arguments:
      -m MULTIPLEX_FILE, --multiplex-file MULTIPLEX_FILE
                            Path to an avocado multiplex (.yaml) file
      --filter-only [FILTER_ONLY [FILTER_ONLY ...]]
                            Filter only path(s) from multiplexing
      --filter-out [FILTER_OUT [FILTER_OUT ...]]
                            Filter out path(s) from multiplexing
    
    avocado run footest --multiplex foo.yaml \
    	--filter-only /root/env /root/os/linux \
            --filter-out /root/hw/virtio
    
    avocado multiplex mux.yaml \
    	--filter-only /root/os/win /root/hw/cpu/intel \
            --filter-out /root/hw/disk/virtio
    Variants generated:
    Variant 1:    /root/hw/cpu/intel, /root/hw/disk/scsi, /root/os/win/winxp, /root/env/debug
    Variant 2:    /root/hw/cpu/intel, /root/hw/disk/scsi, /root/os/win/winxp, /root/env/prod
    Variant 3:    /root/hw/cpu/intel, /root/hw/disk/scsi, /root/os/win/win7, /root/env/debug
    Variant 4:    /root/hw/cpu/intel, /root/hw/disk/scsi, /root/os/win/win7, /root/env/prod
    Variant 5:    /root/hw/cpu/intel, /root/hw/disk/scsi, /root/os/win/win8, /root/env/debug
    Variant 6:    /root/hw/cpu/intel, /root/hw/disk/scsi, /root/os/win/win8, /root/env/prod
    
    Signed-off-by: Ruda Moura <rmoura@redhat.com>

--- avocado/job.py
+++ avocado/job.py
@@ -430,7 +430,9 @@ class Job(object):
             if urls is not None:
                 for url in urls:
                     try:
-                        variants = multiplexer.create_variants_from_yaml(open(multiplex_file))
+                        variants = multiplexer.create_variants_from_yaml(open(multiplex_file),
+                                                                         self.args.filter_only,
+                                                                         self.args.filter_out)
                     except SyntaxError:
                         variants = None
                     if variants:
