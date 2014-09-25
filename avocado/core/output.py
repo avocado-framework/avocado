75412a0de8418f10c2474cab149d23f3e6029e34
Lucas Meneghel Rodrigues <lmr@redhat.com>
2014-09-25 17:19:48 -0300

    avocado.core.output: Add LogFile object
    Add a new object that can properly stub the stdout
    and stderr streams for test subprocess executions,
    delivering the messages to our stdout and stderr
    loggers instead of directly to sys.stdout and/or
    sys.stderr.
    
    Signed-off-by: Lucas Meneghel Rodrigues <lmr@redhat.com>

--- avocado/core/output.py
+++ avocado/core/output.py
@@ -230,6 +230,68 @@ class TermSupport(object):
 term_support = TermSupport()
 
 
+class LoggingFile(object):
+
+    """
+    File-like object that will receive messages pass them to logging.
+    """
+
+    def __init__(self, prefix='', level=logging.DEBUG,
+                 logger=logging.getLogger()):
+        """
+        Constructor. Sets prefixes and which logger is going to be used.
+
+        :param prefix - The prefix for each line logged by this object.
+        """
+
+        self._prefix = prefix
+        self._level = level
+        self._buffer = []
+        self._logger = logger
+
+    def write(self, data):
+        """"
+        Writes data only if it constitutes a whole line. If it's not the case,
+        store it in a buffer and wait until we have a complete line.
+        :param data - Raw data (a string) that will be processed.
+        """
+        # splitlines() discards a trailing blank line, so use split() instead
+        data_lines = data.split('\n')
+        if len(data_lines) > 1:
+            self._buffer.append(data_lines[0])
+            self._flush_buffer()
+        for line in data_lines[1:-1]:
+            self._log_line(line)
+        if data_lines[-1]:
+            self._buffer.append(data_lines[-1])
+
+    def writelines(self, lines):
+        """"
+        Writes itertable of lines
+
+        :param lines: An iterable of strings that will be processed.
+        """
+        for data in lines:
+            self.write(data)
+
+    def _log_line(self, line):
+        """
+        Passes lines of output to the logging module.
+        """
+        self._logger.log(self._level, self._prefix + line)
+
+    def _flush_buffer(self):
+        if self._buffer:
+            self._log_line(''.join(self._buffer))
+            self._buffer = []
+
+    def flush(self):
+        self._flush_buffer()
+
+    def isatty(self):
+        return False
+
+
 class View(object):
 
     """
