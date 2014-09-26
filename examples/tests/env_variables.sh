53c8b1ea4a85a7b1d45bcab40ee8dac0c4ce0e4a
Lucas Meneghel Rodrigues <lmr@redhat.com>
2014-09-26 12:27:06 -0300

    examples/tests: Add env_variables.sh
    Add an example of how avocado exposes information to
    shell scripts through environment variables.
    
    Signed-off-by: Lucas Meneghel Rodrigues <lmr@redhat.com>

---
+++ examples/tests/env_variables.sh
@@ -0,0 +1,20 @@
+#!/bin/sh
+# This test demonstrates that shell scripts have access to a lot off
+# avocado runtime information, exported through environment variables.
+echo "Avocado Version: $AVOCADO_VERSION"
+echo "Avocado Test basedir: $AVOCADO_TEST_BASEDIR"
+echo "Avocado Test datadir: $AVOCADO_TEST_DATADIR"
+echo "Avocado Test workdir: $AVOCADO_TEST_WORKDIR"
+echo "Avocado Test srcdir: $AVOCADO_TEST_SRCDIR"
+echo "Avocado Test logdir: $AVOCADO_TEST_LOGDIR"
+echo "Avocado Test logfile: $AVOCADO_TEST_LOGFILE"
+echo "Avocado Test outputdir: $AVOCADO_TEST_OUTPUTDIR"
+echo "Avocado Test sysinfodir: $AVOCADO_TEST_SYSINFODIR"
+
+test -d "$AVOCADO_TEST_BASEDIR" -a \
+     -d "$AVOCADO_TEST_WORKDIR" -a \
+     -d "$AVOCADO_TEST_SRCDIR" -a \
+     -d "$AVOCADO_TEST_LOGDIR" -a \
+     -f "$AVOCADO_TEST_LOGFILE" -a \
+     -d "$AVOCADO_TEST_OUTPUTDIR" -a \
+     -d "$AVOCADO_TEST_SYSINFODIR"
