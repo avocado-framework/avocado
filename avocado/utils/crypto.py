c6468ad8799f9ec6cbafb01f5eac8f03f1c7d85b
Cleber Rosa <crosa@redhat.com>
2014-09-29 00:46:42 -0300

    avocado/utils/crypto.py: remove get_random_string function
    The exactly same function body is present under avocado/data_factory.py,
    under the name generate_random_string.
    
    Signed-off-by: Cleber Rosa <crosa@redhat.com>

--- avocado/utils/crypto.py
+++ avocado/utils/crypto.py
@@ -12,8 +12,6 @@
 # Copyright: Red Hat Inc. 2013-2014
 # Author: Lucas Meneghel Rodrigues <lmr@redhat.com>
 
-import random
-import string
 import os
 import logging
 try:
@@ -21,34 +19,6 @@ try:
 except ImportError:
     import md5
     import sha
-
-
-def get_random_string(length, ignore_str=string.punctuation,
-                      convert_str=""):
-    """
-    Return a random string using alphanumeric characters.
-
-    :param length: Length of the string that will be generated.
-    :param ignore_str: Characters that will not include in generated string.
-    :param convert_str: Characters that need to be escaped (prepend "\\").
-
-    :return: The generated random string.
-    """
-    r = random.SystemRandom()
-    result = ""
-    chars = string.letters + string.digits + string.punctuation
-    if not ignore_str:
-        ignore_str = ""
-    for i in ignore_str:
-        chars = chars.replace(i, "")
-
-    while length > 0:
-        tmp = r.choice(chars)
-        if convert_str and (tmp in convert_str):
-            tmp = "\\%s" % tmp
-        result += tmp
-        length -= 1
-    return result
 
 
 def hash_wrapper(algorithm='md5', data=None):
