5d5b7bc6febe094cec653c38d15ae542718075e3
Rudá Moura <rmoura@redhat.com>
2014-11-18 14:25:53 -0200

    Introduce new runner option --wrapper.
    Introduce a new command line option (--wrapper) to wrap
    every new process created with help of module `avocado.utils.process`
    inside a support script.
    
    Wrappers can be global, so it will wrap every process or
    you can set a wrapper to a specific program.
    
    * Set wrapper to ltrace.sh on each process:
        avocado run --wrapper ltrace.sh sometest
    * Set wrapper to ltrace.sh and binary myprog:
        avocado run --wrapper ltrace.sh:myprog sometest
    
    You can provide multiples --wrapper options,
    but only one global wrapper can be active at time.
    
    Signed-off-by: Rudá Moura <rmoura@redhat.com>

---
+++ avocado/plugins/wrapper.py
@@ -0,0 +1,58 @@
+# This program is free software; you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation; either version 2 of the License, or
+# (at your option) any later version.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.
+#
+# See LICENSE for more details.
+#
+# Copyright: Red Hat Inc. 2014
+# Author: Ruda Moura <rmoura@redhat.com>
+
+import os
+import sys
+
+from avocado import runtime
+from avocado.core import error_codes
+from avocado.core import output
+from avocado.plugins import plugin
+
+
+class Wrapper(plugin.Plugin):
+
+    name = 'wrapper'
+    enabled = True
+
+    def configure(self, parser):
+        self.parser = parser
+        wrap_group = self.parser.runner.add_argument_group(
+            'Wrap avocado.utils.process module')
+        wrap_group.add_argument('--wrapper', action='append', default=[],
+                                help='')
+        self.configured = True
+
+    def activate(self, app_args):
+        view = output.View(app_args=app_args)
+        try:
+            for wrap in app_args.wrapper:
+                if ':' not in wrap:
+                    if runtime.WRAP_PROCESS is None:
+                        script = os.path.abspath(wrap)
+                        runtime.WRAP_PROCESS = os.path.abspath(script)
+                    else:
+                        view.notify(event='error',
+                                    msg="You can't have multiple global wrappers at once.")
+                        sys.exit(error_codes.numeric_status['AVOCADO_CRASH'])
+                else:
+                    script, cmd = wrap.split(':', 1)
+                    script = os.path.abspath(script)
+                    runtime.WRAP_PROCESS_NAMES_EXPR.append((script, cmd))
+                if not os.path.exists(script):
+                    view.notify(event='error',
+                                msg="Wrapper '%s' not found!" % script)
+                    sys.exit(error_codes.numeric_status['AVOCADO_CRASH'])
+        except AttributeError:
+            pass
