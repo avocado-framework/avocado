6fa0bfd874c9f928d2c78f23df63f92144a14e70
Rudá Moura <rmoura@redhat.com>
2014-11-26 18:51:33 -0200

    Remove license text from the files that are not essential.
    Remove the license text from files inside docs, examples and selftest,
    to keep the license text only in relevant parts of the project.
    
    References:
    
    * https://fedoraproject.org/wiki/Packaging:LicensingGuidelines
    * https://www.gnu.org/licenses/gpl-howto.html
    
    Signed-off-by: Rudá Moura <rmoura@redhat.com>

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -1,16 +1,3 @@
-# This program is free software; you can redistribute it and/or modify
-# it under the terms of the GNU General Public License as published by
-# the Free Software Foundation; specifically version 2 of the License.
-#
-# This program is distributed in the hope that it will be useful,
-# but WITHOUT ANY WARRANTY; without even the implied warranty of
-# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.
-#
-# See LICENSE for more details.
-#
-# Copyright: Red Hat Inc. 2013-2014
-# Author: Lucas Meneghel Rodrigues <lmr@redhat.com>
-
 # -*- coding: utf-8 -*-
 
 import sys
--- examples/tests/abort.py
+++ examples/tests/abort.py
@@ -1,19 +1,5 @@
 #!/usr/bin/python
 
-# This program is free software; you can redistribute it and/or modify
-# it under the terms of the GNU General Public License as published by
-# the Free Software Foundation; either version 2 of the License, or
-# (at your option) any later version.
-#
-# This program is distributed in the hope that it will be useful,
-# but WITHOUT ANY WARRANTY; without even the implied warranty of
-# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.
-#
-# See LICENSE for more details.
-#
-# Copyright: Red Hat Inc. 2014
-# Author: Ruda Moura <rmoura@redhat.com>
-
 import os
 
 from avocado import test
