5d96e0c97b6c668070ed8231825f78d61ed86c2b
Lukáš Doktor <ldoktor@redhat.com>
2014-12-16 18:51:47 +0100

    avocado.core.tree: Docstrings, pylint/pep8 fixes
    Signed-off-by: Lukáš Doktor <ldoktor@redhat.com>

--- avocado/core/tree.py
+++ avocado/core/tree.py
@@ -46,6 +46,10 @@ except ImportError:
 
 class TreeNode(object):
 
+    """
+    Class for bounding nodes into tree-structure.
+    """
+
     def __init__(self, name='', value=None, parent=None, children=None):
         if value is None:
             value = {}
@@ -67,9 +71,11 @@ class TreeNode(object):
         return '%s: %s' % (self.path, ', '.join(variables))
 
     def __len__(self):
+        """ Return number of descended leaf nodes """
         return len(tuple(self.iter_leaves()))
 
     def __iter__(self):
+        """ Iterate through descended leaf nodes """
         return self.iter_leaves()
 
     def __eq__(self, other):
@@ -84,6 +90,10 @@ class TreeNode(object):
             return True
 
     def add_child(self, node):
+        """
+        Append node as child. Nodes with the same name gets merged into the
+        existing position.
+        """
         if isinstance(node, self.__class__):
             if node.name in self.children:
                 self.children[self.children.index(node.name)].merge(node)
@@ -94,26 +104,36 @@ class TreeNode(object):
             raise ValueError('Bad node type.')
 
     def merge(self, other):
-        """ Merges $other node into this one (doesn't check the name) """
+        """
+        Merges `other` node into this one without checking the name of the
+        other node. New values are appended, existing values overwritten
+        and unaffected ones are kept. Then all other node children are
+        added as children (recursively they get either appended at the end
+        or merged into existing node in the previous position.
+        """
         self.value.update(other.value)
         for child in other.children:
             self.add_child(child)
 
     @property
     def is_leaf(self):
+        """ Is this a leaf node? """
         return not self.children
 
     @property
     def root(self):
+        """ Root of this tree """
         return self.get_root()
 
     def get_root(self):
+        """ Get root of this tree """
         root = None
         for root in self.iter_parents():
             pass
         return root
 
     def iter_parents(self):
+        """ Iterate through parent nodes to root """
         node = self.parent
         while True:
             if node is None:
@@ -123,16 +143,20 @@ class TreeNode(object):
 
     @property
     def parents(self):
+        """ List of parent nodes """
         return self.get_parents()
 
     def get_parents(self):
+        """ Get list of parent nodes """
         return list(self.iter_parents())
 
     @property
     def path(self):
+        """ Node path """
         return self.get_path()
 
     def get_path(self, sep='/'):
+        """ Get node path """
         path = [str(self.name)]
         for node in self.iter_parents():
             path.append(str(node.name))
@@ -140,9 +164,11 @@ class TreeNode(object):
 
     @property
     def environment(self):
+        """ Node environment (values + preceding envs) """
         return self.get_environment()
 
     def get_environment(self):
+        """ Get node environment (values + preceding envs) """
         if self._environment is None:
             self._environment = (self.parent.environment.copy()
                                  if self.parent else {})
@@ -158,6 +184,11 @@ class TreeNode(object):
         return self._environment
 
     def set_environment_dirty(self):
+        """
+        Set the environment cache dirty. You should call this always when
+        you query for the environment and then change the value or structure.
+        Otherwise you'll get the old environment instead.
+        """
         for child in self.children:
             child.set_environment_dirty()
         self._environment = None
@@ -175,55 +206,72 @@ class TreeNode(object):
                 node = None
 
     def iter_leaves(self):
+        """ Iterate throuh leaf nodes """
         for node in self.iter_children_preorder():
             if node.is_leaf:
                 yield node
 
     def get_leaves(self):
+        """ Get list of leaf nodes """
         return list(self.iter_leaves())
 
     def get_ascii(self, show_internal=True, compact=False, attributes=None):
-        (lines, _) = self._ascii_art(show_internal=show_internal,
-                                     compact=compact, attributes=attributes)
+        """
+        Get ascii-art tree structure
+        :param show_internal: Show intermediary nodes
+        :param compact: Compress the tree vertically
+        :param attributes: List of node attributes to be printed out ['name']
+        :return: string
+        """
+        (lines, _) = self.ascii_art(show_internal=show_internal,
+                                    compact=compact, attributes=attributes)
         return '\n' + '\n'.join(lines)
 
-    def _ascii_art(self, char1='-', show_internal=True, compact=False,
-                   attributes=None):
+    def ascii_art(self, char1='-', show_internal=True, compact=False,
+                  attributes=None):
+        """
+        Generate ascii-art for this node
+        :param char1: Incomming path character [-]
+        :param show_internal: Show intermediary nodes
+        :param compact: Compress the tree vertically
+        :param attributes: List of node attributes to be printed out ['name']
+        :return: list of strings
+        """
         if attributes is None:
             attributes = ["name"]
         node_name = ', '.join(map(str, [getattr(self, v)
                                         for v in attributes
                                         if hasattr(self, v)]))
 
-        LEN = max(3, len(node_name)
-                  if not self.children or show_internal else 3)
-        PAD = ' ' * LEN
-        PA = ' ' * (LEN - 1)
+        length = max(3, len(node_name)
+                     if not self.children or show_internal else 3)
+        pad = ' ' * length
+        _pad = ' ' * (length - 1)
         if not self.is_leaf:
             mids = []
             result = []
-            for c in self.children:
+            for char in self.children:
                 if len(self.children) == 1:
                     char2 = '/'
-                elif c is self.children[0]:
+                elif char is self.children[0]:
                     char2 = '/'
-                elif c is self.children[-1]:
+                elif char is self.children[-1]:
                     char2 = '\\'
                 else:
                     char2 = '-'
-                (clines, mid) = c._ascii_art(char2, show_internal, compact,
-                                             attributes)
+                (clines, mid) = char.ascii_art(char2, show_internal, compact,
+                                               attributes)
                 mids.append(mid + len(result))
                 result.extend(clines)
                 if not compact:
                     result.append('')
             if not compact:
                 result.pop()
-            (lo, hi, end) = (mids[0], mids[-1], len(result))
-            prefixes = ([PAD] * (lo + 1) + [PA + '|'] * (hi - lo - 1)
-                        + [PAD] * (end - hi))
-            mid = (lo + hi) / 2
-            prefixes[mid] = char1 + '-' * (LEN - 2) + prefixes[mid][-1]
+            (low, high, end) = (mids[0], mids[-1], len(result))
+            prefixes = ([pad] * (low + 1) + [_pad + '|'] * (high - low - 1)
+                        + [pad] * (end - high))
+            mid = (low + high) / 2
+            prefixes[mid] = char1 + '-' * (length - 2) + prefixes[mid][-1]
             result = [p + l for (p, l) in zip(prefixes, result)]
             if show_internal:
                 stem = result[mid]
@@ -233,6 +281,7 @@ class TreeNode(object):
             return [char1 + '-' + node_name], 0
 
     def detach(self):
+        """ Detach this node from parent """
         if self.parent:
             self.parent.children.remove(self)
             self.parent = None
@@ -247,7 +296,7 @@ def _create_from_yaml(stream):
         pass
 
     def tree_node_from_values(name, values):
-        """ Create $name node and add values  """
+        """ Create `name` node and add values  """
         node_children = []
         node_values = []
         for value in values:
