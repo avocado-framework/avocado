32bce730f32b05524363ebd43489c358b588e03f
Cleber Rosa <crosa@redhat.com>
2015-01-15 14:32:44 -0300

    Distro plugin: add representation for packages and distro definitions
    A distro definition is an expansion of the distro class that includes
    packages that are shipped with the distro.
    
    Changes from v0:
     * Fixed typos on commit message
    
    Signed-off-by: Cleber Rosa <crosa@redhat.com>

--- avocado/plugins/distro.py
+++ avocado/plugins/distro.py
@@ -12,9 +12,85 @@
 # Copyright: Red Hat Inc. 2015
 # Author: Cleber Rosa <cleber@redhat.com>
 
+import bz2
+import json
+
 from avocado.core import output
 from avocado.plugins import plugin
 from avocado.linux import distro as distro_utils
+
+
+class SoftwarePackage(object):
+
+    '''
+    Definition of relevant information on a software package
+    '''
+
+    def __init__(self, name, version, release, checksum, arch):
+        self.name = name
+        self.version = version
+        self.release = release
+        self.checksum = checksum
+        self.arch = arch
+
+    def to_dict(self):
+        '''
+        Returns the representation as a dictionary
+        '''
+        return {'name': self.name,
+                'version': self.version,
+                'release': self.release,
+                'checksum': self.checksum,
+                'arch': self.arch}
+
+    def to_json(self):
+        '''
+        Returns the representation of the distro as JSON
+        '''
+        return json.dumps(self.to_dict())
+
+
+class DistroDef(distro_utils.LinuxDistro):
+
+    '''
+    More complete information on a given Linux Distribution
+
+    Can and should include all the software packages that ship with the distro,
+    so that an analysis can be made on whether a given package that may be
+    responsible for a regression is part of the official set or an external
+    package.
+    '''
+
+    def __init__(self, name, version, release, arch):
+        super(DistroDef, self).__init__(name, version, release, arch)
+
+        #: All the software packages that ship with this Linux distro
+        self.software_packages = []
+
+        #: A simple text that denotes the software type that makes this distro
+        self.software_packages_type = 'unknown'
+
+    def to_dict(self):
+        '''
+        Returns the representation as a dictionary
+        '''
+        d = {'name': self.name,
+             'version': self.version,
+             'release': self.release,
+             'arch': self.arch,
+             'software_packages_type': self.software_packages_type,
+             'software_packages': []}
+
+        for package in self.software_packages:
+            d['software_packages'].append(package.to_dict())
+
+        return d
+
+    def to_json(self):
+        '''
+        Returns the representation of the distro as JSON
+        '''
+        return json.dumps(self.to_dict())
 
 
 class DistroOptions(plugin.Plugin):
