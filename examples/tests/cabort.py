abd20f032d3b64c84b697b33598a86a91f79ec57
Rudá Moura <rmoura@redhat.com>
2015-02-23 15:43:49 -0300

    examples: New test cabort.py.
    Add new test `cabort.py` to call abort() from lib C.
    
    Signed-off-by: Rudá Moura <rmoura@redhat.com>

---
+++ examples/tests/cabort.py
@@ -0,0 +1,44 @@
+#!/usr/bin/python
+
+import os
+import shutil
+
+from avocado import test
+from avocado import job
+from avocado.utils import build
+from avocado.utils import process
+
+
+class CAbort(test.Test):
+
+    """
+    A test that calls C standard lib function abort().
+    """
+
+    default_params = {'source': 'abort.c'}
+
+    def setup(self):
+        """
+        Build 'abort'.
+        """
+        c_file = self.get_data_path(self.params.source)
+        c_file_name = os.path.basename(c_file)
+        dest_c_file = os.path.join(self.srcdir, c_file_name)
+        shutil.copy(c_file, dest_c_file)
+        build.make(self.srcdir,
+                   env={'CFLAGS': '-g -O0'},
+                   extra_args='abort')
+
+    def action(self):
+        """
+        Execute 'abort'.
+        """
+        cmd = os.path.join(self.srcdir, 'abort')
+        cmd_result = process.run(cmd, ignore_status=True)
+        self.log.info(cmd_result)
+        expected_result = -6  # SIGABRT = 6
+        self.assertEqual(cmd_result.exit_status, expected_result)
+
+
+if __name__ == "__main__":
+    job.main()
