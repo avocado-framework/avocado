bc210382e59956701fe500f7d447df2f7b6a5d6f
Lukáš Doktor <ldoktor@redhat.com>
2015-04-09 14:32:44 +0200

    avocado.plugins.multiplexer: Cmdline arg to show attributes
    This patch adds support to set "attributes" of the tree multiplexer view
    on the command line.
    
    Signed-off-by: Lukáš Doktor <ldoktor@redhat.com>

--- avocado/plugins/multiplexer.py
+++ avocado/plugins/multiplexer.py
@@ -49,7 +49,9 @@ class Multiplexer(plugin.Plugin):
 
         self.parser.add_argument('-t', '--tree', action='store_true', default=False,
                                  help='Shows the multiplex tree structure')
-
+        self.parser.add_argument('--attributes', nargs='*', default=None,
+                                 help="Which attributes to show when using "
+                                 "--tree (default is 'name')")
         self.parser.add_argument('-c', '--contents', action='store_true', default=False,
                                  help="Shows the variant content (variables)")
         self.parser.add_argument('-d', '--debug', action='store_true',
@@ -64,7 +66,8 @@ class Multiplexer(plugin.Plugin):
             view.notify(event='message', msg='Config file tree structure:')
             t = tree.create_from_yaml(multiplex_files)
             t = tree.apply_filters(t, args.filter_only, args.filter_out)
-            view.notify(event='minor', msg=t.get_ascii())
+            attr = args.attributes
+            view.notify(event='minor', msg=t.get_ascii(attributes=attr))
             sys.exit(exit_codes.AVOCADO_ALL_OK)
 
         try:
