3f0b16dfb111486aa18526996fb58c3127b387f2
Lucas Meneghel Rodrigues <lmr@redhat.com>
2015-04-10 10:55:48 -0300

    selftests/functional/gdb_tests.py: Use --job-results-dir
    Signed-off-by: Lucas Meneghel Rodrigues <lmr@redhat.com>

--- selftests/all/functional/avocado/gdb_tests.py
+++ selftests/all/functional/avocado/gdb_tests.py
@@ -1,6 +1,8 @@
 import os
 import sys
 import unittest
+import shutil
+import tempfile
 
 # simple magic for using scripts within a source tree
 basedir = os.path.join(os.path.dirname(os.path.abspath(__file__)), '..', '..', '..', '..')
@@ -13,16 +15,23 @@ from avocado.utils import process
 
 class GDBPluginTest(unittest.TestCase):
 
+    def setUp(self):
+        self.tmpdir = tempfile.mkdtemp()
+
     def test_gdb_prerun_commands(self):
         os.chdir(basedir)
-        cmd_line = './scripts/avocado run --sysinfo=off --gdb-prerun-commands=/dev/null passtest'
+        cmd_line = ('./scripts/avocado run --job-results-dir %s --sysinfo=off '
+                    '--gdb-prerun-commands=/dev/null passtest' % self.tmpdir)
         process.run(cmd_line)
 
     def test_gdb_multiple_prerun_commands(self):
         os.chdir(basedir)
-        cmd_line = ('./scripts/avocado run --sysinfo=off --gdb-prerun-commands=/dev/null '
-                    '--gdb-prerun-commands=foo:/dev/null passtest')
+        cmd_line = ('./scripts/avocado run --job-results-dir %s --sysinfo=off --gdb-prerun-commands=/dev/null '
+                    '--gdb-prerun-commands=foo:/dev/null passtest' % self.tmpdir)
         process.run(cmd_line)
+
+    def tearDown(self):
+        shutil.rmtree(self.tmpdir)
 
 if __name__ == '__main__':
     unittest.main()
