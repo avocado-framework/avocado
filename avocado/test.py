avocado-framework/avocado

commit 133b60e8dbae94eef5d9a69411bec6a565e90d45

avocado.test: Set datadir based upon filename.
Set the datadir for test based upon the filename of the class,
so that multiple test methods in a same test class will share
the same datadir directory.

Signed-off-by: Rudá Moura <rmoura@redhat.com>

avocado/test.py
@@ -94,8 +94,9 @@ class Test(unittest.TestCase):
 
         tmpdir = data_dir.get_tmp_dir()
 
-        self.basedir = os.path.dirname(inspect.getfile(self.__class__))
-        self.datadir = os.path.join(self.basedir, '%s.data' % basename)
+        self.filename = inspect.getfile(self.__class__).rstrip('co')
+        self.basedir = os.path.dirname(self.filename)
+        self.datadir = utils_path.init_dir(self.filename + '.data')
 
         self.expected_stdout_file = os.path.join(self.datadir,
                                                  'stdout.expected')

commit 9c159b0bff831e58af6e76476d25731ff4b4753d

avocado.test: Don't force creation of the test datadir
A test may or may not have a data dir. Also, a test might
be on a read only location, so forcing the creation of
one will raise an OSError. This fixes a problem introduced
with 133b60e8dbae94eef5d9a69411bec6a565e90d45.

Signed-off-by: Lucas Meneghel Rodrigues <lmr@redhat.com>

avocado/test.py
@@ -91,7 +91,7 @@ class Test(unittest.TestCase):
 
         self.filename = inspect.getfile(self.__class__).rstrip('co')
         self.basedir = os.path.dirname(self.filename)
-        self.datadir = utils_path.init_dir(self.filename + '.data')
+        self.datadir = self.filename + '.data'
 
         self.expected_stdout_file = os.path.join(self.datadir,
                                                  'stdout.expected')