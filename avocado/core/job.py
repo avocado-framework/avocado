avocado-framework/avocado

commit cb751ea5e238cbfab735144173d56a1cc48e5ab5

avocado: Avoid loading plugin classes in the code based on hardcoded arg names
For a while we have been using code similar to this:

def load_plugins(self, args):
    for key in args.__dict__.keys():
        if key.endswith('_loader'):

One could argue that the sufix check is not quite elegant
or correct, so we can remove that and get straight to the
business of checking the actual loading class we're interested
in. This PR changes all the occurrences of that practice inside
the avocado code.

Changes from v2:
 * Use iteritems() instead of .keys(), as suggested by ldoktor.

Signed-off-by: Lucas Meneghel Rodrigues <lmr@redhat.com>

avocado/core/job.py
@@ -149,12 +149,13 @@ class Job(object):
         shutil.rmtree(self.logdir, ignore_errors=True)
 
     def _make_test_loader(self):
-        for key in self.args.__dict__.keys():
-            if key.endswith('_loader'):
-                loader_class = getattr(self.args, key)
-                if issubclass(loader_class, loader.TestLoader):
-                    loader_plugin = loader_class(self)
+        for loader_class_candidate in self.args.__dict__.itervalues():
+            try:
+                if issubclass(loader_class_candidate, loader.TestLoader):
+                    loader_plugin = loader_class_candidate(self)
                     self.test_loader.add_loader_plugin(loader_plugin)
+            except TypeError:
+                pass
         filesystem_loader = loader.TestLoader(self)
         self.test_loader.add_loader_plugin(filesystem_loader)
 
@@ -168,13 +169,14 @@ class Job(object):
                                              test_result=self.result_proxy)
 
     def _set_output_plugins(self):
-        for key in self.args.__dict__:
-            if key.endswith('_result'):
-                result_class = getattr(self.args, key)
-                if issubclass(result_class, result.TestResult):
-                    result_plugin = result_class(self.view,
-                                                 self.args)
+        for result_class_candidate in self.args.__dict__.itervalues():
+            try:
+                if issubclass(result_class_candidate, result.TestResult):
+                    result_plugin = result_class_candidate(self.view,
+                                                           self.args)
                     self.result_proxy.add_output_plugin(result_plugin)
+            except TypeError:
+                pass
 
     def _make_test_result(self):
         """

avocado/core/loader.py
@@ -66,12 +66,13 @@ class TestLoaderProxy(object):
         self.loader_plugins.append(plugin)
 
     def load_plugins(self, args):
-        for key in args.__dict__.keys():
-            if key.endswith('_loader'):
-                loader_class = getattr(args, key)
-                if issubclass(loader_class, TestLoader):
-                    loader_plugin = loader_class(args=args)
+        for loader_class_candidate in args.__dict__.itervalues():
+            try:
+                if issubclass(loader_class_candidate, TestLoader):
+                    loader_plugin = loader_class_candidate(args=args)
                     self.add_loader_plugin(loader_plugin)
+            except TypeError:
+                pass
         filesystem_loader = TestLoader()
         self.add_loader_plugin(filesystem_loader)
 

commit 03bf9c85c8f0930d3aaee395b2095a8992e9ed5c

avocado: Fix use of itervalues() in args dict
This fixes a bug introduced by cb751ea5e238cbfab735144173d56a1cc48e5ab5.
It turns out that .itervalues() is a bad idea, given that the
args dictionary is modified by plugins, leading to a bug when
we have multiple test source plugins enabled at once.

I'm sorry I didn't notice this before the commit was merged.

Signed-off-by: Lucas Meneghel Rodrigues <lmr@redhat.com>

avocado/core/job.py
@@ -148,7 +148,8 @@ class Job(object):
         shutil.rmtree(self.logdir, ignore_errors=True)
 
     def _make_test_loader(self):
-        for loader_class_candidate in self.args.__dict__.itervalues():
+        for key in self.args.__dict__.keys():
+            loader_class_candidate = getattr(self.args, key)
             try:
                 if issubclass(loader_class_candidate, loader.TestLoader):
                     loader_plugin = loader_class_candidate(self)
@@ -168,7 +169,8 @@ class Job(object):
                                              test_result=self.result_proxy)
 
     def _set_output_plugins(self):
-        for result_class_candidate in self.args.__dict__.itervalues():
+        for key in self.args.__dict__.keys():
+            result_class_candidate = getattr(self.args, key)
             try:
                 if issubclass(result_class_candidate, result.TestResult):
                     result_plugin = result_class_candidate(self.view,

avocado/core/loader.py
@@ -66,7 +66,8 @@ class TestLoaderProxy(object):
         self.loader_plugins.append(plugin)
 
     def load_plugins(self, args):
-        for loader_class_candidate in args.__dict__.itervalues():
+        for key in args.__dict__.keys():
+            loader_class_candidate = getattr(args, key)
             try:
                 if issubclass(loader_class_candidate, TestLoader):
                     loader_plugin = loader_class_candidate(args=args)