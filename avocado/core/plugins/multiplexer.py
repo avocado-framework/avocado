6e8c5c7de35fc6d8dc97c1c13913af74d73ac59d
Lukáš Doktor <ldoktor@redhat.com>
2015-06-09 19:41:34 +0200

    avocado.core.plugins.multiplexer: Support for --tree-view
    Modify the --tree view to look like linux "tree" command and add support
    to display content, inherited values or booth.
    
    Signed-off-by: Lukáš Doktor <ldoktor@redhat.com>

--- avocado/core/plugins/multiplexer.py
+++ avocado/core/plugins/multiplexer.py
@@ -19,6 +19,7 @@ from .. import exit_codes
 from .. import output
 from .. import tree
 from . import plugin
+from ..settings import settings
 
 
 class Multiplexer(plugin.Plugin):
@@ -52,22 +53,23 @@ class Multiplexer(plugin.Plugin):
         self.parser.add_argument('-s', '--system-wide', action='store_true',
                                  help="Combine the files with the default "
                                  "tree.")
-
-        self.parser.add_argument('-t', '--tree', action='store_true', default=False,
-                                 help='Shows the multiplex tree structure')
-        self.parser.add_argument('--attr', nargs='*', default=[],
-                                 help="Which attributes to show when using "
-                                 "--tree (default is 'name')")
-        self.parser.add_argument('-c', '--contents', action='store_true', default=False,
-                                 help="Shows the variant content (variables)")
-        self.parser.add_argument('-d', '--debug', action='store_true',
-                                 default=False, help="Debug multiplexed "
-                                 "files.")
+        self.parser.add_argument('-c', '--contents', action='store_true',
+                                 default=False, help="Shows the node content "
+                                 "(variables)")
         self.parser.add_argument('--env', default=[], nargs='*',
                                  help="Inject [path:]key:node values into "
                                  "the final multiplex tree.")
+        env_parser = self.parser.add_argument_group("environment view options")
+        env_parser.add_argument('-d', '--debug', action='store_true',
+                                default=False, help="Debug multiplexed "
+                                "files.")
+        tree_parser = self.parser.add_argument_group("tree view options")
+        tree_parser.add_argument('-t', '--tree', action='store_true',
+                                 default=False, help='Shows the multiplex '
+                                 'tree structure')
+        tree_parser.add_argument('-i', '--inherit', action="store_true",
+                                 help="Show the inherited values")
         super(Multiplexer, self).configure(self.parser)
-        self._from_args_tree = tree.TreeNode()
 
     def activate(self, args):
         # Extend default multiplex tree of --env values
@@ -84,6 +86,15 @@ class Multiplexer(plugin.Plugin):
 
     def run(self, args):
         view = output.View(app_args=args)
+        err = None
+        if args.tree and args.debug:
+            err = "Option --tree is incompatible with --debug."
+        elif not args.tree and args.inherit:
+            err = "Option --inherit can be only used with --tree"
+        if err:
+            view.notify(event="minor", msg=self.parser.format_help())
+            view.notify(event="error", msg=err)
+            sys.exit(exit_codes.AVOCADO_FAIL)
         try:
             mux_tree = multiplexer.yaml2tree(args.multiplex_files,
                                              args.filter_only, args.filter_out,
@@ -96,9 +107,16 @@ class Multiplexer(plugin.Plugin):
             mux_tree.merge(args.default_multiplex_tree)
         mux_tree.merge(self._from_args_tree)
         if args.tree:
-            view.notify(event='message', msg='Config file tree structure:')
-            view.notify(event='minor',
-                        msg=mux_tree.get_ascii(attributes=args.attr))
+            if args.contents:
+                verbose = 1
+            else:
+                verbose = 0
+            if args.inherit:
+                verbose += 2
+            use_utf8 = settings.get_value("runner.output", "utf8",
+                                          key_type=bool, default=None)
+            view.notify(event='minor', msg=tree.tree_view(mux_tree, verbose,
+                                                          use_utf8))
             sys.exit(exit_codes.AVOCADO_ALL_OK)
 
         variants = multiplexer.MuxTree(mux_tree)
