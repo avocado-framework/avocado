5b4d6461b8bfe5bf2f692e0078e0db5bfe5e3bfb
Lukáš Doktor <ldoktor@redhat.com>
2015-06-11 16:19:57 +0200

    plugins.multiplexer: Improve the environment view
    The current implementation iterates through the avaliable key/values
    and displays only the last value per unique key. This patch uses
    the value origin and displays the "origin:key => value" for all
    unique available records per variant.
    
    Signed-off-by: Lukáš Doktor <ldoktor@redhat.com>

--- selftests/all/functional/avocado/multiplex_tests.py
+++ selftests/all/functional/avocado/multiplex_tests.py
@@ -20,12 +20,13 @@ if os.path.isdir(os.path.join(basedir, 'avocado')):
 from avocado.utils import process
 
 DEBUG_OUT = """Variant 16:    amd@examples/mux-environment.yaml, virtio@examples/mux-environment.yaml, mint@examples/mux-environment.yaml, debug@examples/mux-environment.yaml
-    corruptlist: nonlist@examples/mux-selftest.yaml:/hw/disk
-    cpu_CFLAGS: -march=athlon64@examples/mux-environment.yaml:/hw/cpu/amd
-    disk_type: virtio@examples/mux-environment.yaml:/hw/disk/virtio
-    init: systemv@examples/mux-environment.yaml:/distro/mint
-    joinlist: ['first_item']@examples/mux-selftest.yaml:/hw/cpu + ['second', 'third']@examples/mux-selftest.yaml:/hw/cpu/amd
-    opt_CFLAGS: -O0 -g@examples/mux-environment.yaml:/env/debug
+    /distro/mint:init         => systemv@examples/mux-environment.yaml:/distro/mint
+    /env/debug:opt_CFLAGS     => -O0 -g@examples/mux-environment.yaml:/env/debug
+    /hw/cpu/amd:cpu_CFLAGS    => -march=athlon64@examples/mux-environment.yaml:/hw/cpu/amd
+    /hw/cpu/amd:joinlist      => ['first_item']@examples/mux-selftest.yaml:/hw/cpu + ['second', 'third']@examples/mux-selftest.yaml:/hw/cpu/amd
+    /hw/disk/virtio:disk_type => virtio@examples/mux-environment.yaml:/hw/disk/virtio
+    /hw/disk:corruptlist      => nonlist@examples/mux-selftest.yaml:/hw/disk
+    /hw:corruptlist           => ['upper_node_list']@examples/mux-selftest.yaml:/hw
 """
 
 
