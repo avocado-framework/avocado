69c7d0b5c600d5efa58537c5c0c9edccf4668aa9
Lucas Meneghel Rodrigues <lmr@redhat.com>
2015-08-12 19:03:33 -0300

    Fix unhelpful message when calling process/GDB APIs
    The process and gdb related APIs trust on subprocess.Popen
    to start a process. When shell=False and the command being
    executed is missing, Subprocss.Popen will raise an OSError
    with errno 2, with no additional message about what file
    is actually missing.
    
    OSError: [Errno 2] No such file or directory
    
    As you might have guessed, my workaround is to capture
    OSErrors with errno 2, and re-raise them with a more
    helpful message.
    
    'File /usr/bin/gdbserver not found'
    
    Signed-off-by: Lucas Meneghel Rodrigues <lmr@redhat.com>

--- avocado/utils/gdb.py
+++ avocado/utils/gdb.py
@@ -339,11 +339,19 @@ class GDB(object):
         args += self.REQUIRED_ARGS
         args += extra_args
 
-        self.process = subprocess.Popen(args,
-                                        stdin=subprocess.PIPE,
-                                        stdout=subprocess.PIPE,
-                                        stderr=subprocess.PIPE,
-                                        close_fds=True)
+        try:
+            self.process = subprocess.Popen(args,
+                                            stdin=subprocess.PIPE,
+                                            stdout=subprocess.PIPE,
+                                            stderr=subprocess.PIPE,
+                                            close_fds=True)
+        except OSError, details:
+            if details.errno == 2:
+                exc = OSError("File '%s' not found" % args[0])
+                exc.errno = 2
+                raise exc
+            else:
+                raise
 
         fcntl.fcntl(self.process.stdout.fileno(),
                     fcntl.F_SETFL, os.O_NONBLOCK)
@@ -649,11 +657,19 @@ class GDBServer(object):
         _, self.stderr_path = tempfile.mkstemp(prefix=prefix + 'stderr_')
         self.stderr = open(self.stderr_path, 'w')
 
-        self.process = subprocess.Popen(args,
-                                        stdin=subprocess.PIPE,
-                                        stdout=self.stdout,
-                                        stderr=self.stderr,
-                                        close_fds=True)
+        try:
+            self.process = subprocess.Popen(args,
+                                            stdin=subprocess.PIPE,
+                                            stdout=self.stdout,
+                                            stderr=self.stderr,
+                                            close_fds=True)
+        except OSError, details:
+            if details.errno == 2:
+                exc = OSError("File '%s' not found" % args[0])
+                exc.errno = 2
+                raise exc
+            else:
+                raise
 
         if wait_until_running:
             self._wait_until_running()
