33f50c6857681a0278264d0dbeb6af168e9adbc0
Cleber Rosa <crosa@redhat.com>
2015-10-06 17:07:32 -0300

    Test Loader: add the concept of test tags
    Inside test classes there may docstrings which may contain tags that
    give special meaning. The tags are defined by ":avocado: <value>" in a
    single line. For now the only two "official" Avocado tags values
    exist:
    
     * enable: the class is an Avocado INSTRUMENTED test, even if it
       doesn't "look like" one.
     * disable: the class is *NOT* an Avocado INSTRUMENTED test, even if
       looks like one.
    
    Signed-off-by: Cleber Rosa <crosa@redhat.com>

--- avocado/core/loader.py
+++ avocado/core/loader.py
@@ -46,6 +46,28 @@ AVAILABLE = None    # Available tests (for listing purposes)
 ALL = True          # All tests (inicluding broken ones)
 
 
+#: Gets the tag value from a string. Used to tag a test class in various ways
+AVOCADO_DOCSTRING_TAG_RE = re.compile(r'\s*:avocado:\s*(\S+)\s*')
+
+
+def get_docstring_tag(docstring):
+    if docstring is None:
+        return None
+    result = AVOCADO_DOCSTRING_TAG_RE.search(docstring)
+    if result is not None:
+        return result.groups()[0]
+
+
+def is_docstring_tag_enable(docstring):
+    result = get_docstring_tag(docstring)
+    return result == 'enable'
+
+
+def is_docstring_tag_disable(docstring):
+    result = get_docstring_tag(docstring)
+    return result == 'disable'
+
+
 class LoaderError(Exception):
 
     """ Loader exception """
