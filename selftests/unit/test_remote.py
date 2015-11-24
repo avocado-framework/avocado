b1dd79b18d5f581a914f00e74e1aab4aedb03068
Lucas Meneghel Rodrigues <lookkas@gmail.com>
2015-11-24 02:52:37 -0200

    avocado remote plugin: Add support to identity files
    Add support for identity files as valid SSH options
    to our remote plugin. The use case for this is connecting
    to Amazon EC2 instances using the provided keypair (.pem
    files).
    
    avocado run --remote-hostname my-machine.com --remote-username myuser --remote-key-file /path/to/myfile.pem passtest
    
    Signed-off-by: Lucas Meneghel Rodrigues <lookkas@gmail.com>

--- selftests/unit/test_remote.py
+++ selftests/unit/test_remote.py
@@ -136,7 +136,8 @@ class RemoteTestResultTest(unittest.TestCase):
         Stream.should_receive('notify').once().ordered()
         remote_remote = flexmock(remoter)
         (remote_remote.should_receive('Remote')
-         .with_args('hostname', 'username', 'password', 22, 60)
+         .with_args(hostname='hostname', username='username',
+                    password='password', key_filename=None, port=22, timeout=60)
          .once().ordered()
          .and_return(Remote))
         Args = flexmock(test_result_total=1,
@@ -146,6 +147,7 @@ class RemoteTestResultTest(unittest.TestCase):
                         remote_hostname='hostname',
                         remote_port=22,
                         remote_password='password',
+                        remote_key_file=None,
                         remote_no_copy=False,
                         remote_timeout=60)
         self.remote = remote.RemoteTestResult(Stream, Args)
