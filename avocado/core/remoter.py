452f8e9d42dfbb6adb02b2c0008728080c70a7c5
Lucas Meneghel Rodrigues <lookkas@gmail.com>
2016-01-28 19:32:23 -0200

    avocado.core.remoter.Remote: Turn static methods into module level functions
    The static methods can be turned into module level functions,
    in particular the command running code, that can be reused
    by any code that updates fabric environment info accordingly,
    so let's do it.
    
    Signed-off-by: Lucas Meneghel Rodrigues <lookkas@gmail.com>

--- avocado/core/remoter.py
+++ avocado/core/remoter.py
@@ -29,6 +29,7 @@ try:
     import fabric.api
     import fabric.network
     import fabric.operations
+    import fabric.tasks
 except ImportError:
     REMOTE_CAPABLE = False
     LOG.info('Remote module is disabled: could not import fabric')
@@ -43,6 +44,102 @@ class RemoterError(Exception):
 
 class ConnectionError(RemoterError):
     pass
+
+
+def run(command, ignore_status=False, quiet=True, timeout=60):
+    """
+    Executes a command on the defined fabric hosts.
+
+    This is basically a wrapper to fabric.operations.run, encapsulating
+    the result on an avocado process.CmdResult object. This also assumes
+    the fabric environment was previously (and properly) initialized.
+
+    :param command: the command string to execute.
+    :param ignore_status: Whether to not raise exceptions in case the
+        command's return code is different than zero.
+    :param timeout: Maximum time allowed for the command to return.
+    :param quiet: Whether to not log command stdout/err. Default: True.
+
+    :return: the result of the remote program's execution.
+    :rtype: :class:`avocado.utils.process.CmdResult`.
+    :raise fabric.exceptions.CommandTimeout: When timeout exhausted.
+    """
+
+    result = process.CmdResult()
+    start_time = time.time()
+    end_time = time.time() + (timeout or 0)   # Support timeout=None
+    # Fabric sometimes returns NetworkError even when timeout not reached
+    fabric_result = None
+    fabric_exception = None
+    while True:
+        try:
+            fabric_result = fabric.operations.run(command=command,
+                                                  quiet=quiet,
+                                                  warn_only=True,
+                                                  timeout=timeout)
+            break
+        except fabric.network.NetworkError, details:
+            fabric_exception = details
+            timeout = end_time - time.time()
+        if time.time() < end_time:
+            break
+    if fabric_result is None:
+        if fabric_exception is not None:
+            raise fabric_exception  # it's not None pylint: disable=E0702
+        else:
+            raise fabric.network.NetworkError("Remote execution of '%s'"
+                                              "failed without any "
+                                              "exception. This should not "
+                                              "happen." % command)
+    end_time = time.time()
+    duration = end_time - start_time
+    result.command = command
+    result.stdout = str(fabric_result)
+    result.stderr = fabric_result.stderr
+    result.duration = duration
+    result.exit_status = fabric_result.return_code
+    result.failed = fabric_result.failed
+    result.succeeded = fabric_result.succeeded
+    if not ignore_status:
+        if result.failed:
+            raise process.CmdError(command=command, result=result)
+    return result
+
+
+def send_files(local_path, remote_path):
+    """
+    Send files to the defined fabric host.
+
+    This assumes the fabric environment was previously (and properly)
+    initialized.
+
+    :param local_path: the local path.
+    :param remote_path: the remote path.
+    """
+    try:
+        fabric.operations.put(local_path, remote_path,
+                              mirror_local_mode=True)
+    except ValueError:
+        return False
+    return True
+
+
+def receive_files(local_path, remote_path):
+    """
+    Receive files from the defined fabric host.
+
+    This assumes the fabric environment was previously (and properly)
+    initialized.
+
+    :param local_path: the local path.
+    :param remote_path: the remote path.
+    """
+    try:
+        fabric.operations.get(remote_path,
+                              local_path)
+    except ValueError:
+        return False
+    return True
 
 
 class Remote(object):
@@ -95,7 +192,7 @@ class Remote(object):
 
     def run(self, command, ignore_status=False, quiet=True, timeout=60):
         """
-        Run a remote command.
+        Run a command on the remote host.
 
         :param command: the command string to execute.
         :param ignore_status: Whether to not raise exceptions in case the
@@ -107,7 +204,7 @@ class Remote(object):
         :rtype: :class:`avocado.utils.process.CmdResult`.
         :raise fabric.exceptions.CommandTimeout: When timeout exhausted.
         """
-        return_dict = fabric.tasks.execute(self._run, command, ignore_status,
+        return_dict = fabric.tasks.execute(run, command, ignore_status,
                                            quiet, timeout,
                                            hosts=[self.hostname])
         return return_dict[self.hostname]
@@ -178,41 +275,23 @@ class Remote(object):
         self.run('mkdir -p %s' % remote_path)
 
     def send_files(self, local_path, remote_path):
-        result_dict = fabric.tasks.execute(self._send_files, local_path,
-                                           remote_path, hosts=[self.hostname])
-        return result_dict[self.hostname]
-
-    @staticmethod
-    def _send_files(local_path, remote_path):
         """
-        Send files to remote.
+        Send files to remote host.
 
         :param local_path: the local path.
         :param remote_path: the remote path.
         """
-        try:
-            fabric.operations.put(local_path, remote_path,
-                                  mirror_local_mode=True)
-        except ValueError:
-            return False
-        return True
+        result_dict = fabric.tasks.execute(send_files, local_path,
+                                           remote_path, hosts=[self.hostname])
+        return result_dict[self.hostname]
 
     def receive_files(self, local_path, remote_path):
-        result_dict = fabric.tasks.execute(self._receive_files, local_path,
-                                           remote_path, hosts=[self.hostname])
-        return result_dict[self.hostname]
-
-    @staticmethod
-    def _receive_files(self, local_path, remote_path):
         """
-        receive remote files.
+        Receive files from the remote host.
 
         :param local_path: the local path.
         :param remote_path: the remote path.
         """
-        try:
-            fabric.operations.get(remote_path,
-                                  local_path)
-        except ValueError:
-            return False
-        return True
+        result_dict = fabric.tasks.execute(receive_files, local_path,
+                                           remote_path, hosts=[self.hostname])
+        return result_dict[self.hostname]
