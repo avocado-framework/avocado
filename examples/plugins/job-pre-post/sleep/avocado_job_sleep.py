a9bd9fddb67ab499a3dd2db165c3da7a2ffe4ceb
Cleber Rosa <crosa@redhat.com>
2016-04-22 15:24:07 -0300

    Plugins: Introduce Pre and Post Job Execution Plugins
    These new plugin interfaces, to be registered under the
    'avocado.plugins.job.prepost' namespace, allow for any custom action
    to be executed before and/or after the job execution.
    
    The JobPre plugin interface requires the `pre` method to be
    implemented, while the JobPost plugin interface requires the `post`
    method to be implemented.  The separation of the interfaces allows for
    mandatory implementation of the given interfaces, which was the whole
    point of using Abstract base classes, instead of having empty default
    implementations.
    
    One single class may implement either one, or both of those
    interfaces.  Both `pre` and `post` methods receive the current Job
    instance as an argument, which allows the plugin to fetch any job
    information or even to tweak as it may seem fit.
    
    Signed-off-by: Cleber Rosa <crosa@redhat.com>

---
+++ examples/plugins/job-pre-post/sleep/avocado_job_sleep.py
@@ -0,0 +1,25 @@
+import time
+import logging
+
+from avocado.core.settings import settings
+from avocado.plugins.base import JobPre, JobPost
+
+
+class Sleep(JobPre, JobPost):
+
+    name = 'sleep'
+    description = 'Sleeps for a number of seconds'
+
+    def __init__(self):
+        self.log = logging.getLogger("avocado.app")
+        self.seconds = settings.get_value(section="plugins.job.sleep",
+                                          key="seconds",
+                                          key_type=int,
+                                          default=3)
+
+    def sleep(self, job):
+        for i in xrange(1, self.seconds + 1):
+            self.log.info("Sleeping %2i/%s", i, self.seconds)
+            time.sleep(1)
+
+    pre = post = sleep
