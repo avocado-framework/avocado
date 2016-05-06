d37c23fae01574b63d7d0983e0b3defce0f1a981
Lukáš Doktor <ldoktor@redhat.com>
2016-05-06 10:46:04 +0200

    avocado: Avoid custom handling of SIGINT and SIGUSR
    Currently avocado adds custom handlers of SIGINT and SIGUSR1 in order to
    produce traceback and notify about user interaction or timeout. This
    could be missleading in case the test uses those signals and potentially
    dangerous as some tests assume default behavior.
    
    This patch removes the custom handling of SIGINT and SIGUSR1 and
    reports the failure in `job.log`. Additionally it tries to inject the
    error message in the test output, if status.logfile available.
    
    In order to keep the useful traceback in case of interruption, this
    patch overrides the default SIGTERM handler. The default behavior of
    SIGTERM is to die, our custom handler raises SystemExit with info saying
    the test was interrupted by sigterm, which should generate traceback and
    finish. The runner then changes the result to INTERRUPTED, so even
    when the test modifies the SIGTERM handler, we get the correct status.
    
    Worth mentioning that in case test ignores SIGTERM, SIGKILL is emitted
    by the runner, so this should be safe approach.
    
    Signed-off-by: Lukáš Doktor <ldoktor@redhat.com>

--- avocado/core/runner.py
+++ avocado/core/runner.py
@@ -230,6 +230,12 @@ class TestRunner(object):
         sys.stdout = output.LoggingFile(logger=logger_list_stdout)
         sys.stderr = output.LoggingFile(logger=logger_list_stderr)
 
+        def sigterm_handler(signum, frame):     # pylint: disable=W0613
+            """ Produce traceback on SIGTERM """
+            raise SystemExit("Test interrupted by SIGTERM")
+
+        signal.signal(signal.SIGTERM, sigterm_handler)
+
         instance = loader.load_test(test_factory)
         if instance.runner_queue is None:
             instance.runner_queue = queue
@@ -237,17 +243,6 @@ class TestRunner(object):
         early_state = instance.get_state()
         early_state['early_status'] = True
         queue.put(early_state)
-
-        def timeout_handler(signum, frame):
-            e_msg = "Timeout reached waiting for %s to end" % instance
-            raise exceptions.TestTimeoutInterrupted(e_msg)
-
-        def interrupt_handler(signum, frame):
-            e_msg = "Test %s interrupted by user" % instance
-            raise exceptions.TestInterruptedError(e_msg)
-
-        signal.signal(signal.SIGUSR1, timeout_handler)
-        signal.signal(signal.SIGINT, interrupt_handler)
 
         self.result.start_test(early_state)
         try:
@@ -330,12 +325,14 @@ class TestRunner(object):
         stage_2_msg_displayed = False
         first = 0.01
         step = 0.1
+        abort_reason = None
 
         while True:
             try:
                 if time.time() >= deadline:
+                    abort_reason = "Timeout reached"
                     try:
-                        os.kill(proc.pid, signal.SIGUSR1)
+                        os.kill(proc.pid, signal.SIGTERM)
                     except OSError:
                         pass
                     break
@@ -357,6 +354,7 @@ class TestRunner(object):
                 ctrl_c_count += 1
                 if ctrl_c_count == 1:
                     if not stage_1_msg_displayed:
+                        abort_reason = "Interrupted by ctrl+c"
                         self.job.log.debug("\nInterrupt requested. Waiting %d "
                                            "seconds for test to finish "
                                            "(ignoring new Ctrl+C until then)",
@@ -365,6 +363,7 @@ class TestRunner(object):
                     ignore_time_started = time.time()
                 if (ctrl_c_count > 1) and (time_elapsed > ignore_window):
                     if not stage_2_msg_displayed:
+                        abort_reason = "Interrupted by ctrl+c (multiple-times)"
                         self.job.log.debug("Killing test subprocess %s",
                                            proc.pid)
                         stage_2_msg_displayed = True
@@ -376,6 +375,20 @@ class TestRunner(object):
         else:
             test_state = test_status.abort(proc, time_started, cycle_timeout,
                                            first, step)
+
+        # Try to log the timeout reason to test's results and update test_state
+        if abort_reason:
+            TEST_LOG.error(abort_reason)
+            test_log = test_state.get("logfile")
+            if test_log:
+                open(test_log, "a").write("\nRUNNER: " + abort_reason + "\n")
+            if test_state.get("text_output"):
+                test_state["text_output"] += "\nRUNNER: " + abort_reason + "\n"
+            else:
+                test_state["text_output"] = abort_reason
+            test_state["status"] = "INTERRUPTED"
+            test_state["fail_reason"] = abort_reason
+            test_state["fail_class"] = "RUNNER"
 
         # don't process other tests from the list
         if ctrl_c_count > 0:
