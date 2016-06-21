79f259e50d4cee70e2cdb47740c4d302391ec2da
Lukáš Doktor <ldoktor@redhat.com>
2016-06-21 12:13:35 +0200

    avocado.utils.archive: Fix file permissions in zip files
    Zip files are stored with permissions, but python's zipfile does not
    reflect that. This patch adjusts the symlink fix to also update file
    permissions.
    
    Signed-off-by: Lukáš Doktor <ldoktor@redhat.com>

--- avocado/utils/archive.py
+++ avocado/utils/archive.py
@@ -165,26 +165,29 @@ class ArchiveFile(object):
         """
         self._engine.extractall(path)
         if self.is_zip:
-            self._update_symlinks(path)
+            self._update_zip_extra_attrs(path)
 
-    def _update_symlinks(self, dst_dir='.'):
+    def _update_zip_extra_attrs(self, dst_dir):
         if platform.system() != "Linux":
-            LOG.warn("Symlink handling in zip files not supported on Windows.")
+            LOG.warn("Attr handling in zip files only supported on Linux.")
             return
         # Walk all files and re-create files as symlinks
         for path, info in self._engine.NameToInfo.iteritems():
-            if info.external_attr >> 16 & stat.S_IFLNK == stat.S_IFLNK:
+            dst = os.path.join(dst_dir, path)
+            if not os.path.exists(dst):
+                LOG.warn("Paths in this zip file are stored in unsupported "
+                         "format, not updating the attributes. (%s)", msg)
+                return
+            attr = info.external_attr >> 16
+            if attr & stat.S_IFLNK == stat.S_IFLNK:
                 dst = os.path.join(dst_dir, path)
-                try:
-                    src = open(dst, 'r').read()
-                except IOError as details:
-                    msg = ("Zip file symlink path '%s' is stored in "
-                           "unsupported format, symlinks were not updated: %s"
-                           % (path, details))
-                    LOG.warn(msg)
-                    return
+                src = open(dst, 'r').read()
                 os.remove(dst)
                 os.symlink(src, dst)
+                continue    # Don't override any other attributes on links
+            mode = attr & 511   # Mask only permissions
+            if mode and mode != 436:  # If mode is stored and is not default
+                os.chmod(dst, mode)
 
     def close(self):
         """
