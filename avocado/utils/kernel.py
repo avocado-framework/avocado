acdce23382245fab79c1c76fb5fcd58165cbf7d3
Dmitry Monakhov <dmonakhov@openvz.org>
2016-07-04 20:17:59 +0300

    kernel.py: add expire argument to Asset constructor.
    expire arg becomes  mandatory after 1278's pull request.

--- avocado/utils/kernel.py
+++ avocado/utils/kernel.py
@@ -72,7 +72,8 @@ class KernelBuild(object):
         full_url = self.URL + self.SOURCE.format(version=self.version)
         self.asset_path = asset.Asset(full_url, asset_hash=None,
                                       algorithm=None, locations=None,
-                                      cache_dirs=self.data_dirs).fetch()
+                                      cache_dirs=self.data_dirs,
+                                      expire=None).fetch()
 
     def uncompress(self):
         """
