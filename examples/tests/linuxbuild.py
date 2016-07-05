ba8f7cf9d9e078dbbdf1e097785cffc61a3c5737
Dmitry Monakhov <dmonakhov@openvz.org>
2016-07-05 13:51:20 +0300

    example: linuxbuild.py add source_hash support
    linuxbuild.py: add source_hash,hash_algo 
    Also add linuxbuild.yaml which demonstrate how to use this options.
    
    #avocado run linuxbuild.py -m /usr/share/avocado/tests/linuxbuild.py.data/linuxbuild.yaml

--- examples/tests/linuxbuild.py
+++ examples/tests/linuxbuild.py
@@ -6,6 +6,9 @@ from avocado import Test
 from avocado import main
 from avocado.utils import kernel
 
+# External parameters should be passed via multiplex file
+# example: 
+# avocado run linuxbuild.py -m /usr/share/avocado/tests/linuxbuild.py.data/linuxbuild.yaml
 
 class LinuxBuildTest(Test):
 
@@ -14,19 +17,25 @@ class LinuxBuildTest(Test):
 
     :param linux_version: kernel version to be built
     :param linux_config: name of the config file located in deps path
+    :param source_url: source tarball full url
+    :param source_hash: source tarball hash
+    :param hash_algo: hash algorithm
     """
 
     def setUp(self):
         kernel_version = self.params.get('linux_version', default='3.19.8')
         linux_config = self.params.get('linux_config', default=None)
+        source_url = self.params.get('source_url', default=None)
+        source_hash = self.params.get('source_hash', default=None)
+        hash_algo= self.params.get('hash_algo', default=None)
         if linux_config is not None:
             linux_config = os.path.join(self.datadir, linux_config)
-
+        
         self.linux_build = kernel.KernelBuild(kernel_version,
                                               linux_config,
                                               self.srcdir,
                                               self.cache_dirs)
-        self.linux_build.download()
+        self.linux_build.download(source_url, source_hash, hash_algo)
         self.linux_build.uncompress()
         self.linux_build.configure()
 
