6cb8804991ea5f26c8bc26f7204133aae60d48dc
Lukáš Doktor <ldoktor@redhat.com>
2016-09-06 16:21:36 +0200

    avocado.core.sysinfo: Force LANG for sysinfo collection
    This commit allows setting custom LANG for sysinfo collection to
    simplify `avocado diff` comparison between different machines. One can
    disable this by setting no/empty value.
    
    Signed-off-by: Lukáš Doktor <ldoktor@redhat.com>

--- avocado/core/sysinfo.py
+++ avocado/core/sysinfo.py
@@ -151,6 +151,9 @@ class Command(Collectible):
         env = os.environ.copy()
         if "PATH" not in env:
             env["PATH"] = "/usr/bin:/bin"
+        lang = settings.get_value("sysinfo.collect", "lang", str, None)
+        if lang:
+            env["LANG"] = lang
         logf_path = os.path.join(logdir, self.logf)
         stdin = open(os.devnull, "r")
         stdout = open(logf_path, "w")
@@ -185,6 +188,9 @@ class Daemon(Command):
         env = os.environ.copy()
         if "PATH" not in env:
             env["PATH"] = "/usr/bin:/bin"
+        lang = settings.get_value("sysinfo.collect", "lang", str, None)
+        if lang:
+            env["LANG"] = lang
         logf_path = os.path.join(logdir, self.logf)
         stdin = open(os.devnull, "r")
         stdout = open(logf_path, "w")
