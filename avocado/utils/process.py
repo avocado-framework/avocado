40231e1ee75ddad03d37961c5426e19810b087b0
Lukáš Doktor <ldoktor@redhat.com>
2016-09-20 15:57:34 +0200

    avocado.utils.process: Add method to check whether sudo is configured
    The `avocado.utils.process` allows the use of sudo, but it might not be
    always enabled/configured in the system. This method should allow
    detecting whether it is or is not available.
    
    Signed-off-by: Lukáš Doktor <ldoktor@redhat.com>

--- avocado/utils/process.py
+++ avocado/utils/process.py
@@ -98,6 +98,18 @@ class CmdError(Exception):
             return "CmdError"
 
 
+def sudo_available():
+    """
+    :return: True when sudo is available (or is root)
+    """
+    if os.getpid() == 0:
+        return True
+    elif system_output("id -u", ignore_status=True, sudo=True).strip() == "0":
+        return True
+    else:
+        return False
+
+
 def pid_exists(pid):
     """
     Return True if a given PID exists.
