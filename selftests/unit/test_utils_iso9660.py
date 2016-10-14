042d8af1cf801b5aeabb01f1b183f3c7c22c91bc
Lukáš Doktor <ldoktor@redhat.com>
2016-10-14 17:19:19 +0200

    selftests: Improve the can_sudo handling in selftests
    There were several implementations of can_sudo methods in selftests,
    most of them broken as eg. "mount" can run without sudo, which is what
    happens if sudo is not available.
    
    This patch replaces those custom methods with the one recently
    introduced in `avocado.utils.process`.
    
    Signed-off-by: Lukáš Doktor <ldoktor@redhat.com>

--- selftests/unit/test_utils_iso9660.py
+++ selftests/unit/test_utils_iso9660.py
@@ -43,8 +43,8 @@ class BaseIso9660(unittest.TestCase):
         self.iso.close()
         self.iso.close()    # check that double-close won't fail
 
-    @unittest.skipIf(not process.can_sudo(),
-                     "This test requires sudo or root")
+    @unittest.skipIf(not process.can_sudo("mount"),
+                     "This test requires mount to run under sudo or root")
     def mnt_dir_workflow(self):
         """
         Check the mnt_dir functionality
@@ -116,7 +116,7 @@ class IsoMount(BaseIso9660):
     Mount-based check
     """
 
-    @unittest.skipIf(not process.can_sudo(),
+    @unittest.skipIf(not process.can_sudo("mount"),
                      "This test requires sudo or root")
     def setUp(self):
         super(IsoMount, self).setUp()
