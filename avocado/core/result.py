f55c4a4f7d638bbc3e2e4f7a7a8493bfd06019e3
Cleber Rosa <crosa@redhat.com>
2016-10-16 21:36:47 -0300

    Result: do not hide errors from jobs without unique IDs
    A result has a 1:1 relation to a Job. To match one to the other the
    job `unique_id` is saved to the result as `job_unique_id`.
    
    The same way that a job alwayas was a unique_id, the result should
    never miss the job_unique_id.  So, let's not try to hide or postpone
    errors from such as scenario.
    
    Signed-off-by: Cleber Rosa <crosa@redhat.com>

--- avocado/core/result.py
+++ avocado/core/result.py
@@ -94,7 +94,7 @@ class Result(object):
 
         :param job: an instance of :class:`avocado.core.job.Job`.
         """
-        self.job_unique_id = getattr(job, "unique_id", None)
+        self.job_unique_id = getattr(job, "unique_id")
         self.logfile = getattr(job, "logfile", None)
         self.tests_total = getattr(job.args, 'test_result_total', 0)
         self.tests_run = 0
