c7c6179974694e101dab0f80123b62db79c2110f
Lukáš Doktor <ldoktor@redhat.com>
2016-10-20 07:52:08 +0200

    mux: Use check instead of overriding the methods
    For speed optimization we used to override the "data_inject" and
    "data_merge" methods after parsing. Let's stop doing that as the check
    should not be that costly and support `Mux.ignore_new_data` attribute to
    allow ignoring new data on parsed tree (used by replay)
    
    Signed-off-by: Lukáš Doktor <ldoktor@redhat.com>

--- avocado/core/multiplexer.py
+++ avocado/core/multiplexer.py
@@ -366,14 +366,6 @@ class AvocadoParam(object):
                 yield (leaf.environment_origin[key].path, key, value)
 
 
-def _report_mux_already_parsed(self, *args, **kwargs):
-    """
-    Raises exception describing that `self.data` alteration is restricted
-    """
-    raise RuntimeError("Mux already parsed, altering is restricted. %s %s"
-                       % (args, kwargs))
-
-
 class Mux(object):
 
     """
@@ -391,6 +383,7 @@ class Mux(object):
         self.debug = debug
         self.data = tree.TreeNodeDebug() if debug else tree.TreeNode()
         self._mux_path = None
+        self.ignore_new_data = False   # Used to ignore new data on parsed Mux
 
     def parse(self, args):
         """
@@ -413,8 +406,6 @@ class Mux(object):
             self._mux_path = ['/run/*']
         # disable data alteration (and remove data as they are not useful)
         self.data = None
-        self.data_inject = _report_mux_already_parsed
-        self.data_merge = _report_mux_already_parsed
 
     def _process_default_params(self, args):
         """
@@ -437,7 +428,23 @@ class Mux(object):
         """
         Reports whether the tree was already multiplexed
         """
-        return self.variants is not None
+        return not isinstance(self.default_params, dict)
+
+    def _skip_new_data_check(self, fction, args):
+        """
+        Check whether we can inject the data
+
+        :param fction: Name of the data-inject function
+        :param args: Arguments of the data-inject function
+        :raise RuntimeError: When data injection is restricted
+        :return: True if new data should be ignored
+        """
+        if self.is_parsed():
+            if self.ignore_new_data:
+                return
+            raise RuntimeError("Mux already parsed, unable to execute "
+                               "%s%s"
+                               % (fction, args))
 
     def data_inject(self, name, key, value, path=None):
         """
@@ -448,6 +455,8 @@ class Mux(object):
         :param path: Optional path to the node to which we assign the value,
                      by default '/'.
         """
+        if self._skip_new_data_check("data_inject", (name, key, value, path)):
+            return
         if path is None:
             path = "/"
         if name not in self.default_params:
@@ -461,6 +470,8 @@ class Mux(object):
         :param tree: Tree to be merged into this database.
         :type tree: :class:`avocado.core.tree.TreeNode`
         """
+        if self._skip_new_data_check("data_merge", (tree,)):
+            return
         self.data.merge(tree)
 
     def get_number_of_tests(self, test_suite):
