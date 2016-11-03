c2cb1c60394515387476d1d29dcbc93370d5f49e
Cleber Rosa <crosa@redhat.com>
2016-11-03 02:12:24 -0300

    Result: do not hide errors from jobs without unique IDs
    A result has a 1:1 relation to a Job. To match one to the other the
    job `unique_id` is saved to the result as `job_unique_id`.
    
    The same way that a job alwayas was a unique_id, the result should
    never miss the job_unique_id.  So, let's not try to hide or postpone
    errors from such as scenario.
    
    Signed-off-by: Cleber Rosa <crosa@redhat.com>

---
+++ selftests/unit/test_result.py
@@ -0,0 +1,33 @@
+import sys
+import argparse
+if sys.version_info[:2] == (2, 6):
+    import unittest2 as unittest
+else:
+    import unittest
+
+from avocado.core.result import Result
+
+
+class FakeJobMissingUniqueId(object):
+
+    def __init__(self, args):
+        self.args = args
+
+
+class FakeJob(object):
+
+    def __init__(self, args):
+        self.args = args
+        self.unique_id = '0000000000000000000000000000000000000000'
+
+
+class ResultTest(unittest.TestCase):
+
+    def test_result_job_without_id(self):
+        args = argparse.Namespace()
+        result = Result(FakeJob(args))
+        self.assertRaises(AttributeError, Result, FakeJobMissingUniqueId(args))
+
+
+if __name__ == '__main__':
+    unittest.main()
