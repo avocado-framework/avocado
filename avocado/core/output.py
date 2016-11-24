20edc3916d1dc89a8d8cbff44ab35c7e4a3ab828
Amador Pahim <apahim@redhat.com>
2016-11-24 15:08:50 +0100

    protect output against infinite recursion
    If an instance of output.LoggingFile() contains a logger without a
    handler, the call to the python logging will write an error to stderr
    due to the missing handler. The consequent write to stderr will cause
    an infinite recursion if the sys.stderr is an instance of the
    output.LoggingFile() with a logger without handler.
    
    This patch checks if all loggers in output.LoggingFile() contains a
    handler and, if not, just skip the logger, avoiding the infinite recursion.
    
    Signed-off-by: Amador Pahim <apahim@redhat.com>

--- avocado/core/output.py
+++ avocado/core/output.py
@@ -639,6 +639,8 @@ class LoggingFile(object):
         Passes lines of output to the logging module.
         """
         for lg in self._logger:
+            if not lg.handlers:
+                continue
             lg.log(self._level, self._prefix + line)
 
     def _flush_buffer(self):
