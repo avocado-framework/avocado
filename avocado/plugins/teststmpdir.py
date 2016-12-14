d5820bba7c866af8aee7b4c77a94acb217f2339f
Amador Pahim <apahim@redhat.com>
2016-12-14 13:34:59 +0100

    plugins: tests temporary directory
    This commit creates a job pre/post plugin that generates a temporary
    directory intended to be consumed by the tests. The temporary directory
    will be created in 'pre', available via environment variable and removed
    in 'post. We are also exposing the tests temporary directory in the Test
    API.
    
    Reference: https://trello.com/c/P1JOtVY7
    Signed-off-by: Amador Pahim <apahim@redhat.com>

---
+++ avocado/plugins/teststmpdir.py
@@ -0,0 +1,50 @@
+# This program is free software; you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation; either version 2 of the License, or
+# (at your option) any later version.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.
+#
+# See LICENSE for more details.
+#
+# Copyright: Red Hat, Inc. 2016
+# Author: Amador Pahim <apahim@redhat.com>
+"""
+Tests temporary directory plugin
+"""
+
+import os
+import shutil
+import tempfile
+
+from avocado.core.plugin_interfaces import JobPre, JobPost
+
+
+class TestsTmpDir(JobPre, JobPost):
+
+    name = 'teststmpdir'
+    description = 'Creates a temporary directory for tests consumption'
+
+    def __init__(self):
+        self._varname = 'AVOCADO_TESTS_COMMON_TMPDIR'
+        self._dirname = None
+
+    def pre(self, job):
+        if os.environ.get(self._varname) is None:
+            self._dirname = tempfile.mkdtemp(prefix='avocado_')
+            os.environ[self._varname] = self._dirname
+
+    def post(self, job):
+        if (self._dirname is not None and
+                os.environ.get(self._varname) == self._dirname):
+            try:
+                shutil.rmtree(self._dirname)
+                del os.environ[self._varname]
+            except:
+                pass
+
+    def __del__(self):
+        # Make sure we will cleanup if something bad happens
+        self.post(None)
