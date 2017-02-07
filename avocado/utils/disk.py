1f4dd3be0e35a2a9f43931841a5d0759fb7ae64d
Narasimhan V <sim@linux.vnet.ibm.com>
2017-02-07 20:34:07 +0530

    Added filesystem and mount functions on disks
    avocado/utils/disk.py:
    * Added creating filesystem, mount and unmount functionalities
    * Fixed docstring
    
    Signed-off-by: Narasimhan V <sim@linux.vnet.ibm.com>

--- avocado/utils/disk.py
+++ avocado/utils/disk.py
@@ -9,8 +9,10 @@
 #
 # See LICENSE for more details.
 #
-# This code was inspired in the autotest project,
+# Copyright: 2017 IBM
+# Author: Narasimhan V sim@linux.vnet.ibm.com
 #
+# freespace() function was inspired in the autotest project,
 # client/base_utils.py
 
 
@@ -18,9 +20,73 @@
 Disk utilities
 """
 
+import logging
 import os
+from . import process
+
+
+LOGGER = logging.getLogger('avocado.test')
+
+
+class FSException(Exception):
+    """
+    Base Exception Class for all exceptions
+    """
+    pass
+
+
+class DiskException(Exception):
+    """
+    Base Exception Class for all exceptions
+    """
 
 
 def freespace(path):
+    """
+    Gets freespace on the given path.
+    """
     fs_stats = os.statvfs(path)
     return fs_stats.f_bsize * fs_stats.f_bavail
+
+
+def zero_disk(disk_name):
+    """
+    Zero a disk.
+    :param disk_name: Name of the disk
+    """
+
+    try:
+        process.run("dd if=/dev/zero of=%s bs=4096 count=1" % disk_name)
+    except process.CmdError, ex:
+        raise DiskException("Fail to zero disk: %s" % ex)
+
+
+def disk_mount(disk_name, mount_loc, create_filesystem="", args=""):
+    """
+    Mount a filesystem to a mount location.
+    :param disk_name: Name of the disk
+    :mount_loc: Location to mount the logical volume
+    :param create_filesystem: Can be one of ext2, ext3, ext4, vfat or empty
+    """
+
+    try:
+        if create_filesystem:
+            process.run("mkfs.%s %s %s" %
+                        (create_filesystem, args, disk_name),
+                        sudo=True)
+        process.run("mount %s %s" %
+                    (disk_name, mount_loc), sudo=True)
+    except process.CmdError, ex:
+        raise FSException("Fail to mount fs: %s" % ex)
+
+
+def disk_umount(disk_name):
+    """
+    Unmount a filesystem from a mount location.
+    :param disk_name: Name of disk
+    """
+
+    try:
+        process.run("umount %s" % disk_name, sudo=True)
+    except process.CmdError, ex:
+        raise FSException("Fail to unmount fs: %s" % ex)
