36b2338332de2ceff21226231d823e417925bfbd
Lukáš Doktor <ldoktor@redhat.com>
2017-03-28 10:16:34 +0200

    yaml_to_mux: Refactor tree_node_from_values
    The tree_node_from_values is already quite complex and is about to be
    extended. Let's split the body into functions.
    
    Signed-off-by: Lukáš Doktor <ldoktor@redhat.com>

--- avocado/plugins/yaml_to_mux.py
+++ avocado/plugins/yaml_to_mux.py
@@ -78,38 +78,48 @@ def _create_from_yaml(path, cls_node=mux.MuxTreeNode):
     """ Create tree structure from yaml stream """
     def tree_node_from_values(name, values):
         """ Create `name` node and add values  """
+        def handle_control_tag(node, value):
+            """ Handling of YAML tags (except of !using) """
+            if value[0].code == YAML_INCLUDE:
+                # Include file
+                ypath = value[1]
+                if not os.path.isabs(ypath):
+                    ypath = os.path.join(os.path.dirname(path), ypath)
+                if not os.path.exists(ypath):
+                    raise ValueError("File '%s' included from '%s' does not "
+                                     "exist." % (ypath, path))
+                node.merge(_create_from_yaml('/:' + ypath, cls_node))
+            elif value[0].code == YAML_REMOVE_NODE:
+                value[0].value = value[1]   # set the name
+                node.ctrl.append(value[0])    # add "blue pill" of death
+            elif value[0].code == YAML_REMOVE_VALUE:
+                value[0].value = value[1]   # set the name
+                node.ctrl.append(value[0])
+            elif value[0].code == YAML_MUX:
+                node.multiplex = True
+
+        def handle_control_tag_using(name, using, value):
+            """ Handling of the !using tag """
+            if using:
+                raise ValueError("!using can be used only once per "
+                                 "node! (%s:%s)" % (path, name))
+            using = value[1]
+            if using[0] == '/':
+                using = using[1:]
+            if using[-1] == '/':
+                using = using[:-1]
+            return using
+
         node = cls_node(str(name))
         using = ''
         for value in values:
             if isinstance(value, cls_node):
                 node.add_child(value)
             elif isinstance(value[0], mux.Control):
-                if value[0].code == YAML_INCLUDE:
-                    # Include file
-                    ypath = value[1]
-                    if not os.path.isabs(ypath):
-                        ypath = os.path.join(os.path.dirname(path), ypath)
-                    if not os.path.exists(ypath):
-                        raise ValueError("File '%s' included from '%s' does not "
-                                         "exist." % (ypath, path))
-                    node.merge(_create_from_yaml('/:' + ypath, cls_node))
-                elif value[0].code == YAML_USING:
-                    if using:
-                        raise ValueError("!using can be used only once per "
-                                         "node! (%s:%s)" % (path, name))
-                    using = value[1]
-                    if using[0] == '/':
-                        using = using[1:]
-                    if using[-1] == '/':
-                        using = using[:-1]
-                elif value[0].code == YAML_REMOVE_NODE:
-                    value[0].value = value[1]   # set the name
-                    node.ctrl.append(value[0])    # add "blue pill" of death
-                elif value[0].code == YAML_REMOVE_VALUE:
-                    value[0].value = value[1]   # set the name
-                    node.ctrl.append(value[0])
-                elif value[0].code == YAML_MUX:
-                    node.multiplex = True
+                if value[0].code == YAML_USING:
+                    using = handle_control_tag_using(name, using, value)
+                else:
+                    handle_control_tag(node, value)
             else:
                 node.value[value[0]] = value[1]
         if using:
