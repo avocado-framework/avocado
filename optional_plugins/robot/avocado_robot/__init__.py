442bff7393adc1c3b0affbde7344d2c0b1a9a04f
Amador Pahim <apahim@redhat.com>
2017-03-29 14:17:02 +0200

    robot: fix error on unknown tests
    When robot plugin cannot determine the tests from the test references we
    should return an empty list instead of an exception.
    
    Signed-off-by: Amador Pahim <apahim@redhat.com>

--- optional_plugins/robot/avocado_robot/__init__.py
+++ optional_plugins/robot/avocado_robot/__init__.py
@@ -27,6 +27,7 @@ from avocado.core.plugin_interfaces import CLI
 from avocado.core.runner import TestRunner
 from avocado.utils import path as utils_path
 from robot import run
+from robot.errors import DataError
 from robot.parsing.model import TestData
 from robot.model import SuiteNamePatterns
 
@@ -89,10 +90,14 @@ class RobotLoader(loader.TestLoader):
         if ':' in url:
             url, _subtests_filter = url.split(':', 1)
             subtests_filter = re.compile(_subtests_filter)
-        robot_suite = self._find_tests(TestData(parent=None,
-                                                source=url,
-                                                include_suites=SuiteNamePatterns()),
-                                       test_suite={})
+        try:
+            robot_suite = self._find_tests(TestData(parent=None,
+                                           source=url,
+                                           include_suites=SuiteNamePatterns()),
+                                           test_suite={})
+        except DataError:
+            return []
+
         for item in robot_suite:
             for robot_test in robot_suite[item]:
                 test_name = "%s:%s.%s" % (robot_test['test_source'],
