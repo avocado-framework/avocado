17ba6724cd9408bf30d388642a73408ae53cc043
Cleber Rosa <crosa@redhat.com>
2017-08-22 00:05:15 -0400

    avocado/core/test.py: rename TestName to TestID
    The current TestName class records more than a test name (according
    to the Avocado specification).  It records, and by default, presents
    the Test ID (__str__() and __repr__()).
    
    The Test Name, again according to the Avocado specifications, is
    contained within a Test ID, so it's not necessary to have two
    different classes IMO.
    
    Finally, the str_filesystem() method was adapted to be coherent with
    the fact that it returns a file system representation of the Test ID,
    not the Test Name.  It also became a property, to match the same use
    of str_uid and str_variant.
    
    Signed-off-by: Cleber Rosa <crosa@redhat.com>

--- selftests/unit/test_loader.py
+++ selftests/unit/test_loader.py
@@ -253,7 +253,7 @@ class LoaderTest(unittest.TestCase):
         test_class, test_parameters = (
             self.loader.discover(simple_test.path, loader.ALL)[0])
         self.assertTrue(test_class == test.SimpleTest, test_class)
-        test_parameters['name'] = test.TestName(0, test_parameters['name'])
+        test_parameters['name'] = test.TestID(0, test_parameters['name'])
         test_parameters['base_logdir'] = self.tmpdir
         tc = test_class(**test_parameters)
         tc.run_avocado()
@@ -299,7 +299,7 @@ class LoaderTest(unittest.TestCase):
         test_class, test_parameters = (
             self.loader.discover(avocado_not_a_test.path, loader.ALL)[0])
         self.assertTrue(test_class == test.SimpleTest, test_class)
-        test_parameters['name'] = test.TestName(0, test_parameters['name'])
+        test_parameters['name'] = test.TestID(0, test_parameters['name'])
         test_parameters['base_logdir'] = self.tmpdir
         tc = test_class(**test_parameters)
         # The test can't be executed (no shebang), raising an OSError
@@ -315,7 +315,7 @@ class LoaderTest(unittest.TestCase):
         test_class, test_parameters = (
             self.loader.discover(avocado_simple_test.path, loader.ALL)[0])
         self.assertTrue(test_class == test.SimpleTest)
-        test_parameters['name'] = test.TestName(0, test_parameters['name'])
+        test_parameters['name'] = test.TestID(0, test_parameters['name'])
         test_parameters['base_logdir'] = self.tmpdir
         tc = test_class(**test_parameters)
         tc.run_avocado()
