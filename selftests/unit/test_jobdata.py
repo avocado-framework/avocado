3344699ac8e91b953d7e47d89a3407feec401c69
Cleber Rosa <crosa@redhat.com>
2017-10-04 11:36:16 -0400

    Python 3 port: address test for avocado.core.jobdata
    Signed-off-by: Cleber Rosa <crosa@redhat.com>

--- selftests/unit/test_jobdata.py
+++ selftests/unit/test_jobdata.py
@@ -2,6 +2,8 @@ import glob
 import os
 import unittest
 
+from six import PY3
+
 from avocado.core import jobdata
 
 
@@ -87,6 +89,7 @@ class JobdataTest(unittest.TestCase):
             errs.append("cmdline: Invalid cmdline '%s' (%s)" % (act, exp))
         return errs
 
+    @unittest.skipIf(PY3, "Skipping tests with data pickled on Python 2")
     def test_versions(self):
         os.chdir(BASEDIR)
         errs = []
