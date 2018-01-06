52341ce71fc7b4cec83d89bf7b799f0ac615323c
Cleber Rosa <crosa@redhat.com>
2018-01-05 21:35:03 -0500

    Settings: remove variables from the main module namespace
    The main module namespace has a lot of variables which are only
    used in the main class initialization method.  Let's avoid poluting
    the module namespace and move those to where they're used.  If
    we want to put variables at the global module namespace, we
    should consider if they're useful to other modules, make them
    all caps, and document them.
    
    A nicer refactor could be done here, but IMO, this is still an
    improvement.
    
    Signed-off-by: Cleber Rosa <crosa@redhat.com>

--- avocado/core/settings.py
+++ avocado/core/settings.py
@@ -30,26 +30,6 @@ from six import string_types
 
 from ..utils import path
 
-if 'VIRTUAL_ENV' in os.environ:
-    CFG_DIR = os.path.join(os.environ['VIRTUAL_ENV'], 'etc')
-    USER_DIR = os.environ['VIRTUAL_ENV']
-else:
-    CFG_DIR = '/etc'
-    USER_DIR = os.path.expanduser("~")
-
-_config_dir_system = os.path.join(CFG_DIR, 'avocado')
-_config_dir_system_extra = os.path.join(CFG_DIR, 'avocado', 'conf.d')
-_config_dir_local = os.path.join(USER_DIR, '.config', 'avocado')
-_source_tree_root = os.path.join(sys.modules[__name__].__file__, "..", "..", "..")
-_config_path_intree = os.path.join(os.path.abspath(_source_tree_root),
-                                   'avocado', 'etc', 'avocado')
-_config_path_intree_extra = os.path.join(_config_path_intree, 'conf.d')
-
-config_filename = 'avocado.conf'
-config_path_system = os.path.join(_config_dir_system, config_filename)
-config_path_local = os.path.join(_config_dir_local, config_filename)
-config_path_intree = os.path.join(_config_path_intree, config_filename)
-
 
 class SettingsError(Exception):
 
@@ -168,6 +148,26 @@ class Settings(object):
         self.config_paths = []
         self.config_paths_failed = []
         if config_path is None:
+            if 'VIRTUAL_ENV' in os.environ:
+                cfg_dir = os.path.join(os.environ['VIRTUAL_ENV'], 'etc')
+                user_dir = os.environ['VIRTUAL_ENV']
+            else:
+                cfg_dir = '/etc'
+                user_dir = os.path.expanduser("~")
+
+            _config_dir_system = os.path.join(cfg_dir, 'avocado')
+            _config_dir_system_extra = os.path.join(cfg_dir, 'avocado', 'conf.d')
+            _config_dir_local = os.path.join(user_dir, '.config', 'avocado')
+            _source_tree_root = os.path.join(sys.modules[__name__].__file__, "..", "..", "..")
+            _config_path_intree = os.path.join(os.path.abspath(_source_tree_root),
+                                               'avocado', 'etc', 'avocado')
+            _config_path_intree_extra = os.path.join(_config_path_intree, 'conf.d')
+
+            config_filename = 'avocado.conf'
+            config_path_system = os.path.join(_config_dir_system, config_filename)
+            config_path_local = os.path.join(_config_dir_local, config_filename)
+            config_path_intree = os.path.join(_config_path_intree, config_filename)
+
             config_system = os.path.exists(config_path_system)
             config_system_extra = os.path.exists(_config_dir_system_extra)
             config_local = os.path.exists(config_path_local)
