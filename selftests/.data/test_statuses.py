a2c5c5f29f9618120f0a0fd7693ed2c107a5ae12
Cleber Rosa <crosa@redhat.com>
2018-01-30 04:54:20 -0500

    Test statuses: do not rely on yaml_to_mux varianter plugin
    Which is an optional plugin, and the test statuses feature is at the
    innermost core of Avocado.  While the original test indeed saved a lot
    of duplicated code, it's code that serves a specific testing purpose,
    and it's better to be simpler and more verbose and with the right (no)
    dependencies.
    
    Signed-off-by: Cleber Rosa <crosa@redhat.com>

--- selftests/.data/test_statuses.py
+++ selftests/.data/test_statuses.py
@@ -1,22 +1,308 @@
+import os
+import sys
+
 from avocado import Test
 
 
-class StatusTest(Test):
+class SkipSetup(Test):
 
     def setUp(self):
         self.log.info('setup pre')
-        if self.params.get('location') == 'setUp':
-            exec(self.params.get('command'))
+        self.skip()
         self.log.info('setup post')
 
     def test(self):
         self.log.info('test pre')
-        if self.params.get('location') == 'test':
-            exec(self.params.get('command'))
         self.log.info('test post')
 
     def tearDown(self):
         self.log.info('teardown pre')
-        if self.params.get('location') == 'tearDown':
-            exec(self.params.get('command'))
+        self.log.info('teardown post')
+
+
+class Skip(Test):
+
+    def setUp(self):
+        self.log.info('setup pre')
+        self.log.info('setup post')
+
+    def test(self):
+        self.log.info('test pre')
+        self.skip()
+        self.log.info('test post')
+
+    def tearDown(self):
+        self.log.info('teardown pre')
+        self.log.info('teardown post')
+
+
+class SkipTeardown(Test):
+
+    def setUp(self):
+        self.log.info('setup pre')
+        self.log.info('setup post')
+
+    def test(self):
+        self.log.info('test pre')
+        self.log.info('test post')
+
+    def tearDown(self):
+        self.log.info('teardown pre')
+        self.skip()
+        self.log.info('teardown post')
+
+
+class CancelSetup(Test):
+
+    def setUp(self):
+        self.log.info('setup pre')
+        self.cancel()
+        self.log.info('setup post')
+
+    def test(self):
+        self.log.info('test pre')
+        self.log.info('test post')
+
+    def tearDown(self):
+        self.log.info('teardown pre')
+        self.log.info('teardown post')
+
+
+class Cancel(Test):
+
+    def setUp(self):
+        self.log.info('setup pre')
+        self.log.info('setup post')
+
+    def test(self):
+        self.log.info('test pre')
+        self.cancel()
+        self.log.info('test post')
+
+    def tearDown(self):
+        self.log.info('teardown pre')
+        self.log.info('teardown post')
+
+
+class CancelTeardown(Test):
+
+    def setUp(self):
+        self.log.info('setup pre')
+        self.log.info('setup post')
+
+    def test(self):
+        self.log.info('test pre')
+        self.log.info('test post')
+
+    def tearDown(self):
+        self.log.info('teardown pre')
+        self.cancel()
+        self.log.info('teardown post')
+
+
+class FailSetup(Test):
+
+    def setUp(self):
+        self.log.info('setup pre')
+        self.fail()
+        self.log.info('setup post')
+
+    def test(self):
+        self.log.info('test pre')
+        self.log.info('test post')
+
+    def tearDown(self):
+        self.log.info('teardown pre')
+        self.log.info('teardown post')
+
+
+class Fail(Test):
+
+    def setUp(self):
+        self.log.info('setup pre')
+        self.log.info('setup post')
+
+    def test(self):
+        self.log.info('test pre')
+        self.fail()
+        self.log.info('test post')
+
+    def tearDown(self):
+        self.log.info('teardown pre')
+        self.log.info('teardown post')
+
+
+class FailTeardown(Test):
+
+    def setUp(self):
+        self.log.info('setup pre')
+        self.log.info('setup post')
+
+    def test(self):
+        self.log.info('test pre')
+        self.log.info('test post')
+
+    def tearDown(self):
+        self.log.info('teardown pre')
+        self.fail()
+        self.log.info('teardown post')
+
+
+class WarnSetup(Test):
+
+    def setUp(self):
+        self.log.info('setup pre')
+        self.log.warn('')
+        self.log.info('setup post')
+
+    def test(self):
+        self.log.info('test pre')
+        self.log.info('test post')
+
+    def tearDown(self):
+        self.log.info('teardown pre')
+        self.log.info('teardown post')
+
+
+class Warn(Test):
+
+    def setUp(self):
+        self.log.info('setup pre')
+        self.log.info('setup post')
+
+    def test(self):
+        self.log.info('test pre')
+        self.log.warn('')
+        self.log.info('test post')
+
+    def tearDown(self):
+        self.log.info('teardown pre')
+        self.log.info('teardown post')
+
+
+class WarnTeardown(Test):
+
+    def setUp(self):
+        self.log.info('setup pre')
+        self.log.info('setup post')
+
+    def test(self):
+        self.log.info('test pre')
+        self.log.info('test post')
+
+    def tearDown(self):
+        self.log.info('teardown pre')
+        self.log.warn('')
+        self.log.info('teardown post')
+
+
+class ExitSetup(Test):
+
+    def setUp(self):
+        self.log.info('setup pre')
+        sys.exit(-1)
+        self.log.info('setup post')
+
+    def test(self):
+        self.log.info('test pre')
+        self.log.info('test post')
+
+    def tearDown(self):
+        self.log.info('teardown pre')
+        self.log.info('teardown post')
+
+
+class Exit(Test):
+
+    def setUp(self):
+        self.log.info('setup pre')
+        self.log.info('setup post')
+
+    def test(self):
+        self.log.info('test pre')
+        sys.exit(-1)
+        self.log.info('test post')
+
+    def tearDown(self):
+        self.log.info('teardown pre')
+        self.log.info('teardown post')
+
+
+class ExitTeardown(Test):
+
+    def setUp(self):
+        self.log.info('setup pre')
+        self.log.info('setup post')
+
+    def test(self):
+        self.log.info('test pre')
+        self.log.info('test post')
+
+    def tearDown(self):
+        self.log.info('teardown pre')
+        sys.exit(-1)
+        self.log.info('teardown post')
+
+
+class ExceptionSetup(Test):
+
+    def setUp(self):
+        self.log.info('setup pre')
+        raise ValueError
+        self.log.info('setup post')
+
+    def test(self):
+        self.log.info('test pre')
+        self.log.info('test post')
+
+    def tearDown(self):
+        self.log.info('teardown pre')
+        self.log.info('teardown post')
+
+
+class Exception(Test):
+
+    def setUp(self):
+        self.log.info('setup pre')
+        self.log.info('setup post')
+
+    def test(self):
+        self.log.info('test pre')
+        raise ValueError
+        self.log.info('test post')
+
+    def tearDown(self):
+        self.log.info('teardown pre')
+        self.log.info('teardown post')
+
+
+class ExceptionTeardown(Test):
+
+    def setUp(self):
+        self.log.info('setup pre')
+        self.log.info('setup post')
+
+    def test(self):
+        self.log.info('test pre')
+        self.log.info('test post')
+
+    def tearDown(self):
+        self.log.info('teardown pre')
+        raise ValueError
+        self.log.info('teardown post')
+
+
+class Kill(Test):
+
+    def setUp(self):
+        self.log.info('setup pre')
+        self.log.info('setup post')
+
+    def test(self):
+        self.log.info('test pre')
+        os.kill(os.getpid(), 9)
+        self.log.info('test post')
+
+    def tearDown(self):
+        self.log.info('teardown pre')
         self.log.info('teardown post')
