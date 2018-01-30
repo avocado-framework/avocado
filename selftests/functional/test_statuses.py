6e14beb9dd837a690c3251eab8d1187254d9b0f1
Cleber Rosa <crosa@redhat.com>
2018-01-30 04:54:20 -0500

    Test statuses: do not rely on varianter_yaml_to_mux plugin
    Which is an optional plugin, and the test statuses feature is at the
    innermost core of Avocado.  While the original test indeed saved a lot
    of duplicated code, it's code that serves a specific testing purpose,
    and it's better to be simpler and more verbose and with the right (no)
    dependencies.
    
    This issue came up during the Python 3 port work.  Now that all the
    unittests (selftests/unit/*) were passing (locally, there's one other
    PR pending to be accepted), the next logical step is getting the
    "core" functional (selftests/functional/*) tests passing.  Because
    the 'varianter_yaml_to_mux' optional plugins port is not done, the
    "core" functional tests fail and the current work flow is broken.
    
    Signed-off-by: Cleber Rosa <crosa@redhat.com>

--- selftests/functional/test_statuses.py
+++ selftests/functional/test_statuses.py
@@ -20,124 +20,124 @@ ALL_MESSAGES = ['setup pre',
                 'teardown pre',
                 'teardown post']
 
-# Format: {variant1: (status,
-#                     [msg_in, ...])
-#          variant2: (status,
-#                     [msg_in, ...])
-EXPECTED_RESULTS = {'skip-setup-d304': ('SKIP',
-                                        ['setup pre',
-                                         "[WARNING: self.skip() will be "
-                                         "deprecated. Use 'self.cancel()' "
-                                         "or the skip decorators]"]),
-                    'skip-test-914e': ('ERROR',
+# Format: {klass.test_method: (status,
+#                              [msg_in, ...]),
+#          ...}
+EXPECTED_RESULTS = {'SkipSetup.test': ('SKIP',
                                        ['setup pre',
-                                        'setup post',
-                                        'test pre',
-                                        'teardown pre',
-                                        'teardown post',
-                                        "Calling skip() in places other "
-                                        "than setUp() is not allowed in "
-                                        "avocado, you must fix your "
-                                        "test."]),
-                    'skip-teardown-d105': ('ERROR',
-                                           ['setup pre',
-                                            'setup post',
-                                            'test pre',
-                                            'test post',
-                                            'teardown pre',
-                                            "Calling skip() in places other "
-                                            "than setUp() is not allowed in "
-                                            "avocado, you must fix your "
-                                            "test."]),
-                    'cancel-setup-965f': ('CANCEL',
+                                        "[WARNING: self.skip() will be "
+                                        "deprecated. Use 'self.cancel()' "
+                                        "or the skip decorators]"]),
+                    'Skip.test': ('ERROR',
+                                  ['setup pre',
+                                   'setup post',
+                                   'test pre',
+                                   'teardown pre',
+                                   'teardown post',
+                                   "Calling skip() in places other "
+                                   "than setUp() is not allowed in "
+                                   "avocado, you must fix your "
+                                   "test."]),
+                    'SkipTeardown.test': ('ERROR',
                                           ['setup pre',
+                                           'setup post',
+                                           'test pre',
+                                           'test post',
                                            'teardown pre',
-                                           'teardown post']),
-                    'cancel-test-9699': ('CANCEL',
+                                           "Calling skip() in places other "
+                                           "than setUp() is not allowed in "
+                                           "avocado, you must fix your "
+                                           "test."]),
+                    'CancelSetup.test': ('CANCEL',
                                          ['setup pre',
-                                          'setup post',
-                                          'test pre',
                                           'teardown pre',
                                           'teardown post']),
-                    'cancel-teardown-8867': ('CANCEL',
-                                             ['setup pre',
-                                              'setup post',
-                                              'test pre',
-                                              'test post',
-                                              'teardown pre']),
-                    'fail-setup-6c89': ('ERROR',
-                                        ['setup pre',
-                                         'teardown pre',
-                                         'teardown post']),
-                    'fail-test-f361': ('FAIL',
+
+                    'Cancel.test': ('CANCEL',
+                                    ['setup pre',
+                                     'setup post',
+                                     'test pre',
+                                     'teardown pre',
+                                     'teardown post']),
+                    'CancelTeardown.test': ('CANCEL',
+                                            ['setup pre',
+                                             'setup post',
+                                             'test pre',
+                                             'test post',
+                                             'teardown pre']),
+                    'FailSetup.test': ('ERROR',
                                        ['setup pre',
-                                        'setup post',
-                                        'test pre',
                                         'teardown pre',
                                         'teardown post']),
-                    'fail-teardown-a0b0': ('ERROR',
-                                           ['setup pre',
-                                            'setup post',
-                                            'test pre',
-                                            'test post',
-                                            'teardown pre']),
-                    'warn-setup-0730': ('WARN',
-                                        ['setup pre',
-                                         'setup post',
-                                         'test pre',
-                                         'test post',
-                                         'teardown pre',
-                                         'teardown post']),
-                    'warn-test-b0e3': ('WARN',
+                    'Fail.test': ('FAIL',
+                                  ['setup pre',
+                                   'setup post',
+                                   'test pre',
+                                   'teardown pre',
+                                   'teardown post']),
+                    'FailTeardown.test': ('ERROR',
+                                          ['setup pre',
+                                           'setup post',
+                                           'test pre',
+                                           'test post',
+                                           'teardown pre']),
+                    'WarnSetup.test': ('WARN',
                                        ['setup pre',
                                         'setup post',
                                         'test pre',
                                         'test post',
                                         'teardown pre',
                                         'teardown post']),
-                    'warn-teardown-3d38': ('WARN',
-                                           ['setup pre',
-                                            'setup post',
-                                            'test pre',
-                                            'test post',
-                                            'teardown pre',
-                                            'teardown post']),
-                    'exit-setup-0eba': ('ERROR',
-                                        ['setup pre',
-                                         'teardown pre',
-                                         'teardown post']),
-                    'exit-test-f680': ('ERROR',
+                    'Warn.test': ('WARN',
+                                  ['setup pre',
+                                   'setup post',
+                                   'test pre',
+                                   'test post',
+                                   'teardown pre',
+                                   'teardown post']),
+                    'WarnTeardown.test': ('WARN',
+                                          ['setup pre',
+                                           'setup post',
+                                           'test pre',
+                                           'test post',
+                                           'teardown pre',
+                                           'teardown post']),
+                    'ExitSetup.test': ('ERROR',
+                                       ['setup pre',
+                                        'teardown pre',
+                                        'teardown post']),
+                    'Exit.test': ('ERROR',
+                                  ['setup pre',
+                                   'setup post',
+                                   'test pre',
+                                   'teardown pre',
+                                   'teardown post']),
+                    'ExitTeardown.test': ('ERROR',
+                                          ['setup pre',
+                                           'setup post',
+                                           'test pre',
+                                           'test post',
+                                           'teardown pre']),
+                    'ExceptionSetup.test': ('ERROR',
+                                            ['setup pre',
+                                             'teardown pre',
+                                             'teardown post']),
+                    'Exception.test': ('ERROR',
                                        ['setup pre',
                                         'setup post',
                                         'test pre',
                                         'teardown pre',
                                         'teardown post']),
-                    'exit-teardown-6304': ('ERROR',
-                                           ['setup pre',
-                                            'setup post',
-                                            'test pre',
-                                            'test post',
-                                            'teardown pre']),
-                    'exception-setup-37d3': ('ERROR',
-                                             ['setup pre',
-                                              'teardown pre',
-                                              'teardown post']),
-                    'exception-test-8f7d': ('ERROR',
-                                            ['setup pre',
-                                             'setup post',
-                                             'test pre',
-                                             'teardown pre',
-                                             'teardown post']),
-                    'exception-teardown-dfb1': ('ERROR',
-                                                ['setup pre',
-                                                 'setup post',
-                                                 'test pre',
-                                                 'test post',
-                                                 'teardown pre']),
-                    'kill-test-57fe': ('ERROR',
-                                       ['setup pre',
-                                        'setup post',
-                                        'test pre']),
+                    'ExceptionTeardown.test': ('ERROR',
+                                               ['setup pre',
+                                                'setup post',
+                                                'test pre',
+                                                'test post',
+                                                'teardown pre']),
+                    'Kill.test': ('ERROR',
+                                  ['setup pre',
+                                   'setup post',
+                                   'test pre']),
                     }
 
 
@@ -149,14 +149,10 @@ class TestStatuses(unittest.TestCase):
                                                  os.path.pardir,
                                                  ".data",
                                                  'test_statuses.py'))
-        yaml_file = os.path.abspath(os.path.join(os.path.dirname(__file__),
-                                                 os.path.pardir,
-                                                 ".data",
-                                                 'test_statuses.yaml'))
 
         os.chdir(basedir)
-        cmd = ('%s run %s -m %s --sysinfo=off --job-results-dir %s --json -' %
-               (AVOCADO, test_file, yaml_file, self.tmpdir))
+        cmd = ('%s run %s --sysinfo=off --job-results-dir %s --json -' %
+               (AVOCADO, test_file, self.tmpdir))
 
         results = process.run(cmd, ignore_status=True)
         self.results = json.loads(results.stdout)
@@ -166,37 +162,37 @@ class TestStatuses(unittest.TestCase):
 
         # Testing each individual test results
         for test in self.results['tests']:
-            variant = test['id'].split(';')[1]
-            expected = EXPECTED_RESULTS.get(variant, False)
+            klass_method = test['id'].split(':')[1]
+            expected = EXPECTED_RESULTS.get(klass_method, False)
             if not expected:
-                missing_tests.append(variant)
+                missing_tests.append(klass_method)
             else:
                 self._check_test(test, expected)
 
-        # Testing if all variants were covered
+        # Testing if all class/methods were covered
         missing_msg = ' '.join(missing_tests)
         self.assertEqual(missing_msg, '',
-                         "Expected results not found for variants: %s" %
+                         "Expected results not found for class/method: %s" %
                          missing_msg)
 
     def _check_test(self, test, expected):
-        variant = test['id'].split(';')[1]
+        klass_method = test['id'].split(':')[1]
         self.assertEqual(expected[0], test['status'],
                          "Status error: '%s' != '%s' (%s)" %
-                         (expected[0], test['status'], variant))
+                         (expected[0], test['status'], klass_method))
         debug_log = genio.read_file(test['logfile'])
         for msg in expected[1]:
             self.assertIn(msg, debug_log,
                           "Message '%s' should be in the log (%s)."
                           "\nJSON results:\n%s"
                           "\nDebug Log:\n%s" %
-                          (msg, variant, test, debug_log))
+                          (msg, klass_method, test, debug_log))
         for msg in set(ALL_MESSAGES) - set(expected[1]):
             self.assertNotIn(msg, debug_log,
                              "Message '%s' should not be in the log (%s)"
                              "\nJSON results:\n%s"
                              "\nDebug Log:\n%s" %
-                             (msg, variant, test, debug_log))
+                             (msg, klass_method, test, debug_log))
 
     def tearDown(self):
         shutil.rmtree(self.tmpdir)
