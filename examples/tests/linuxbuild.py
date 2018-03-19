52ea978e4fafa3d08463c8a9f18e3e06e53967cc
William Tambe <william.tambe@amd.com>
2018-03-19 17:58:09 -0500

    Extend avocado/avocado/utils/kernel.py to include kernel install.
    Signed-off-by: William Tambe <william.tambe@amd.com>

--- examples/tests/linuxbuild.py
+++ examples/tests/linuxbuild.py
@@ -17,8 +17,10 @@ class LinuxBuildTest(Test):
     """
 
     def setUp(self):
+        kernel_src_url = self.params.get('linux_src_url', default='https://www.kernel.org/pub/linux/kernel/v3.x')
         kernel_version = self.params.get('linux_version', default='3.19.8')
         linux_config = self.params.get('linux_config', default=None)
+        self.do_kernel_install = self.params.get('do_kernel_install', default=None)
         if linux_config is not None:
             linux_config = self.get_data(linux_config)
         if linux_config is None:
@@ -26,14 +28,16 @@ class LinuxBuildTest(Test):
 
         self.linux_build = kernel.KernelBuild(kernel_version,
                                               linux_config,
-                                              self.workdir,
+                                              self.srcdir,
                                               self.cache_dirs)
-        self.linux_build.download()
+        self.linux_build.download(kernel_src_url)
         self.linux_build.uncompress()
         self.linux_build.configure()
 
     def test(self):
-        self.linux_build.build()
+        self.linux_build.build(True if self.do_kernel_install is not None else False)
+        if self.do_kernel_install is not None:
+            self.linux_build.install()
 
 
 if __name__ == "__main__":
