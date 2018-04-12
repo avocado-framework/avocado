be5aa4c6c3ce53cc59e5484097ca01b5f3035758
Cleber Rosa <crosa@redhat.com>
2018-04-11 21:01:10 -0400

    selftests/unit/test_utils_script.py: add test for the script module
    Signed-off-by: Cleber Rosa <crosa@redhat.com>

---
+++ selftests/unit/test_utils_script.py
@@ -0,0 +1,20 @@
+import os
+import unittest
+
+
+from avocado.utils import script
+
+
+class TestTemporary(unittest.TestCase):
+
+    def test_unicode_name(self):
+        path = u'\u00e1 \u00e9 \u00ed \u00f3 \u00fa'
+        content = "a e i o u"
+        with script.TemporaryScript(path, content) as temp_script:
+            self.assertTrue(os.path.exists(temp_script.path))
+            with open(temp_script.path) as temp_script_file:
+                self.assertEqual(content, temp_script_file.read())
+
+
+if __name__ == "__main__":
+    unittest.main()
