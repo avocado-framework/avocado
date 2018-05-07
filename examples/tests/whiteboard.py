94aa1eea9b55cdd0c37bbd66dc552092de4aa43a
Wainer dos Santos Moschetta <wainersm@redhat.com>
2018-05-07 16:50:13 -0400

    Refactor whiteboard example test
    Moved original whiteboard example test to selftests and then
    created a simpler example code.
    
    Reference: https://trello.com/c/sG6J0Vpw/1150-move-whiteboard-test-to-selftests-data-and-create-better-example-whiteboard-test-instead
    Signed-off-by: Wainer dos Santos Moschetta <wainersm@redhat.com>

--- examples/tests/whiteboard.py
+++ examples/tests/whiteboard.py
@@ -2,8 +2,6 @@
 
 import base64
 
-from six.moves import xrange as range
-
 from avocado import Test
 from avocado import main
 
@@ -11,34 +9,14 @@ from avocado import main
 class WhiteBoard(Test):
 
     """
-    Simple test that saves test custom data to the test whiteboard
-
-    :param whiteboard_data_file: File to be used as source for whiteboard data
-    :param whiteboard_data_size: Size of the generated data of the whiteboard
-    :param whiteboard_data_text: Text used when no file supplied
-    :param whiteboard_writes: How many times to copy the data into whiteboard
+    Example of whiteboard usage.
     """
 
     def test(self):
-        data_file = self.params.get('whiteboard_data_file', default='')
-        data_size = self.params.get('whiteboard_data_size', default='10')
-        if data_file:
-            self.log.info('Writing data to whiteboard from file: %s',
-                          data_file)
-            with open(data_file, 'r') as whiteboard_file:
-                size = int(data_size)
-                data = whiteboard_file.read(size)
-        else:
-            offset = int(data_size) - 1
-            data = self.params.get('whiteboard_data_text',
-                                   default='default whiteboard text')[0:offset]
-
-        iterations = int(self.params.get('whiteboard_writes', default=1))
-
-        result = ''
-        for _ in range(0, iterations):
-            result += data
-        self.whiteboard = base64.encodestring(result.encode()).decode('ascii')
+        """
+        This should write a message to the whiteboard.
+        """
+        self.whiteboard = base64.encodestring(b'My message encoded in base64').decode('ascii')
 
 
 if __name__ == "__main__":
---
+++ selftests/.data/whiteboard.py
@@ -0,0 +1,45 @@
+#!/usr/bin/env python
+
+import base64
+
+from six.moves import xrange as range
+
+from avocado import Test
+from avocado import main
+
+
+class WhiteBoard(Test):
+
+    """
+    Simple test that saves test custom data to the test whiteboard
+
+    :param whiteboard_data_file: File to be used as source for whiteboard data
+    :param whiteboard_data_size: Size of the generated data of the whiteboard
+    :param whiteboard_data_text: Text used when no file supplied
+    :param whiteboard_writes: How many times to copy the data into whiteboard
+    """
+
+    def test(self):
+        data_file = self.params.get('whiteboard_data_file', default='')
+        data_size = self.params.get('whiteboard_data_size', default='10')
+        if data_file:
+            self.log.info('Writing data to whiteboard from file: %s',
+                          data_file)
+            with open(data_file, 'r') as whiteboard_file:
+                size = int(data_size)
+                data = whiteboard_file.read(size)
+        else:
+            offset = int(data_size) - 1
+            data = self.params.get('whiteboard_data_text',
+                                   default='default whiteboard text')[0:offset]
+
+        iterations = int(self.params.get('whiteboard_writes', default=1))
+
+        result = ''
+        for _ in range(0, iterations):
+            result += data
+        self.whiteboard = base64.encodestring(result.encode()).decode('ascii')
+
+
+if __name__ == "__main__":
+    main()
