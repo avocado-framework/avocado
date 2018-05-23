2a404e024e41575e02bfa01c0e72bff6bd2d1a56
Harish <harish@linux.vnet.ibm.com>
2018-05-23 11:52:05 +0530

    Added method to get NUMA nodes with memory
    This patch adds method to get list of nodes with memory on given
    machine.
    
    Signed-off-by: Harish <harish@linux.vnet.ibm.com>

---
+++ selftests/unit/test_utils_memory.py
@@ -0,0 +1,24 @@
+import unittest
+
+try:
+    from unittest import mock
+except ImportError:
+    import mock
+
+from avocado.utils import memory
+
+
+class UtilsMemoryTest(unittest.TestCase):
+
+    def test_numa_nodes_with_memory(self):
+        file_values = [u"0\n", u"1-3", u"0-1,12-14\n"]
+        expected_values = [[0], [1, 2, 3], [0, 1, 12, 13, 14]]
+        for value, exp in zip(file_values, expected_values):
+            with mock.patch('os.path.exists', return_value=True):
+                with mock.patch('avocado.utils.genio.read_file', return_value=value):
+                    self.assertEqual(
+                        memory.numa_nodes_with_memory(), exp)
+
+
+if __name__ == '__main__':
+    unittest.main()
