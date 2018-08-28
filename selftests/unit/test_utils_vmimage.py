e98d3c1b8459d5052b101b2e632a908f8e66ce1c
Cleber Rosa <crosa@redhat.com>
2018-08-28 12:58:57 -0400

    avocado.utils.vmimage: differentiate against base and concrete classes
    First of all, a disclaimer: this is hackish.  But, it's the simplest
    and thus best fix I could think of.
    
    What happens here is that this module makes no distintcion between
    what is a base class "abstract" class, and a concrete class.  With the
    goal of reducing code duplication, a goal that will remain valid here
    and in other modules, it made sense to create a common Fedora class.
    
    Now, because only being a ImageProviderBase suffices, the
    FedoraImageProviderBase class, which is not intended to be used as a
    concrete (final) class, is being carried along and its "name" is
    checked, causing crashes.
    
    This introduces the ad-hoc, hackish, notion, that a concrete
    ImageProvider is one that contains a name.
    
    Signed-off-by: Cleber Rosa <crosa@redhat.com>

--- selftests/unit/test_utils_vmimage.py
+++ selftests/unit/test_utils_vmimage.py
@@ -8,6 +8,10 @@ class VMImage(unittest.TestCase):
     def test_list_providers(self):
         self.assertIsNotNone(vmimage.list_providers())
 
+    def test_concrete_providers_have_name(self):
+        for provider in vmimage.list_providers():
+            self.assertTrue(hasattr(provider, 'name'))
+
 
 if __name__ == '__main__':
     unittest.main()
