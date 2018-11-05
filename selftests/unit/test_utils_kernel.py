54ccf507361be9b9e6236645a5ef5e96115bad1e
Caio Carrara <ccarrara@redhat.com>
2018-11-05 13:25:51 -0200

    Add tests for kernel download url building
    The current utils kernel module has no tests. Recently it was added a
    fix[1] to the process of kernel url building. To keep this process
    without future regressions, this change adds a proper test for it.
    
    It's important to note that a new internal method was added to the
    KernelBuild class to make easier to test it. Indirectly it's an
    improvement of download method that now is shorter and more explicit.
    
    [1] - commit c84ac6ba5eb892a54128e42649b9beb53fc1c20e
    
    Signed-off-by: Caio Carrara <ccarrara@redhat.com>

---
+++ selftests/unit/test_utils_kernel.py
@@ -0,0 +1,18 @@
+import unittest
+
+from avocado.utils.kernel import KernelBuild
+
+
+class TestKernelBuild(unittest.TestCase):
+    def setUp(self):
+        self.kernel_version = '4.4.133'
+        self.kernel = KernelBuild(self.kernel_version)
+
+    def test_build_default_url(self):
+        expected_url = 'https://www.kernel.org/pub/linux/kernel/v4.x/linux-4.4.133.tar.gz'
+        self.assertEqual(self.kernel._build_kernel_url(), expected_url)
+
+    def test_build_overrided_url(self):
+        base_url = 'https://mykernel.com/'
+        expected_url = '{}linux-4.4.133.tar.gz'.format(base_url)
+        self.assertEqual(self.kernel._build_kernel_url(base_url=base_url), expected_url)
