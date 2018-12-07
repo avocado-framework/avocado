dc8aba5d67ec8255f2c36af80576a7347c05840b
Caio Carrara <ccarrara@redhat.com>
2018-12-07 11:37:04 -0200

    Adds rate property to Result class
    Currently to generate the HTML report the success rate of a test result
    is computed by the plugin itself. Since this is an information about the
    test result it should be calculated by the Result object and no anywhere
    else.
    
    This change adds the rate property to Result class according with the
    code (method) already in place in HTML plugin. This new property will be
    used soon when the new template engine be added to HTML plugin.
    
    Signed-off-by: Caio Carrara <ccarrara@redhat.com>

--- selftests/unit/test_result.py
+++ selftests/unit/test_result.py
@@ -24,6 +24,46 @@ class ResultTest(unittest.TestCase):
         Result(FakeJob(args))
         self.assertRaises(AttributeError, Result, FakeJobMissingUniqueId(args))
 
+    def test_result_rate_all_succeeded(self):
+        result = Result(FakeJob([]))
+        result.check_test({'status': 'PASS'})
+        result.end_tests()
+        self.assertEquals(result.rate, 100.0)
+
+    def test_result_rate_all_succeeded_with_warns(self):
+        result = Result(FakeJob([]))
+        result.check_test({'status': 'PASS'})
+        result.check_test({'status': 'WARN'})
+        result.end_tests()
+        self.assertEquals(result.rate, 100.0)
+
+    def test_result_rate_all_succeeded_with_skips(self):
+        result = Result(FakeJob([]))
+        result.check_test({'status': 'PASS'})
+        result.check_test({'status': 'SKIP'})
+        result.end_tests()
+        self.assertEquals(result.rate, 100.0)
+
+    def test_result_rate_all_succeeded_with_cancelled(self):
+        result = Result(FakeJob([]))
+        result.check_test({'status': 'PASS'})
+        result.check_test({'status': 'CANCEL'})
+        result.end_tests()
+        self.assertEquals(result.rate, 100.0)
+
+    def test_result_rate_half_succeeded(self):
+        result = Result(FakeJob([]))
+        result.check_test({'status': 'PASS'})
+        result.check_test({'status': 'FAIL'})
+        result.end_tests()
+        self.assertEquals(result.rate, 50.0)
+
+    def test_result_rate_none_succeeded(self):
+        result = Result(FakeJob([]))
+        result.check_test({'status': 'FAIL'})
+        result.end_tests()
+        self.assertEquals(result.rate, 0.0)
+
 
 if __name__ == '__main__':
     unittest.main()
