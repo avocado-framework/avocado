7880d28cc89f58e3d76b0d0f68fb1e33a29fdc80
Caio Carrara <ccarrara@redhat.com>
2018-12-09 21:35:35 -0200

    html: Replaces pystache by jinja to template definition
    Since the pystache project seems to be dead, without recent updates and
    has not support planned for Python 3.7+, this change replace it by jinja
    template engine.
    
    Signed-off-by: Caio Carrara <ccarrara@redhat.com>

--- optional_plugins/html/setup.py
+++ optional_plugins/html/setup.py
@@ -24,7 +24,7 @@ setup(name='avocado-framework-plugin-result-html',
       url='http://avocado-framework.github.io/',
       packages=find_packages(),
       include_package_data=True,
-      install_requires=['avocado-framework', 'pystache'],
+      install_requires=['avocado-framework', 'jinja2'],
       entry_points={
           'avocado.plugins.cli': [
               'html = avocado_result_html:HTML',
