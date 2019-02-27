avocado-framework/avocado

commit 8f8d668b6ed8bc441dd79fcb2f4fb047f26d8018

avocado.core.output: Honor env variable $PAGER for paginators
A number of programs will honor the value of $PAGER if that
is set as the program to use as a paginator, such as git.
Let's make our paginator code to honor it too.

Signed-off-by: Lucas Meneghel Rodrigues <lmr@redhat.com>

avocado/core/output.py
@@ -45,6 +45,10 @@ class ProgressStreamHandler(logging.StreamHandler):
             self.handleError(record)
 
 
+class PagerNotFoundError(Exception):
+    pass
+
+
 class Paginator(object):
 
     """
@@ -54,8 +58,18 @@ class Paginator(object):
     """
 
     def __init__(self):
-        less_cmd = process.find_command('less')
-        self.pipe = os.popen('%s -FRSX' % less_cmd, 'w')
+        try:
+            paginator = "%s -FRSX" % process.find_command('less')
+        except process.CmdNotFoundError:
+            paginator = None
+
+        paginator = os.environ.get('PAGER', paginator)
+        if paginator is None:
+            raise PagerNotFoundError("Could not find a paginator program "
+                                     "('less' not found and env "
+                                     "variable $PAGER not set)")
+
+        self.pipe = os.popen(paginator, 'w')
 
     def __del__(self):
         try:
@@ -74,13 +88,13 @@ def get_paginator():
     """
     Get a paginator. If we can't do that, return stdout.
 
-    The paginator is 'less'. The paginator is a useful feature inspired in
-    programs such as git, since it lets you scroll up and down large buffers
-    of text, increasing the program's usability.
+    The paginator is whatever the user sets as $PAGER, or 'less'. It is a useful
+    feature inspired in programs such as git, since it lets you scroll up and down
+    large buffers of text, increasing the program's usability.
     """
     try:
         return Paginator()
-    except process.CmdNotFoundError:
+    except PagerNotFoundError:
         return sys.stdout
 
 

commit 438ed7d347b438d895f652cce2437c50addbda3c

avocado/core/output.py: remove unused PagerNotFoundError exception
It was introduced in 8f8d668b6, but stoped being used in 6681f2d6d.

Signed-off-by: Cleber Rosa <crosa@redhat.com>

avocado/core/output.py
@@ -518,10 +518,6 @@ class MemStreamHandler(logging.StreamHandler):
         """
 
 
-class PagerNotFoundError(Exception):
-    pass
-
-
 class Paginator(object):
 
     """