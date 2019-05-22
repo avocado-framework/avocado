aa2311c42ee222bca6b00eaa88407d437a660443
Cleber Rosa <crosa@redhat.com>
2019-05-22 16:53:50 -0400

    avocado.utils: introduce data trainer module
    This is a utility module that hopes to make it easier to drain and
    process data that is generated concurrently with other code.
    
    One example is data that is produced by the test, and needs to be
    kept/processed/logged while the test logic is running.
    
    Signed-off-by: Cleber Rosa <crosa@redhat.com>

---
+++ avocado/utils/datadrainer.py
@@ -0,0 +1,105 @@
+# This program is free software; you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation; either version 2 of the License, or
+# (at your option) any later version.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.
+#
+# See LICENSE for more details.
+#
+# Copyright: Red Hat Inc. 2019
+# Author: Cleber Rosa <crosa@redhat.com>
+
+"""
+data drainer
+
+This module provides utility classes for draining data and dispatching
+it to different destinations.  This is intended to be used
+concurrenly with other code, usually test code producing the output
+to be drained/processed.  A thread is started and maintained on behalf
+of the user.
+"""
+
+import threading
+
+
+class BaseDrainer:
+    """
+    Base drainer, doesn't provide complete functionality to be useful.
+    """
+
+    name = 'avocado.utils.datadrainer.BaseDrainer'
+
+    def __init__(self, source, stop_check=None, name=None):
+        """
+        :param source: where to read data from, this is intentionally
+                       abstract
+        :param stop_check: callable that should determine if the
+                           drainer should quit.  If None is given, it
+                           will never stop.
+        :type stop_check: function
+        :param name: instance name of the drainer, used for describing
+                     the name of the thread maintained by this instance
+        :type name: str
+        """
+        self._source = source
+        if stop_check is None:
+            def stop_check():   # pylint: disable=E0102
+                return False
+        self._stop_check = stop_check
+        if name is not None:
+            self.name = name
+        # internal state flag, used to stop processing because of a
+        # condition that may have happened in between the loop cycles
+        self._internal_quit = False
+
+    def data_available(self):
+        """
+        Checks if source appears to have data to be drained
+        """
+        return False
+
+    def read(self):
+        """
+        Abstract method supposed to read from the data source
+        """
+        raise NotImplementedError
+
+    def write(self, data):
+        """
+        Abstract method supposed to write the read data to its destination
+        """
+        raise NotImplementedError
+
+    def _loop(self):
+        """
+        Basic implementation of the thread target
+
+        This loops until either an internal quit flag is set, or when
+        the stop_check function evaluates to True.
+        """
+        while True:
+            if self._internal_quit:
+                break
+            if self.data_available():
+                self.write(self.read())
+            if self._stop_check():
+                break
+
+    def start(self):
+        """
+        Starts a thread to do the data draining
+        """
+        self._thread = threading.Thread(target=self._loop,
+                                        name=self.name)
+
+        self._thread.daemon = True
+        self._thread.start()
+
+    def wait(self):
+        """
+        Waits on the thread completion
+        """
+        self._thread.join()
