d54f4ba9a77852c80e29b9ed5a0a810eb12674bc
Cleber Rosa <crosa@redhat.com>
2019-06-20 17:43:53 -0400

    examples/tests/gendata.py: drop "bsod" example tests
    This is a really bad example of a test, in the sense that it requires
    a huge library just to draw an image.
    
    Let's drop it, given that the remaining example works just as well.
    
    Signed-off-by: Cleber Rosa <crosa@redhat.com>

--- examples/tests/gendata.py
+++ examples/tests/gendata.py
@@ -4,7 +4,6 @@ import os
 
 from avocado import Test
 from avocado import main
-from avocado.utils import genio
 
 
 class GenDataTest(Test):
@@ -12,27 +11,6 @@ class GenDataTest(Test):
     """
     Simple test that generates data to be persisted after the test is run
     """
-
-    def test_bsod(self):
-        try:
-            from PIL import Image
-            from PIL import ImageDraw
-        except ImportError:
-            self.cancel("PIL not available")
-
-        text = ["DREADED BLUE SCREEN OF DEATH"]
-        dmesg_path = os.path.join(self.job.logdir, "sysinfo", "pre", "dmesg_-c")
-        self.log.info("dmesg_path: %s", dmesg_path)
-        if os.path.exists(dmesg_path):
-            text = genio.read_all_lines(dmesg_path)[0:50]
-
-        bsod = Image.new("RGB", (640, 480), "blue")
-        draw = ImageDraw.Draw(bsod)
-        y = 2
-        for line in text:
-            draw.text((2, y), line)
-            y += 12
-        bsod.save(os.path.join(self.outputdir, "bsod.png"))
 
     def test_json(self):
         import json
