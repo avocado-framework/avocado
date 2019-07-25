51954217ff716956f249340b8345162e7e1b6da5
Cleber Rosa <crosa@redhat.com>
2019-07-24 21:26:34 -0400

    Job: provide examples of API usage
    The APIs under avocado.core are intended to be private.  These
    examples are a compromise in an attempt to mature those APIs over the
    next few releases.
    
    Once a satisfactory maturity level is reached, these APIs will be
    declared stable.
    
    Signed-off-by: Cleber Rosa <crosa@redhat.com>

---
+++ examples/jobs/failjob.py
@@ -0,0 +1,9 @@
+#!/usr/bin/env python3
+
+import sys
+from avocado.core.job import Job
+
+config = {'references': ['examples/tests/failtest.py:FailTest.test']}
+
+with Job(config) as j:
+    sys.exit(j.run())
