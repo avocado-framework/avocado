avocado-framework/avocado

commit 48826017ef033915aba2de2c79e6e47923efd4e9

nrunner: support complex runnable arguments via base64 encoded args
Sometimes the arguments to be passed to runnables can not be given on
the command line as simple strings.  Even simple strings can't always
be used because they'll be parsed by the runner command line parser
itself.

To allow for more flexible argument passing, this allows a escaping
prefix "base64:" which will be decoded before being passed into the
Runnable constructor.

One of the functional tests checks that behavior, while the other
serves to make the point that no encoding is necessary in recipe files
as they are JSON and can hold natively the content we need so far.

Signed-off-by: Cleber Rosa <crosa@redhat.com>

avocado/core/nrunner.py
@@ -251,10 +251,27 @@ CMD_RUNNABLE_RUN_ARGS = (
     )
 
 
+def _arg_decode_base64(arg):
+    """
+    Decode arguments possibly endcoded as base64
+
+    :param arg: the possibly encoded argument
+    :type arg: str
+    :returns: the decoded argument
+    :rtype: str
+    """
+    prefix = 'base64:'
+    if arg.startswith(prefix):
+        content = arg[len(prefix):]
+        return base64.decodebytes(content.encode()).decode()
+    return arg
+
+
 def runnable_from_args(args):
+    decoded_args = [_arg_decode_base64(arg) for arg in args.get('arg', ())]
     return Runnable(args.get('kind'),
                     args.get('uri'),
-                    *args.get('arg', ()))
+                    *decoded_args)
 
 
 def subcommand_runnable_run(args, echo=print):

selftests/functional/test_nrunner.py
@@ -1,5 +1,6 @@
-import unittest
 import os
+import sys
+import unittest
 
 from .. import AVOCADO, BASEDIR
 
@@ -13,6 +14,48 @@ class RunnableRun(unittest.TestCase):
         res = process.run("%s runnable-run -k noop" % AVOCADO,
                           ignore_status=True)
         self.assertEqual(res.stdout, b"{'status': 'finished'}\n")
+        self.assertEqual(res.exit_status, exit_codes.AVOCADO_ALL_OK)
+
+    def test_exec(self):
+        # 'base64:LWM=' becomes '-c' and makes Python execute the
+        # commands on the subsequent argument
+        cmd = ("%s runnable-run -k exec -u %s -a 'base64:LWM=' -a "
+               "'import sys; sys.exit(99)'" % (AVOCADO, sys.executable))
+        res = process.run(cmd, ignore_status=True)
+        self.assertIn(b"'status': 'finished'", res.stdout)
+        self.assertIn(b"'returncode': 99", res.stdout)
+        self.assertEqual(res.exit_status, exit_codes.AVOCADO_ALL_OK)
+
+    @unittest.skipUnless(os.path.exists('/bin/echo'),
+                         ('Executable "/bin/echo" used in test is not '
+                          'available in the system'))
+    def test_exec_echo(self):
+        # 'base64:LW4=' becomes '-n' and prevents echo from printing a newline
+        cmd = ("%s runnable-run -k exec -u /bin/echo -a 'base64:LW4=' -a "
+               "_Avocado_Runner_" % AVOCADO)
+        res = process.run(cmd, ignore_status=True)
+        self.assertIn(b"'status': 'finished'", res.stdout)
+        self.assertIn(b"'stdout': b'_Avocado_Runner_'", res.stdout)
+        self.assertIn(b"'returncode': 0", res.stdout)
+        self.assertEqual(res.exit_status, exit_codes.AVOCADO_ALL_OK)
+
+    @unittest.skipUnless(os.path.exists('/bin/echo'),
+                         ('Executable "/bin/echo" used in recipe is not '
+                          'available in the system'))
+    def test_recipe(self):
+        recipe = os.path.join(BASEDIR, "examples", "recipes", "runnables",
+                              "exec_echo_no_newline.json")
+        cmd = "%s runnable-run-recipe %s" % (AVOCADO, recipe)
+        res = process.run(cmd, ignore_status=True)
+        lines = res.stdout_text.splitlines()
+        if len(lines) == 1:
+            first_status = final_status = lines[0]
+        else:
+            first_status = lines[0]
+            final_status = lines[-1]
+            self.assertIn("'status': 'running'", first_status)
+        self.assertIn("'status': 'finished'", final_status)
+        self.assertIn("'stdout': b'avocado'", final_status)
         self.assertEqual(res.exit_status, exit_codes.AVOCADO_ALL_OK)
 
 

commit 3fc528a3757b9d4f991980f312370e0597aea447

nrunner: escape command line arguments that stat with a dash
Because argparser will attempt to parse arguments that start with a
dash as yet another option, instead of the value to the '-a' option,
we need to escape them.

This is a complement to 48826017e, which introduced this concept and
the decoding of base64 arguments, but failed to encode the necessary
ones when creating a list of arguments.

Signed-off-by: Cleber Rosa <crosa@redhat.com>

avocado/core/nrunner.py
@@ -79,6 +79,8 @@ class Runnable:
 
         for arg in self.args:
             args.append('-a')
+            if arg.startswith('-'):
+                arg = 'base64:%s' % base64.b64encode(arg.encode()).decode('ascii')
             args.append(arg)
 
         if self.tags is not None: