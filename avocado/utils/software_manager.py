3d9a9833ff2e72a76e6dbbb36eda4f14307f274c
Wainer dos Santos Moschetta <wainersm@redhat.com>
2020-01-31 17:14:28 -0300

    utils/software_manager: Remove the __main__ entrypoint
    Currently utils/software_manager.py is a Python script with
    __main__, although the file's executable bit is not set. This
    removes the __main__ section and counterpart main() so that
    the file becomes a solely module.
    
    Signed-off-by: Wainer dos Santos Moschetta <wainersm@redhat.com>

--- avocado/utils/software_manager.py
+++ avocado/utils/software_manager.py
@@ -38,7 +38,6 @@ import os
 import re
 import shutil
 import logging
-import argparse
 import tempfile
 import configparser
 
@@ -1228,77 +1227,3 @@ def install_distro_packages(distro_pkg_map, interactive=False):
                   detected_distro.name, detected_distro.arch,
                   detected_distro.version, detected_distro.release)
     return result
-
-
-def main():
-    parser = argparse.ArgumentParser(
-        "usage: %prog [install|remove|check-installed|list-all|list-files|add-repo|"
-        "remove-repo| upgrade|what-provides|install-what-provides] arguments")
-    parser.add_argument('--verbose', dest="debug", action='store_true',
-                        help='include debug messages in console output')
-
-    _, args = parser.parse_known_args()
-    software_manager = SoftwareManager()
-    if args:
-        action = args[0]
-        args = " ".join(args[1:])
-    else:
-        action = 'show-help'
-
-    if action == 'install':
-        if software_manager.install(args):
-            log.info("Packages %s installed successfully", args)
-        else:
-            log.error("Failed to install %s", args)
-
-    elif action == 'remove':
-        if software_manager.remove(args):
-            log.info("Packages %s removed successfully", args)
-        else:
-            log.error("Failed to remove %s", args)
-
-    elif action == 'check-installed':
-        if software_manager.check_installed(args):
-            log.info("Package %s already installed", args)
-        else:
-            log.info("Package %s not installed", args)
-
-    elif action == 'list-all':
-        for pkg in software_manager.list_all():
-            log.info(pkg)
-
-    elif action == 'list-files':
-        for f in software_manager.list_files(args):
-            log.info(f)
-
-    elif action == 'add-repo':
-        if software_manager.add_repo(args):
-            log.info("Repo %s added successfully", args)
-        else:
-            log.error("Failed to remove repo %s", args)
-
-    elif action == 'remove-repo':
-        if software_manager.remove_repo(args):
-            log.info("Repo %s removed successfully", args)
-        else:
-            log.error("Failed to remove repo %s", args)
-
-    elif action == 'upgrade':
-        if software_manager.upgrade():
-            log.info("Package manager upgrade successful")
-
-    elif action == 'what-provides':
-        provides = software_manager.provides(args)
-        if provides is not None:
-            log.info("Package %s provides %s", provides, args)
-
-    elif action == 'install-what-provides':
-        if software_manager.install_what_provides(args):
-            log.info("Installed successfully what provides %s", args)
-
-    elif action == 'show-help':
-        parser.print_help()
-
-
-if __name__ == '__main__':
-    main()
