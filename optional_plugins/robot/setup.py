ba439332d5dbe3ea112d8691ffb8465c519cd742
Cleber Rosa <crosa@redhat.com>
2020-04-13 21:38:19 -0400

    robot plugin: avoid robotframework >= 3.2
    Because it brings a new and incompatible API.
    
    Signed-off-by: Cleber Rosa <crosa@redhat.com>

--- optional_plugins/robot/setup.py
+++ optional_plugins/robot/setup.py
@@ -24,7 +24,7 @@ setup(name='avocado-framework-plugin-robot',
       url='http://avocado-framework.github.io/',
       packages=find_packages(),
       include_package_data=True,
-      install_requires=['robotframework'],
+      install_requires=['robotframework<3.2'],
       entry_points={
           'avocado.plugins.cli': [
               'robot = avocado_robot:RobotCLI',
