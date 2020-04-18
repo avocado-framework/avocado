d4658bca1ac8f1089729193dcc890938317a8c79
Cleber Rosa <crosa@redhat.com>
2020-04-18 11:52:21 -0400

    selftests/unit/test_utils_memory.py: simple variable rename
    While the current name is very descriptive (which is positive), IMO
    the methods are so simple that don't need such a detailed level that
    wins over having huge lines that break the <=79 columns convention.
    
    Signed-off-by: Cleber Rosa <crosa@redhat.com>

--- selftests/unit/test_utils_memory.py
+++ selftests/unit/test_utils_memory.py
@@ -26,80 +26,80 @@ BUDDY_INFO_RESPONSE = '\n'.join([
                      return_value=BUDDY_INFO_RESPONSE)
 class GetBuddyInfo(unittest.TestCase):
 
-    def test_simple_chunk_size(self, buddy_info_content_mocked):
+    def test_simple_chunk_size(self, buddy_mocked):
         chunk_size = '0'
         result = memory.get_buddy_info(chunk_size)
         self.assertEqual(result[chunk_size], 6418)
-        self.assertTrue(buddy_info_content_mocked.called)
+        self.assertTrue(buddy_mocked.called)
 
-    def test_less_than_chunk_size(self, buddy_info_content_mocked):
+    def test_less_than_chunk_size(self, buddy_mocked):
         chunk_size = '<2'
         result = memory.get_buddy_info(chunk_size)
         self.assertEqual(result['0'], 6418)
         self.assertEqual(result['1'], 10439)
-        self.assertTrue(buddy_info_content_mocked.called)
+        self.assertTrue(buddy_mocked.called)
 
-    def test_less_than_equal_chunk_size(self, buddy_info_content_mocked):
+    def test_less_than_equal_chunk_size(self, buddy_mocked):
         chunk_size = '<=2'
         result = memory.get_buddy_info(chunk_size)
         self.assertEqual(result['0'], 6418)
         self.assertEqual(result['1'], 10439)
         self.assertEqual(result['2'], 10048)
-        self.assertTrue(buddy_info_content_mocked.called)
+        self.assertTrue(buddy_mocked.called)
 
-    def test_greater_than_chunk_size(self, buddy_info_content_mocked):
+    def test_greater_than_chunk_size(self, buddy_mocked):
         chunk_size = '>3'
         result = memory.get_buddy_info(chunk_size)
         self.assertEqual(result['4'], 19278)
         self.assertEqual(result['5'], 10357)
-        self.assertTrue(buddy_info_content_mocked.called)
+        self.assertTrue(buddy_mocked.called)
 
-    def test_greater_than_equal_chunk_size(self, buddy_info_content_mocked):
+    def test_greater_than_equal_chunk_size(self, buddy_mocked):
         chunk_size = '>=3'
         result = memory.get_buddy_info(chunk_size)
         self.assertEqual(result['3'], 12819)
         self.assertEqual(result['4'], 19278)
         self.assertEqual(result['5'], 10357)
-        self.assertTrue(buddy_info_content_mocked.called)
+        self.assertTrue(buddy_mocked.called)
 
-    def test_multiple_chunk_size(self, buddy_info_content_mocked):
+    def test_multiple_chunk_size(self, buddy_mocked):
         chunk_size = '2 4'
         result = memory.get_buddy_info(chunk_size)
         self.assertEqual(result['2'], 10048)
         self.assertEqual(result['4'], 19278)
-        self.assertTrue(buddy_info_content_mocked.called)
+        self.assertTrue(buddy_mocked.called)
 
-    def test_multiple_chunk_size_filtering_simple(self, buddy_info_content_mocked):
+    def test_multiple_chunk_size_filtering_simple(self, buddy_mocked):
         chunk_size = '>2 <4'
         result = memory.get_buddy_info(chunk_size)
         self.assertEqual(result['3'], 12819)
-        self.assertTrue(buddy_info_content_mocked.called)
+        self.assertTrue(buddy_mocked.called)
 
-    def test_multiple_chunk_size_filtering(self, buddy_info_content_mocked):
+    def test_multiple_chunk_size_filtering(self, buddy_mocked):
         chunk_size = '>=2 <=4'
         result = memory.get_buddy_info(chunk_size)
         self.assertEqual(result['2'], 10048)
         self.assertEqual(result['3'], 12819)
         self.assertEqual(result['4'], 19278)
-        self.assertTrue(buddy_info_content_mocked.called)
+        self.assertTrue(buddy_mocked.called)
 
-    def test_multiple_chunk_size_filtering_invalid(self, buddy_info_content_mocked):
+    def test_multiple_chunk_size_filtering_invalid(self, buddy_mocked):
         chunk_size = '>2 <2'
         result = memory.get_buddy_info(chunk_size)
         self.assertEqual(result, {})
-        self.assertTrue(buddy_info_content_mocked.called)
+        self.assertTrue(buddy_mocked.called)
 
-    def test_filtering_node(self, buddy_info_content_mocked):
+    def test_filtering_node(self, buddy_mocked):
         chunk_size = '0'
         result = memory.get_buddy_info(chunk_size, nodes='1')
         self.assertEqual(result[chunk_size], 5430)
-        self.assertTrue(buddy_info_content_mocked.called)
+        self.assertTrue(buddy_mocked.called)
 
-    def test_filtering_zone(self, buddy_info_content_mocked):
+    def test_filtering_zone(self, buddy_mocked):
         chunk_size = '0'
         result = memory.get_buddy_info(chunk_size, zones='DMA32')
         self.assertEqual(result[chunk_size], 987)
-        self.assertTrue(buddy_info_content_mocked.called)
+        self.assertTrue(buddy_mocked.called)
 
 
 if __name__ == '__main__':
