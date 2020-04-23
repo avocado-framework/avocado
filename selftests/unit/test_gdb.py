6fda44bca2dbc8c4072910500ed71bbaea1925a2
Cleber Rosa <crosa@redhat.com>
2020-04-23 19:30:58 -0400

    GDB: move functions to GDBRemote static methods
    So that they're better organized under the class that actually makes
    use of them.
    
    Signed-off-by: Cleber Rosa <crosa@redhat.com>

--- selftests/unit/test_gdb.py
+++ selftests/unit/test_gdb.py
@@ -1,34 +1,35 @@
 import unittest
 
-from avocado.utils import gdb
+from avocado.utils.gdb import GDBRemote
+from avocado.utils.gdb import InvalidPacketError
 
 
 class GDBRemoteTest(unittest.TestCase):
 
     def test_checksum(self):
-        self.assertEqual(gdb.remote_checksum(b'!'), b'21')
-        self.assertEqual(gdb.remote_checksum(b'OK'), b'9a')
-        self.assertEqual(gdb.remote_checksum(b'foo'), b'44')
+        self.assertEqual(GDBRemote.checksum(b'!'), b'21')
+        self.assertEqual(GDBRemote.checksum(b'OK'), b'9a')
+        self.assertEqual(GDBRemote.checksum(b'foo'), b'44')
 
     def test_encode_command(self):
-        self.assertEqual(gdb.remote_encode(b'!'), b'$!#21')
-        self.assertEqual(gdb.remote_encode(b'OK'), b'$OK#9a')
-        self.assertEqual(gdb.remote_encode(b'foo'), b'$foo#44')
+        self.assertEqual(GDBRemote.encode(b'!'), b'$!#21')
+        self.assertEqual(GDBRemote.encode(b'OK'), b'$OK#9a')
+        self.assertEqual(GDBRemote.encode(b'foo'), b'$foo#44')
 
     def test_decode_response(self):
-        self.assertEqual(gdb.remote_decode(b'$!#21'), b'!')
-        self.assertEqual(gdb.remote_decode(b'$OK#9a'), b'OK')
-        self.assertEqual(gdb.remote_decode(b'$foo#44'), b'foo')
+        self.assertEqual(GDBRemote.decode(b'$!#21'), b'!')
+        self.assertEqual(GDBRemote.decode(b'$OK#9a'), b'OK')
+        self.assertEqual(GDBRemote.decode(b'$foo#44'), b'foo')
 
     def test_decode_invalid(self):
-        with self.assertRaises(gdb.InvalidPacketError):
-            gdb.remote_decode(b'$!#22')
-        with self.assertRaises(gdb.InvalidPacketError):
-            gdb.remote_decode(b'$foo$bar#21')
-        with self.assertRaises(gdb.InvalidPacketError):
-            gdb.remote_decode(b'!!#21')
-        with self.assertRaises(gdb.InvalidPacketError):
-            gdb.remote_decode(b'+$!#21')
+        with self.assertRaises(InvalidPacketError):
+            GDBRemote.decode(b'$!#22')
+        with self.assertRaises(InvalidPacketError):
+            GDBRemote.decode(b'$foo$bar#21')
+        with self.assertRaises(InvalidPacketError):
+            GDBRemote.decode(b'!!#21')
+        with self.assertRaises(InvalidPacketError):
+            GDBRemote.decode(b'+$!#21')
 
 
 if __name__ == '__main__':
