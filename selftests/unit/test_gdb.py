2ee9831c019294989b2813377964c068a5c0148a
Cleber Rosa <crosa@redhat.com>
2020-04-23 18:00:47 -0400

    GDB: convert remote_checksum to use bytes
    The GDB Remote Serial Protocol is a not intended to support multibyte
    strings, so it's much more honest to have this Python based
    implementation of a client to use bytes natively.  Let's start with
    the checksum calculation.
    
    Signed-off-by: Cleber Rosa <crosa@redhat.com>

--- selftests/unit/test_gdb.py
+++ selftests/unit/test_gdb.py
@@ -6,9 +6,9 @@ from avocado.utils import gdb
 class GDBRemoteTest(unittest.TestCase):
 
     def test_checksum(self):
-        self.assertEqual(gdb.remote_checksum('!'), '21')
-        self.assertEqual(gdb.remote_checksum('OK'), '9a')
-        self.assertEqual(gdb.remote_checksum('foo'), '44')
+        self.assertEqual(gdb.remote_checksum(b'!'), b'21')
+        self.assertEqual(gdb.remote_checksum(b'OK'), b'9a')
+        self.assertEqual(gdb.remote_checksum(b'foo'), b'44')
 
     def test_encode_command(self):
         self.assertEqual(gdb.remote_encode('!'), '$!#21')
