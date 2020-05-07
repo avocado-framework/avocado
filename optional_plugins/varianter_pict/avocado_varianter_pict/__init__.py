4856705575db8009552434b65dcaf8d819edd833
Beraldo Leal <bleal@redhat.com>
2020-05-07 08:58:56 -0300

    optional_plugins/varianter_pict: Just applying isort
    Signed-off-by: Beraldo Leal <bleal@redhat.com>

--- optional_plugins/varianter_pict/avocado_varianter_pict/__init__.py
+++ optional_plugins/varianter_pict/avocado_varianter_pict/__init__.py
@@ -19,8 +19,7 @@ import sys
 
 from avocado.core import exit_codes
 from avocado.core.output import LOG_UI
-from avocado.core.plugin_interfaces import CLI
-from avocado.core.plugin_interfaces import Varianter
+from avocado.core.plugin_interfaces import CLI, Varianter
 from avocado.core.tree import TreeNode
 from avocado.utils import path as utils_path
 from avocado.utils import process
