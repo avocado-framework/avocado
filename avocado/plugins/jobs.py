3e93641673eb1f6316111a627f6b8e5483c361e3
Beraldo Leal <bleal@redhat.com>
2020-05-14 16:16:46 -0300

    Implemented the 'avocado jobs show' subcommand
    This is just an idea of a possible detailed view of a job and can be
    improved soon.
    
    Signed-off-by: Beraldo Leal <bleal@redhat.com>

--- avocado/plugins/jobs.py
+++ avocado/plugins/jobs.py
@@ -23,6 +23,7 @@ from glob import glob
 
 from avocado.core import exit_codes
 from avocado.core.data_dir import get_logs_dir
+from avocado.core.future.settings import settings
 from avocado.core.plugin_interfaces import CLICmd
 
 
@@ -32,6 +33,28 @@ class Jobs(CLICmd):
     """
     name = 'jobs'
     description = 'Manage Avocado jobs'
+
+    def _get_data_from_file(self, filename):
+        if not filename or not os.path.isfile(filename):
+            raise FileNotFoundError('File not found {}'.format(filename))
+
+        with open(filename, 'r') as fp:
+            return json.load(fp)
+
+    def _print_job_details(self, details):
+        for key, value in details.items():
+            print("{:15s}: {}".format(key, value))
+
+    def _print_job_tests(self, tests):
+        print("\nTests:")
+        date_fmt = "%Y/%m/%d %H:%M:%S"
+        print(" Status  End Time              Run Time  Test ID")
+        for test in tests:
+            end = datetime.fromtimestamp(test.get('end'))
+            print(" {:7s} {:20s}  {:.5f}   {}".format(test.get('status'),
+                                                      end.strftime(date_fmt),
+                                                      float(test.get('time')),
+                                                      test.get('id')))
 
     def configure(self, parser):
         """
@@ -47,6 +70,16 @@ class Jobs(CLICmd):
 
         help_msg = 'List all known jobs by Avocado'
         subcommands.add_parser('list', help=help_msg)
+
+        help_msg = 'Show details about a specific job'
+        show_parser = subcommands.add_parser('show', help=help_msg)
+        settings.register_option(section='jobs.show',
+                                 key='job_id',
+                                 help_msg='JOB id',
+                                 metavar='JOBID',
+                                 default=None,
+                                 positional_arg=True,
+                                 parser=show_parser)
 
     def handle_list_command(self, jobs_results):
         """Called when 'avocado jobs list' command is executed."""
@@ -71,6 +104,38 @@ class Jobs(CLICmd):
 
         return exit_codes.AVOCADO_ALL_OK
 
+    def handle_show_command(self, config, jobs_results):
+        """Called when 'avocado jobs show' command is executed."""
+
+        job_id = config.get('jobs.show.job_id')
+        results_file = jobs_results.get(job_id)
+        config_file = os.path.join(os.path.dirname(results_file),
+                                   'jobdata/args.json')
+        try:
+            results_data = self._get_data_from_file(results_file)
+        except FileNotFoundError as ex:
+            # Results data are important and should exit if not found
+            print(ex)
+            return exit_codes.AVOCADO_GENERIC_CRASH
+
+        try:
+            config_data = self._get_data_from_file(config_file)
+        except FileNotFoundError:
+            pass
+
+        data = {'Job id': job_id,
+                'Debug log': results_data.get('debuglog'),
+                'Spawner': config_data.get('nrun.spawner', 'unknown'),
+                '#total tests': results_data.get('total'),
+                '#pass tests': results_data.get('pass'),
+                '#skip tests': results_data.get('skip'),
+                '#errors tests': results_data.get('errors'),
+                '#cancel tests': results_data.get('cancel')}
+
+        # We could improve this soon with more data and colors
+        self._print_job_details(data)
+        self._print_job_tests(results_data.get('tests'))
+
     def run(self, config):
         results = {}
 
@@ -83,4 +148,6 @@ class Jobs(CLICmd):
         subcommand = config.get('jobs_subcommand')
         if subcommand == 'list':
             self.handle_list_command(results)
+        elif subcommand == 'show':
+            self.handle_show_command(config, results)
         return exit_codes.AVOCADO_ALL_OK
