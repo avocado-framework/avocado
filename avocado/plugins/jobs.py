6d0b96bb81b6747e9972ae73463405e392b8007c
Beraldo Leal <bleal@redhat.com>
2020-05-14 16:16:46 -0300

    Implemented the 'avocado jobs show' subcommand
    This is just an idea of a possible detailed view of a job and can be
    improved soon.
    
    Signed-off-by: Beraldo Leal <bleal@redhat.com>

--- avocado/plugins/jobs.py
+++ avocado/plugins/jobs.py
@@ -22,8 +22,9 @@ from datetime import datetime
 from glob import glob
 
 from avocado.core import exit_codes
-from avocado.core.data_dir import get_logs_dir
 from avocado.core.output import LOG_UI
+from avocado.core.data_dir import get_job_results_dir, get_logs_dir
+from avocado.core.future.settings import settings
 from avocado.core.plugin_interfaces import CLICmd
 
 
@@ -33,6 +34,33 @@ class Jobs(CLICmd):
     """
     name = 'jobs'
     description = 'Manage Avocado jobs'
+
+    def _get_data_from_file(self, filename):
+        if not filename or not os.path.isfile(filename):
+            raise FileNotFoundError('File not found {}'.format(filename))
+
+        with open(filename, 'r') as fp:
+            return json.load(fp)
+
+    def _print_job_details(self, details):
+        for key, value in details.items():
+            LOG_UI.info("%-15s: %s", key, value)
+
+    def _print_job_tests(self, tests):
+        LOG_UI.info("\nTests:\n")
+        date_fmt = "%Y/%m/%d %H:%M:%S"
+        LOG_UI.info(" Status  End Time              Run Time   Test ID")
+        for test in tests:
+            end = datetime.fromtimestamp(test.get('end'))
+            status = test.get('status')
+            method = LOG_UI.info
+            if status in ['ERROR', 'FAIL']:
+                method = LOG_UI.error
+            method(" %-7s %-20s  %.5f    %s",
+                   status,
+                   end.strftime(date_fmt),
+                   float(test.get('time')),
+                   test.get('id'))
 
     def configure(self, parser):
         """
@@ -49,6 +77,19 @@ class Jobs(CLICmd):
 
         help_msg = 'List all known jobs by Avocado'
         subcommands.add_parser('list', help=help_msg)
+
+        help_msg = ('Show details about a specific job. When passing a Job '
+                    'ID, you can use any Job Reference (job_id, "latest", '
+                    'or job results path).')
+        show_parser = subcommands.add_parser('show', help=help_msg)
+        settings.register_option(section='jobs.show',
+                                 key='job_id',
+                                 help_msg='JOB id',
+                                 metavar='JOBID',
+                                 default='latest',
+                                 nargs='?',
+                                 positional_arg=True,
+                                 parser=show_parser)
 
     def handle_list_command(self, jobs_results):
         """Called when 'avocado jobs list' command is executed."""
@@ -72,6 +113,43 @@ class Jobs(CLICmd):
 
         return exit_codes.AVOCADO_ALL_OK
 
+    def handle_show_command(self, config):
+        """Called when 'avocado jobs show' command is executed."""
+
+        job_id = config.get('jobs.show.job_id')
+        results_dir = get_job_results_dir(job_id)
+        if results_dir is None:
+            LOG_UI.error("Error: Job %s not found", job_id)
+            return exit_codes.AVOCADO_GENERIC_CRASH
+
+        results_file = os.path.join(results_dir, 'results.json')
+        config_file = os.path.join(results_dir, 'jobdata/args.json')
+        try:
+            results_data = self._get_data_from_file(results_file)
+        except FileNotFoundError as ex:
+            # Results data are important and should exit if not found
+            LOG_UI.error(ex)
+            return exit_codes.AVOCADO_GENERIC_CRASH
+
+        try:
+            config_data = self._get_data_from_file(config_file)
+        except FileNotFoundError:
+            pass
+
+        data = {'Job id': job_id,
+                'Debug log': results_data.get('debuglog'),
+                'Spawner': config_data.get('nrun.spawner', 'unknown'),
+                '#total tests': results_data.get('total'),
+                '#pass tests': results_data.get('pass'),
+                '#skip tests': results_data.get('skip'),
+                '#errors tests': results_data.get('errors'),
+                '#cancel tests': results_data.get('cancel')}
+
+        # We could improve this soon with more data and colors
+        self._print_job_details(data)
+        self._print_job_tests(results_data.get('tests'))
+        return exit_codes.AVOCADO_ALL_OK
+
     def run(self, config):
         results = {}
 
@@ -84,4 +162,6 @@ class Jobs(CLICmd):
         subcommand = config.get('jobs_subcommand')
         if subcommand == 'list':
             return self.handle_list_command(results)
+        elif subcommand == 'show':
+            return self.handle_show_command(config)
         return exit_codes.AVOCADO_ALL_OK
