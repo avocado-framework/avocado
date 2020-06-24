be278fe507eccb3d8d7a536448f9d0c71228e435
Narasimhan V <sim@linux.vnet.ibm.com>
2020-06-24 17:54:16 +0530

    Adding loop device APIs
    Adding APIs to create and delete loop devices.
    
    Signed-off-by: Narasimhan V <sim@linux.vnet.ibm.com>

--- avocado/utils/disk.py
+++ avocado/utils/disk.py
@@ -15,6 +15,7 @@
 #
 # Copyright: 2018 IBM
 # Authors : Praveen K Pandey <praveen@linux.vnet.ibm.com>
+#           Narasimhan V <sim@linux.vnet.ibm.com>
 
 
 """
@@ -29,6 +30,12 @@ import re
 from . import process
 
 
+class DiskError(Exception):
+    """
+    Generic DiskError
+    """
+
+
 def freespace(path):
     fs_stats = os.statvfs(path)
     return fs_stats.f_bsize * fs_stats.f_bavail
@@ -38,6 +45,65 @@ def get_disk_blocksize(path):
     """Return the disk block size, in bytes"""
     fs_stats = os.statvfs(path)
     return fs_stats.f_bsize
+
+
+def create_loop_device(size, blocksize=4096, directory='./'):
+    """
+    Creates a loop device of size and blocksize specified.
+
+    :param size: Size of loop device, in bytes
+    :type syze: int
+    :param blocksize: block size of loop device, in bytes. Defaults to 4096
+    :type blocksize: int
+    :param directory: Directory where the backing file will be created.
+                      Defaults to current directory.
+    :type directory: str
+
+    :return: loop device name
+    :rtype: str
+    """
+    cmd = "losetup --find"
+    loop = process.run(cmd, ignore_status=True,
+                       sudo=True).stdout_text.strip('\n')
+
+    loop_file = os.path.join(directory,
+                             "tmp_%s.img" % loop.split('/')[-1])
+    cmd = "dd if=/dev/zero of={} bs={} count={}".format(loop_file,
+                                                        blocksize,
+                                                        int(size / blocksize))
+    if process.system(cmd, ignore_status=True, sudo=True) != 0:
+        raise DiskError("Unable to create backing file for loop device")
+
+    cmd = "losetup %s %s -P" % (loop, loop_file)
+    if process.system(cmd, ignore_status=True, sudo=True) != 0:
+        raise DiskError("Unable to create the loop device")
+    return loop
+
+
+def delete_loop_device(device):
+    """
+    Deletes the specified loop device.
+
+    :param device: device to be deleted
+    :type device: str
+
+    :return: True if deleted.
+    :rtype: bool
+    """
+    cmd = "losetup -aJl"
+    loop_dic = json.loads(process.run(cmd, ignore_status=True,
+                                      sudo=True).stdout_text)
+    loop_file = ''
+    for loop_dev in loop_dic['loopdevices']:
+        if device == loop_dev['name']:
+            loop_file = loop_dev['back-file']
+    if not loop_file:
+        raise DiskError("Unable to find backing file for loop device")
+    cmd = "losetup -d %s" % device
+    if process.system(cmd, ignore_status=True, sudo=True) != 0:
+        raise DiskError("Unable to delete the loop device")
+    os.remove(loop_file)
+    return True
 
 
 def get_disks():
