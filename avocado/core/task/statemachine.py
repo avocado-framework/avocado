b6284d703efc64041afd35c0f2663b55a3e5076c
Cleber Rosa <crosa@redhat.com>
2020-08-23 22:50:05 -0400

    Introduce the task state machine
    The TaskStateMachine keeps track of the life cycle of a Task, while a
    Worker attempts to move them further in life.  It's, along with the
    status server, the basis for running multiple tasks at once and
    keeping track of their outcome.
    
    Signed-off-by: Cleber Rosa <crosa@redhat.com>

---
+++ avocado/core/task/statemachine.py
@@ -0,0 +1,152 @@
+import asyncio
+import multiprocessing
+import time
+
+
+class TaskStateMachine:
+    """Represents all phases that a task can go through its life."""
+    def __init__(self, tasks):
+        self._requested = tasks
+        self._triaging = []
+        self._ready = []
+        self._started = []
+        self._finished = []
+        self._lock = asyncio.Lock()
+
+    @property
+    def requested(self):
+        return self._requested
+
+    @property
+    def triaging(self):
+        return self._triaging
+
+    @property
+    def ready(self):
+        return self._ready
+
+    @property
+    def started(self):
+        return self._started
+
+    @property
+    def finished(self):
+        return self._finished
+
+    @property
+    def lock(self):
+        return self._lock
+
+    @property
+    async def complete(self):
+        async with self._lock:
+            pending = any([self._requested, self._triaging,
+                           self._ready, self._started])
+        return not pending
+
+
+class Worker:
+
+    def __init__(self, state_machine, spawner,
+                 max_triaging=None, max_running=None, task_timeout=None):
+        self._state_machine = state_machine
+        self._spawner = spawner
+        if max_triaging is None:
+            max_triaging = multiprocessing.cpu_count()
+        self._max_triaging = max_triaging
+        if max_running is None:
+            max_running = 2 * multiprocessing.cpu_count() - 1
+        self._max_running = max_running
+        self._task_timeout = task_timeout
+
+    async def bootstrap(self):
+        """Reads from requested, moves into triaging."""
+        try:
+            async with self._state_machine.lock:
+                if len(self._state_machine.triaging) < self._max_triaging:
+                    runtime_task = self._state_machine.requested.pop()
+                    self._state_machine.triaging.append(runtime_task)
+                else:
+                    return
+        except IndexError:
+            return
+
+    async def triage(self):
+        """Reads from triaging, moves into either: ready or finished."""
+        try:
+            async with self._state_machine.lock:
+                runtime_task = self._state_machine.triaging.pop()
+        except IndexError:
+            return
+
+        requirements_ok = await self._spawner.spawn_task(runtime_task)
+        if requirements_ok:
+            async with self._state_machine.lock:
+                self._state_machine.ready.append(runtime_task)
+        else:
+            async with self._state_machine.lock:
+                self._state_machine.finished.append(runtime_task)
+                runtime_task.status = 'FAILED ON TRIAGE'
+
+    async def start(self):
+        """Reads from ready, moves into either: started or finished."""
+        try:
+            async with self._state_machine.lock:
+                runtime_task = self._state_machine.ready.pop()
+        except IndexError:
+            return
+
+        # enforce a rate limit on the number of started (currently
+        # running) tasks.  this is a global limit, but the spawners
+        # can also be queried with regards to their capacity to handle
+        # new tasks
+        async with self._state_machine.lock:
+            if len(self._state_machine.started) >= self._max_running:
+                self._state_machine.ready.insert(0, runtime_task)
+                runtime_task.status = 'WAITING'
+                return
+
+        start_ok = await self._spawner.spawn_task(runtime_task)
+        if start_ok:
+            runtime_task.status = None
+            if self._task_timeout is not None:
+                runtime_task.execution_timeout = time.monotonic() + self._task_timeout
+            async with self._state_machine.lock:
+                self._state_machine.started.append(runtime_task)
+        else:
+            async with self._state_machine.lock:
+                self._state_machine.finished.append(runtime_task)
+
+    async def monitor(self):
+        """Reads from started, moves into finished."""
+        try:
+            async with self._state_machine.lock:
+                runtime_task = self._state_machine.started.pop()
+        except IndexError:
+            return
+
+        if self._spawner.is_task_alive(runtime_task):
+            try:
+                if runtime_task.execution_timeout is None:
+                    remaining = None
+                else:
+                    remaining = runtime_task.timeout - time.monotonic()
+                await asyncio.wait_for(self._spawner.wait_task(runtime_task),
+                                       remaining)
+                runtime_task.status = 'FINISHED'
+            except asyncio.TimeoutError:
+                runtime_task.status = 'FINISHED W/ TIMEOUT'
+
+        async with self._state_machine.lock:
+            self._state_machine.finished.append(runtime_task)
+
+    async def run(self):
+        """Pushes Tasks forward and makes them do something with their lifes."""
+        while True:
+            is_complete = await self._state_machine.complete
+            if is_complete:
+                break
+            await self.bootstrap()
+            await self.triage()
+            await self.start()
+            await self.monitor()
---
+++ selftests/functional/test_task_statemachine.py
@@ -0,0 +1,32 @@
+import asyncio
+from unittest import TestCase
+
+from avocado.core.nrunner import Runnable, Task
+from avocado.core.task import statemachine
+from avocado.core.task.runtime import RuntimeTask
+from avocado.plugins.spawners.process import ProcessSpawner as Spawner
+
+# This test should, provided the environment supports, also be able
+# to run successfully with other spawners, such as:
+#
+# from avocado.plugins.spawners.podman import PodmanSpawner as Spawner
+
+
+class StateMachine(TestCase):
+
+    def test(self):
+        number_of_tasks = 80
+        number_of_workers = 8
+
+        runnable = Runnable("noop", "noop")
+        runtime_tasks = [RuntimeTask(Task("%03i" % _, runnable))
+                         for _ in range(1, number_of_tasks + 1)]
+        spawner = Spawner()
+
+        state_machine = statemachine.TaskStateMachine(runtime_tasks)
+        loop = asyncio.get_event_loop()
+        workers = [statemachine.Worker(state_machine, spawner).run()
+                   for _ in range(number_of_workers)]
+
+        loop.run_until_complete(asyncio.gather(*workers))
+        self.assertEqual(number_of_tasks, len(state_machine.finished))
