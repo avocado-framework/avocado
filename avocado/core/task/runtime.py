52fdcd70aed204ed051644e1f94fc99d5b804c88
Cleber Rosa <crosa@redhat.com>
2020-08-23 22:50:05 -0400

    Introduce RuntimeTask
    A wrapper around a avocado.core.nrunner.Task for keeping track of its
    run time information.
    
    Signed-off-by: Cleber Rosa <crosa@redhat.com>

---
+++ avocado/core/task/runtime.py
@@ -0,0 +1,36 @@
+class RuntimeTask:
+    """Task with extra status information on its life cycle status.
+
+    The :class:`avocado.core.nrunner.Task` class contains information
+    that is necessary to describe its persistence and execution by itself.
+
+    This class wraps a :class:`avocado.core.nrunner.Task`, with extra
+    information about its execution by a spawner within a state machine.
+    """
+    def __init__(self, task):
+        """Instantiates a new RuntimeTask.
+
+        :param task: The task to keep additional information about
+        :type task: :class:`avocado.core.nrunner.Task`
+        """
+        #: The :class:`avocado.core.nrunner.Task`
+        self.task = task
+        #: Additional descriptive information about the task status
+        self.status = None
+        #: Timeout limit for the completion of the task execution
+        self.execution_timeout = None
+        #: A handle that may be set by a spawner, and that may be
+        #: spawner implementation specific, to keep track the task
+        #: execution.  This may be a PID, a container ID, a FQDN+PID
+        #: etc.
+        self.spawner_handle = None
+        #: The result of the spawning of a Task
+        self.spawning_result = None
+
+    def __repr__(self):
+        if self.status is None:
+            return '<RuntimeTask Task Identifier: "%s">' % self.task.identifier
+        else:
+            return '<RuntimeTask Task Identifier: "%s" Status: "%s">' % (
+                self.task.identifier,
+                self.status)
---
+++ selftests/unit/test_task_runtime.py
@@ -0,0 +1,24 @@
+from unittest import TestCase
+
+from avocado.core.nrunner import Runnable, Task
+from avocado.core.task.runtime import RuntimeTask
+
+
+class Runtime(TestCase):
+
+    def setUp(self):
+        runnable = Runnable('noop', 'noop')
+        task = Task('1-noop', runnable)
+        self.runtime_task = RuntimeTask(task)
+
+    def test_empty(self):
+        self.assertIsNone(self.runtime_task.status)
+        self.assertIsNone(self.runtime_task.execution_timeout)
+        self.assertIsNone(self.runtime_task.spawner_handle)
+        self.assertIsNone(self.runtime_task.spawning_result)
+
+    def test(self):
+        self.runtime_task.status = 'LOST CONTACT'
+        self.assertEqual(self.runtime_task.task.runnable.kind, 'noop')
+        self.assertEqual(self.runtime_task.task.runnable.uri, 'noop')
+        self.assertEqual(self.runtime_task.status, 'LOST CONTACT')
