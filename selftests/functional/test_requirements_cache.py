f758bc431fd6d2002c69087b70dc0647796fdfc4
Cleber Rosa <crosa@redhat.com>
2020-08-23 22:50:05 -0400

    Introduce requirements caching module
    The tasks of checking for requirements, one of the first phases of
    the N(ext) Runner scheduler, is one that can be pretty expensive.
    It makes sense to have utilities that will record the result of
    requirements verification.
    
    Implementation wise, this builds on top of one unique SQLITE based
    backend, because:
    
     * It's built into Python
     * It allows multiple connections to the same database file
       for reading operations
     * It allows proper locking when multiple attempts to write
       to database are performed
    
    With regards to the database schema, a partial normalization was
    chosen because it allows for straightforward use of the database, that
    is, inserts are performed with the real data and queries show
    meaningful data, instead of requiring a view or more complicated query.
    
    Signed-off-by: Cleber Rosa <crosa@redhat.com>

---
+++ selftests/functional/test_requirements_cache.py
@@ -0,0 +1,40 @@
+import os
+import unittest.mock
+
+from avocado.core.requirements import cache
+
+from .. import TestCaseTmpDir
+
+ENTRIES = [
+    ('podman',
+     'cd34d13b2980d0a9d438f754b2e94f85443076d0ebe1b0db09a0439f35feca5e',
+     'package',
+     'bash'),
+    ('podman',
+     'cd34d13b2980d0a9d438f754b2e94f85443076d0ebe1b0db09a0439f35feca5e',
+     'core',
+     'avocado'),
+    ('local',
+     'localhost.localdomain',
+     'core',
+     'avocado')
+    ]
+
+
+class Cache(TestCaseTmpDir):
+
+    def test_entries(self):
+        with unittest.mock.patch(
+                'avocado.core.requirements.cache.backends.sqlite.CACHE_DATABASE_PATH',
+                os.path.join(self.tmpdir.name,
+                             'requirements.sqlite')):
+            for entry in ENTRIES:
+                cache.set_requirement(*entry)
+                self.assertTrue(cache.get_requirement(*entry))
+
+    def test_empty(self):
+        with unittest.mock.patch(
+                'avocado.core.requirements.cache.backends.sqlite.CACHE_DATABASE_PATH',
+                os.path.join(self.tmpdir.name,
+                             'requirements.sqlite')):
+            self.assertFalse(cache.get_requirement(*ENTRIES[0]))
