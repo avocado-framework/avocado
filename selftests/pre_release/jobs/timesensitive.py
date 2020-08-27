382233a6276526610d50b938ee786c284b6f3373
Beraldo Leal <bleal@redhat.com>
2020-08-27 16:37:39 -0300

    pre_release.timesensitive: Adding status_server uri and using custom TestSuite
    This fixes #4153
    
    Signed-off-by: Beraldo Leal <bleal@redhat.com>

--- selftests/pre_release/jobs/timesensitive.py
+++ selftests/pre_release/jobs/timesensitive.py
@@ -14,11 +14,12 @@ CONFIG = {
     'run.references': [os.path.join(ROOT_DIR, 'selftests', 'unit'),
                        os.path.join(ROOT_DIR, 'selftests', 'functional')],
     'filter.by_tags.tags': ['parallel:1'],
+    'nrunner.status_server_uri': '127.0.0.1:8888',
     'nrunner.max_parallel_tasks': 1,
     }
 
 
 if __name__ == '__main__':
-    with Job(CONFIG) as j:
+    with Job.from_config(CONFIG) as j:
         os.environ['AVOCADO_CHECK_LEVEL'] = '3'
         sys.exit(j.run())
