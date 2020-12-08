026d66d4770cc8a544e55ea266b8605a6d64f4e7
Cleber Rosa <crosa@redhat.com>
2020-12-08 15:12:53 -0500

    nrunner: use a dynamic port for the status example job
    This job is usually run as part of the "selftests/check.py" job, which
    starts itself a status server.  Let's avoid clashes and have a dynamic
    port for this example job.
    
    Signed-off-by: Cleber Rosa <crosa@redhat.com>

--- examples/jobs/nrunner.py
+++ examples/jobs/nrunner.py
@@ -4,10 +4,14 @@ import sys
 
 from avocado.core.job import Job
 from avocado.core.suite import TestSuite
+from avocado.utils.network.ports import find_free_port
+
+status_server = '127.0.0.1:%u' % find_free_port()
 
 config = {
     'run.test_runner': 'nrunner',
-    'nrunner.status_server_uri': '127.0.0.1:9999',
+    'nrunner.status_server_listen': status_server,
+    'nrunner.status_server_uri': status_server,
     'run.references': [
         'selftests/unit/test_resolver.py',
         'selftests/functional/test_argument_parsing.py',
