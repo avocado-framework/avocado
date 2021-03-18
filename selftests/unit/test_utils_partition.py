626e35df05851ce16d165490500fd1a7e4f526b4
Cleber Rosa <crosa@redhat.com>
2021-03-18 11:47:23 -0400

    selftests/unit/test_utils_partition.py: skip tests that depend on mkfs.ext2
    If mkfs.ext2 is not available, some partition tests will fail.  The
    currently existing check (can_sudo()) always succeeds when the user's
    uid is 0, and the command given is never actually executed, making
    this extra check necessary.
    
    Signed-off-by: Cleber Rosa <crosa@redhat.com>

--- selftests/unit/test_utils_partition.py
+++ selftests/unit/test_utils_partition.py
@@ -37,6 +37,8 @@ class Base(unittest.TestCase):
     @unittest.skipIf(not process.can_sudo('mkfs.ext2 -V'),
                      'current user must be allowed to run "mkfs.ext2" under '
                      'sudo')
+    @unittest.skipUnless(utils_path.find_command('mkfs.ext2', False),
+                         'mkfs.ext2 utility must be available')
     @unittest.skipIf(os.getenv('TRAVIS') and
                      os.getenv('TRAVIS_CPU_ARCH') in ['arm64', 'ppc64le', 's390x'],
                      'TRAVIS Environment is unsuitable for these tests')
