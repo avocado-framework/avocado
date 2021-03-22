a62183dafaa0f6a2b5fc33e50442e0e6d6c2f36b
Beraldo Leal <bleal@redhat.com>
2021-03-22 10:05:35 -0300

    setup.py: adding lint command
    The idea is to split the checks into small stages and make all references as
    part of setup.py. Developers can now, run only lint issues with "python3
    setup.py lint".
    
    Signed-off-by: Beraldo Leal <bleal@redhat.com>

--- selftests/inspekt-indent.sh
+++ selftests/inspekt-indent.sh
@@ -1,3 +1,5 @@
 #!/bin/sh -e
+echo "** Running inspekt-indent..."
+
 # Skip checking test_utils_cpu.py due to inspektor bug
 inspekt indent --exclude=.git,selftests/unit/test_utils_cpu.py
--- selftests/spell.sh
+++ selftests/spell.sh
@@ -1,2 +1,4 @@
 #!/bin/sh -e
+echo "** Running spell check..."
+
 pylint -j 1 --errors-only --disable=all --enable=spelling --spelling-dict=en_US --spelling-private-dict-file=spell.ignore *
