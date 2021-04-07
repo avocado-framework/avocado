avocado-framework/avocado

commit f402b6d81a740dee75032a4806d6441c6dac7ed3

Decorators: support callable conditionals
Avocado's skipIf and skipUnless decorators allow a condition
to be given, but they have two limitations:

1) They are evaluated early
2) They are given no context

This expands the decorators by allowing them to be evaluated
later by accepting callables.  Additionally, the callables
are given the class as a parameter, this way they can have
more context on what's going on.

Signed-off-by: Cleber Rosa <crosa@redhat.com>

avocado/core/decorators.py
@@ -69,31 +69,39 @@ fail_on = deco_factory("fail", core_exceptions.TestFail)
 cancel_on = deco_factory("cancel", core_exceptions.TestCancel)
 
 
-def _skip_method_decorator(function, message):
+def _skip_method_decorator(function, message, condition=None):
     """Creates a skip decorator for a method."""
     @wraps(function)
-    def wrapper(*args, **kwargs):  # pylint: disable=W0613
-        raise core_exceptions.TestSkipError(message)
+    def wrapper(obj, *args, **kwargs):  # pylint: disable=W0613
+        if condition is None:
+            raise core_exceptions.TestSkipError(message)
+        else:
+            if callable(condition):
+                if condition(obj):
+                    raise core_exceptions.TestSkipError(message)
+            elif condition:
+                raise core_exceptions.TestSkipError(message)
     wrapper.__skip_test_decorator__ = True
     return wrapper
 
 
-def _skip_class_decorator(cls, message):
+def _skip_class_decorator(cls, message, condition=None):
     """Creates a skip decorator for a class."""
     for key in cls.__dict__:
         if key.startswith('test') and callable(getattr(cls, key)):
-            wrapped = _skip_method_decorator(getattr(cls, key), message)
+            wrapped = _skip_method_decorator(getattr(cls, key),
+                                             message, condition)
             setattr(cls, key, wrapped)
     return cls
 
 
-def _get_skip_method_or_class_decorator(message):
+def _get_skip_method_or_class_decorator(message, condition=None):
     """Returns a method or class decorator, according to decorated type."""
     def decorator(obj):
         if isinstance(obj, type):
-            return _skip_class_decorator(obj, message)
+            return _skip_class_decorator(obj, message, condition)
         else:
-            return _skip_method_decorator(obj, message)
+            return _skip_method_decorator(obj, message, condition)
     return decorator
 
 
@@ -112,29 +120,27 @@ def skipIf(condition, message=None):
     """
     Decorator to skip a test if a condition is True.
 
-    :param condition: a condition that will be evaluated as either True or False
-    :type condition: bool
+    :param condition: a condition that will be evaluated as either True or
+                      False, if it's a callable, it will be called with the
+                      class instance as a parameter
+    :type condition: bool or callable
     :param message: the message given when the test is skipped
     :type message: str
     """
-    if condition:
-        return skip(message)
-    return _itself
+    return _get_skip_method_or_class_decorator(message, condition)
 
 
 def skipUnless(condition, message=None):
     """
     Decorator to skip a test if a condition is False.
 
-    :param condition: a condition that will be evaluated as either True or False
-    :type condition: bool
+    :param condition: a condition that will be evaluated as either True or
+                      False, if it's a callable, it will be called with the
+                      class instance as a parameter
+    :type condition: bool or callable
     :param message: the message given when the test is skipped
     :type message: str
     """
-    if not condition:
-        return skip(message)
-    return _itself
-
-
-def _itself(obj):
-    return obj
+    return _get_skip_method_or_class_decorator(
+        message,
+        lambda x: not (condition(x) if callable(condition) else condition))

selftests/functional/test_skiptests.py
@@ -13,6 +13,9 @@ from lib_skip_decorators import check_condition
 
 class AvocadoSkipTests(avocado.Test):
 
+    TRUE_CONDITION = True
+    FALSE_CONDITION = False
+
     def setUp(self):
         self.log.info('setup executed')
 
@@ -28,6 +31,16 @@ class AvocadoSkipTests(avocado.Test):
     @avocado.skipUnless(check_condition(False),
                         'Skipped due to the False condition')
     def test3(self):
+        self.log.info('test executed')
+
+    @avocado.skipIf(lambda x: x.TRUE_CONDITION,
+                    'Skipped due to the True condition')
+    def test4(self):
+        self.log.info('test executed')
+
+    @avocado.skipUnless(lambda x: x.FALSE_CONDITION,
+                        'Skipped due to the False condition')
+    def test5(self):
         self.log.info('test executed')
 
     def tearDown(self):
@@ -237,7 +250,7 @@ class TestSkipDecorators(TestCaseTmpDir):
         debuglog = json_results['debuglog']
 
         self.assertEqual(result.exit_status, exit_codes.AVOCADO_ALL_OK)
-        self.assertEqual(json_results['skip'], 3)
+        self.assertEqual(json_results['skip'], 5)
         debuglog_contents = genio.read_file(debuglog)
         self.assertFalse('setup executed' in debuglog_contents)
         self.assertFalse('test executed' in debuglog_contents)

examples/tests/skip_conditional.py
@@ -1,0 +1,47 @@
+from avocado import Test, skipIf, skipUnless
+
+
+class BaseTest(Test):
+    """Base class for tests
+
+    :avocado: disable
+    """
+
+    SUPPORTED_ENVS = []
+
+    @skipUnless(lambda x: 'BARE_METAL' in x.SUPPORTED_ENVS,
+                'Bare metal environment is required')
+    def test_bare_metal(self):
+        pass
+
+    @skipIf(lambda x: getattr(x, 'MEMORY', 0) < 4096,
+            'Not enough memory for test')
+    def test_large_memory(self):
+        pass
+
+    @skipUnless(lambda x: 'VIRTUAL_MACHINE' in x.SUPPORTED_ENVS,
+                'Virtual Machine environment is required')
+    def test_nested_virtualization(self):
+        pass
+
+    @skipUnless(lambda x: 'CONTAINER' in x.SUPPORTED_ENVS,
+                'Container environment is required')
+    def test_container(self):
+        pass
+
+
+class BareMetal(BaseTest):
+
+    SUPPORTED_ENVS = ['BARE_METAL']
+    MEMORY = 2048
+
+    def test_specific(self):
+        pass
+
+
+class NonBareMetal(BaseTest):
+
+    SUPPORTED_ENVS = ['VIRTUAL_MACHINE', 'CONTAINER']
+
+    def test_specific(self):
+        pass

commit 1ccfe32feceb3e0f1fba432e581a0c0e10779be1

Decorators: fix issue with conditional skips and None
Callable conditional were introduced in f402b6d81, but it also
introduced a bug.  In an effort to support both non-conditionals
(plain "@skip") and conditionals ("@skipIf" and "@skipUnless")
a parameter called condition was introduced, and checked for being
None.

The problem is that None should evaluate to False, and thus cause
the skipIf to *not* skip, such as in:

   @avocado.skipIf(None,
                   'skipIf with None condition, should not happen')

And conversely, skipUnless to skip.  The bugfix simply makes the
non-conditional skip have a True condition instead.

Also added are tests that verify the behavior of skipIf/skipUnless
with both callables, non-callables and None, asserting that conditions
where tests should not be skipped are respected.

Signed-off-by: Cleber Rosa <crosa@redhat.com>

avocado/core/decorators.py
@@ -69,18 +69,15 @@ fail_on = deco_factory("fail", core_exceptions.TestFail)
 cancel_on = deco_factory("cancel", core_exceptions.TestCancel)
 
 
-def _skip_method_decorator(function, message, condition=None):
+def _skip_method_decorator(function, message, condition):
     """Creates a skip decorator for a method."""
     @wraps(function)
     def wrapper(obj, *args, **kwargs):  # pylint: disable=W0613
-        if condition is None:
-            raise core_exceptions.TestSkipError(message)
-        else:
-            if callable(condition):
-                if condition(obj):
-                    raise core_exceptions.TestSkipError(message)
-            elif condition:
+        if callable(condition):
+            if condition(obj):
                 raise core_exceptions.TestSkipError(message)
+        elif condition:
+            raise core_exceptions.TestSkipError(message)
     wrapper.__skip_test_decorator__ = True
     return wrapper
 
@@ -95,7 +92,7 @@ def _skip_class_decorator(cls, message, condition=None):
     return cls
 
 
-def _get_skip_method_or_class_decorator(message, condition=None):
+def _get_skip_method_or_class_decorator(message, condition):
     """Returns a method or class decorator, according to decorated type."""
     def decorator(obj):
         if isinstance(obj, type):
@@ -113,7 +110,7 @@ def skip(message=None):
     :type message: str
 
     """
-    return _get_skip_method_or_class_decorator(message)
+    return _get_skip_method_or_class_decorator(message, True)
 
 
 def skipIf(condition, message=None):

selftests/functional/test_skiptests.py
@@ -43,9 +43,67 @@ class AvocadoSkipTests(avocado.Test):
     def test5(self):
         self.log.info('test executed')
 
+    @avocado.skipIf(lambda x: x.FALSE_CONDITION,
+                    'skipIf with False condition, should not happen')
+    def test6(self):
+        self.log.info('test executed')
+
+    @avocado.skipUnless(lambda x: x.TRUE_CONDITION,
+                        'skipUnless with True condition, should not happen')
+    def test7(self):
+        self.log.info('test executed')
+
     def tearDown(self):
         self.log.info('teardown executed')
 """
+
+
+AVOCADO_TEST_NOT_SKIP_DECORATORS = """
+import avocado
+from lib_skip_decorators import check_condition
+
+class AvocadoSkipTests(avocado.Test):
+
+    TRUE_CONDITION = True
+    FALSE_CONDITION = False
+
+    def setUp(self):
+        self.log.info('setup executed')
+
+    @avocado.skipIf(False,
+                    'skipIf with False condition, should not happen')
+    def test1(self):
+        self.log.info('test executed')
+
+    @avocado.skipUnless(True,
+                        'SkipUnless with True condition, should not happen')
+    def test2(self):
+        self.log.info('test executed')
+
+    @avocado.skipIf(lambda x: x.FALSE_CONDITION,
+                    'skipIf with False condition, should not happen')
+    def test3(self):
+        self.log.info('test executed')
+
+    @avocado.skipUnless(lambda x: x.TRUE_CONDITION,
+                        'skipUnless with True condition, should not happen')
+    def test4(self):
+        self.log.info('test executed')
+
+    @avocado.skipIf(None,
+                    'skipIf with None condition, should not happen')
+    def test5(self):
+        self.log.info('test executed')
+
+    @avocado.skipIf(lambda x: None,
+                    'skipIf with None condition, should not happen')
+    def test6(self):
+        self.log.info('test executed')
+
+    def tearDown(self):
+        self.log.info('teardown executed')
+"""
+
 
 AVOCADO_TEST_SKIP_CLASS_DECORATORS = """
 import avocado
@@ -252,6 +310,17 @@ class Skip(Base):
 
     def test_skip_decorators(self):
         self.check_skips_and_content(5)
+
+
+class NotSkip(Base):
+
+    FILE_NAME_CONTENT_MAP = {
+        'lib_skip_decorators.py': AVOCADO_TEST_SKIP_LIB,
+        'script_to_exec.py': AVOCADO_TEST_NOT_SKIP_DECORATORS
+    }
+
+    def test(self):
+        self.check_status(**{'pass': 6, 'skip': 0})
 
 
 class SkipClass(Base):