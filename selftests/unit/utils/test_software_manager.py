5715a83a73a7df1aba7d27ca1bbe818a4198960c
Cleber Rosa <crosa@redhat.com>
2021-04-30 19:58:52 -0400

    Software Manager: open up the Apt test to all applicable distros
    Instead of blocking this test from running on almost every applicable
    distros, this will let it run on most distros.  Then, if a
    incompatiblity is found, we can address the issue.
    
    Signed-off-by: Cleber Rosa <crosa@redhat.com>

--- selftests/unit/utils/test_software_manager.py
+++ selftests/unit/utils/test_software_manager.py
@@ -8,15 +8,8 @@ setup_avocado_loggers()
 
 
 def apt_supported_distro():
-    """
-    The only Linux distributions this was tested on
-    """
-    this = distro.detect()
-    if this.name == 'debian':
-        return this.version == '9' and this.release == '6'
-    elif this.name == 'Ubuntu':
-        return this.version == '18' and this.release == '04'
-    return False
+    """Distros we expect to have the apt backend selected."""
+    return distro.detect().name in ['debian', 'Ubuntu']
 
 
 @unittest.skipUnless(os.getuid() == 0, "This test requires root privileges")
