ec1b22a45e4a49ee6928e270b119cf0bd54c0b69
Beraldo Leal <bleal@redhat.com>
2021-05-12 10:33:10 -0300

    isort: ignoring .data/ files
    Signed-off-by: Beraldo Leal <bleal@redhat.com>

--- selftests/isort.sh
+++ selftests/isort.sh
@@ -4,4 +4,5 @@ isort --recursive --check-only \
       --skip selftests/.data/loader_instrumented/dont_crash.py \
       --skip selftests/.data/loader_instrumented/double_import.py \
       --skip selftests/.data/loader_instrumented/dont_detect_non_avocado.py \
+      --skip selftests/.data/loader_instrumented/imports.py \
       --skip avocado/__init__.py .
