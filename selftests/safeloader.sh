a46f9daf66c7f6ea1d22635ab22c0ea1ba2b2761
Cleber Rosa <crosa@redhat.com>
2021-05-31 19:36:33 -0400

    Introduce selftests for safeloader handling of unittests
    This is a test with a temporary set of files, to help with the
    resolution of issue #4625.
    
    This check should include *ALL* files that contain Python unittests.
    
    Further work may also introduce a "non-safe" version of the loader
    for avocado-instrumented tests for a similar comparison.
    
    Signed-off-by: Cleber Rosa <crosa@redhat.com>

---
+++ selftests/safeloader.sh
@@ -0,0 +1,18 @@
+#!/bin/bash -e
+
+SAFELOADER_OUTPUT=$(mktemp /tmp/safeloader-XXXXXX)
+DEFAULT_LOADER_OUTPUT=$(mktemp /tmp/safeloader-XXXXXX)
+
+function cleanup()
+{
+    rm $SAFELOADER_OUTPUT $DEFAULT_LOADER_OUTPUT
+}
+
+trap cleanup EXIT
+
+for PYTHON_FILE in $(grep -c -m1 -E 'import unittest$' selftests/unit/test_*.py selftests/unit/utils/test_*.py | grep -v -E ':0$' | cut -d ':' -f 1); do
+    echo "*** Checking safeloader on $PYTHON_FILE ***";
+    python3 contrib/scripts/find-python-unittest $PYTHON_FILE > $SAFELOADER_OUTPUT;
+    AVOCADO_SAFELOADER_UNITTEST_ORDER_COMPAT=1 python3 contrib/scripts/avocado-safeloader-find-python-unittest $PYTHON_FILE > $DEFAULT_LOADER_OUTPUT;
+    diff -u $SAFELOADER_OUTPUT $DEFAULT_LOADER_OUTPUT;
+done;
