a10e542fbdbfc32b3dcc2f243301ecadf1b42a3e
Willian Rampazzo <willianr@redhat.com>
2021-06-01 16:51:04 -0300

    Adjust requirement_package.py runner to do not fail on collision
    On issue https://github.com/avocado-framework/avocado/issues/4620, when
    two tasks try to fulfill a package, one is smart enough to wait for the
    other to finish, but as soon as the lock is released, one may fail if the
    other has still work to complete, like erase the cache.
    
    This patch checks if the failure was not a false negative.
    
    Fixes: #4620
    
    Signed-off-by: Willian Rampazzo <willianr@redhat.com>

--- selftests/functional/test_requirements.py
+++ selftests/functional/test_requirements.py
@@ -104,7 +104,6 @@ class BasicTest(TestCaseTmpDir):
             self.assertIn('PASS 1', result.stdout_text,)
             self.assertNotIn('bash', result.stdout_text,)
 
-    @unittest.skip('Skipping until test collision is fixed (#4620).')
     @unittest.skipUnless(os.getenv('CI'), skip_package_manager_message)
     def test_single_fail(self):
         with script.Script(os.path.join(self.tmpdir.name,
@@ -118,7 +117,6 @@ class BasicTest(TestCaseTmpDir):
             self.assertIn('SKIP 1', result.stdout_text,)
             self.assertNotIn('-foo-bar-', result.stdout_text,)
 
-    @unittest.skip('Skipping until test collision is fixed (#4620).')
     @unittest.skipUnless(os.getenv('CI'), skip_install_message)
     def test_multiple_success(self):
         with script.Script(os.path.join(self.tmpdir.name,
@@ -131,7 +129,6 @@ class BasicTest(TestCaseTmpDir):
             self.assertIn('PASS 3', result.stdout_text,)
             self.assertNotIn('vim-common', result.stdout_text,)
 
-    @unittest.skip('Skipping until test collision is fixed (#4620).')
     @unittest.skipUnless(os.getenv('CI'), skip_install_message)
     def test_multiple_fails(self):
         with script.Script(os.path.join(self.tmpdir.name,
