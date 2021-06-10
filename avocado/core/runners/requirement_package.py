330159bcc2e0b858b1bee35854c9f25d003bdcd3
Jan Richter <jarichte@redhat.com>
2021-06-10 14:46:33 +0200

    requirement package runner with messaging utility
    Usage of message helper methods inside requirement package runner.
    
    Signed-off-by: Jan Richter <jarichte@redhat.com>

--- avocado/core/runners/requirement_package.py
+++ avocado/core/runners/requirement_package.py
@@ -4,6 +4,7 @@ from multiprocessing import Process, SimpleQueue
 from ...utils.software_manager.main import MESSAGES
 from ...utils.software_manager.manager import SoftwareManager
 from .. import nrunner
+from .utils import messages
 
 
 class RequirementPackageRunner(nrunner.BaseRunner):
@@ -105,17 +106,15 @@ class RequirementPackageRunner(nrunner.BaseRunner):
         queue.put(output)
 
     def run(self):
-        yield self.prepare_status('started')
+        yield messages.StartedMessage.get()
         # check if there is a valid 'action' argument
         cmd = self.runnable.kwargs.get('action', 'install')
         # avoid invalid arguments
         if cmd not in ['install', 'check', 'remove']:
             stderr = ("Invalid action %s. Use one of 'install', 'check' or"
                       " 'remove'" % cmd)
-            yield self.prepare_status('running',
-                                      {'type': 'stderr',
-                                       'log': stderr.encode()})
-            yield self.prepare_status('finished', {'result': 'error'})
+            yield messages.StderrMessage.get(stderr.encode())
+            yield messages.FinishedMessage.get('error')
             return
 
         package = self.runnable.kwargs.get('name')
@@ -131,7 +130,7 @@ class RequirementPackageRunner(nrunner.BaseRunner):
 
             while queue.empty():
                 time.sleep(nrunner.RUNNER_RUN_STATUS_INTERVAL)
-                yield self.prepare_status('running')
+                yield messages.RunningMessage.get()
 
             output = queue.get()
             result = output['result']
@@ -144,13 +143,9 @@ class RequirementPackageRunner(nrunner.BaseRunner):
             stderr = ('Package name should be passed as kwargs using'
                       ' name="package_name".')
 
-        yield self.prepare_status('running',
-                                  {'type': 'stdout',
-                                   'log': stdout.encode()})
-        yield self.prepare_status('running',
-                                  {'type': 'stderr',
-                                   'log': stderr.encode()})
-        yield self.prepare_status('finished', {'result': result})
+        yield messages.StdoutMessage.get(stdout.encode())
+        yield messages.StderrMessage.get(stderr.encode())
+        yield messages.FinishedMessage.get(result)
 
 
 class RunnerApp(nrunner.BaseRunnerApp):
