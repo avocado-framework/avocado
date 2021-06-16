02d119304ad0ee8f830c82ce561d345419bf9746
Cleber Rosa <crosa@redhat.com>
2021-06-15 23:34:22 -0400

    setup.py: declare support for Python 3.9
    We have been testing Avocado under Python 3.9 (on Fedora 33) for
    a while.  Let's declare support for Python 3.9 formally.
    
    Signed-off-by: Cleber Rosa <crosa@redhat.com>

--- setup.py
+++ setup.py
@@ -134,6 +134,7 @@ if __name__ == '__main__':
               "Programming Language :: Python :: 3.6",
               "Programming Language :: Python :: 3.7",
               "Programming Language :: Python :: 3.8",
+              "Programming Language :: Python :: 3.9",
               ],
           packages=find_packages(exclude=('selftests*',)),
           include_package_data=True,
