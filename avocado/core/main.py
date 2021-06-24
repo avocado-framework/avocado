e1c47f38dfd1954285a8325aa37bd0b436d4d2cc
Beraldo Leal <bleal@redhat.com>
2021-06-24 10:53:34 -0300

    core.main: Using exit_codes globals when possible
    Since the first try..except is catching any import error with avocado,
    we can use the globals on those places.
    
    Signed-off-by: Beraldo Leal <bleal@redhat.com>

--- avocado/core/main.py
+++ avocado/core/main.py
@@ -19,6 +19,7 @@ import time
 import traceback
 
 try:
+    from avocado.core import exit_codes
     from avocado.core.settings import settings
 except ImportError:
     sys.stderr.write("Unable to import Avocado libraries, please verify "
@@ -53,15 +54,13 @@ def handle_exception(*exc_info):
     os.close(tmp)
     if exc_info[0] is KeyboardInterrupt:
         msg = "%s\nYou can find details in %s\n" % (exc_info[0].__doc__, name)
-        exit_code = 8
+        exit_code = exit_codes.AVOCADO_JOB_INTERRUPTED
     else:
         # Print friendly message in console-like output
         msg = ("Avocado crashed unexpectedly: %s\nYou can find details in %s\n"
                % (exc_info[1], name))
-        exit_code = -1
+        exit_code = exit_codes.AVOCADO_GENERIC_CRASH
     os.write(2, msg.encode('utf-8'))
-    # This exit code is replicated from avocado/core/exit_codes.py and not
-    # imported because we are dealing with import failures
     sys.exit(exit_code)
 
 
