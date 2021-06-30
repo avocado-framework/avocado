149d6595b0811e693658d0d7cd3db3077921ce72
Jan Richter <jarichte@redhat.com>
2021-06-30 14:56:53 +0200

    logfile value in results.json
    The path to the test log file wasn't send to the `end_test` plugins.
    This commit fix it and adds the appropriate test to catch future bugs.
    
    Reference: #4750
    Signed-off-by: Jan Richter <jarichte@redhat.com>

---
+++ selftests/functional/plugin/test_jsonresult.py
@@ -0,0 +1,21 @@
+import json
+from os import path
+
+from avocado.utils import process
+from selftests.utils import AVOCADO, TestCaseTmpDir
+
+
+class JsonResultTest(TestCaseTmpDir):
+
+    def test_logfile(self):
+        cmd_line = ('%s run --test-runner=nrunner examples/tests/failtest.py '
+                    '--job-results-dir %s --disable-sysinfo ' %
+                    (AVOCADO, self.tmpdir.name))
+        process.run(cmd_line, ignore_status=True)
+        json_path = path.join(self.tmpdir.name, 'latest', 'results.json')
+
+        with open(json_path, 'r') as json_file:
+            data = json.load(json_file)
+            test_data = data['tests'].pop()
+            expected_logfile = path.join(test_data['logdir'], 'debug.log')
+            self.assertEqual(expected_logfile, test_data['logfile'])
