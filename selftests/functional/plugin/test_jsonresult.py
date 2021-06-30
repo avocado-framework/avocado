2736201c1c6e7417f13b167de8d07bbf8fbf84d3
Jan Richter <jarichte@redhat.com>
2021-06-30 11:12:56 +0200

    Instrumented tests with fail reason
    The avocado-instrumented runner wasn't gathering the fail-reason of
    tests. This fix the problem and adds the appropriate test to catch
    future bugs.
    
    Reference: #4749
    Signed-off-by: Jan Richter <jarichte@redhat.com>

---
+++ selftests/functional/plugin/test_jsonresult.py
@@ -0,0 +1,20 @@
+import json
+from os import path
+
+from avocado.utils import process
+from selftests.utils import AVOCADO, TestCaseTmpDir
+
+
+class JsonResultTest(TestCaseTmpDir):
+
+    def test_fail_reason(self):
+        cmd_line = ('%s run --test-runner=nrunner examples/tests/failtest.py '
+                    '--job-results-dir %s --disable-sysinfo ' %
+                    (AVOCADO, self.tmpdir.name))
+        process.run(cmd_line, ignore_status=True)
+        json_path = path.join(self.tmpdir.name, 'latest', 'results.json')
+        with open(json_path, 'r') as json_file:
+            data = json.load(json_file)
+            test_data = data['tests'].pop()
+            self.assertEqual('This test is supposed to fail',
+                             test_data['fail_reason'])
