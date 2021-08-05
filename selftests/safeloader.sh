4bb90415b5ca41bb59e2a702f41454421bb138d6
Cleber Rosa <crosa@redhat.com>
2021-08-05 14:57:21 -0400

    selftests/safeloader.sh: split actual check into utility function
    Signed-off-by: Cleber Rosa <crosa@redhat.com>

--- selftests/safeloader.sh
+++ selftests/safeloader.sh
@@ -10,9 +10,14 @@ function cleanup()
 
 trap cleanup EXIT
 
-for PYTHON_FILE in $(grep -c -m1 -E 'import unittest$' selftests/unit{,/plugin,/utils}/test_*.py selftests/functional{,/plugin}/test_*.py | grep -v -E ':0$' | cut -d ':' -f 1); do
-    echo "*** Checking safeloader on $PYTHON_FILE ***";
-    python3 contrib/scripts/find-python-unittest $PYTHON_FILE > $SAFELOADER_OUTPUT;
-    AVOCADO_SAFELOADER_UNITTEST_ORDER_COMPAT=1 python3 contrib/scripts/avocado-safeloader-find-python-unittest $PYTHON_FILE > $DEFAULT_LOADER_OUTPUT;
+function check()
+{
+    echo "*** Checking safeloader on $1 ***";
+    python3 contrib/scripts/find-python-unittest $1 > $SAFELOADER_OUTPUT;
+    AVOCADO_SAFELOADER_UNITTEST_ORDER_COMPAT=1 python3 contrib/scripts/avocado-safeloader-find-python-unittest $1 > $DEFAULT_LOADER_OUTPUT;
     diff -u $SAFELOADER_OUTPUT $DEFAULT_LOADER_OUTPUT;
-done;
+}
+
+for input_file in $(grep -c -m1 -E 'import unittest$' selftests/unit{,/plugin,/utils}/test_*.py selftests/functional{,/plugin}/test_*.py | grep -v -E ':0$' | cut -d ':' -f 1); do
+    check input_file
+done
