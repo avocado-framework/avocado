96b6d134c91eff21ba4c9d9f100a71e002fcd2b8
Cleber Rosa <crosa@redhat.com>
2021-08-05 15:29:35 -0400

    selftests/safeloader.sh: support checking a single file
    When debugging / reproducing safeloader issues with this test, it's
    useful to check a single file.  This adds the possibility of passing
    a command line parameter with the specific file path to be checked.
    
    Signed-off-by: Cleber Rosa <crosa@redhat.com>

--- selftests/safeloader.sh
+++ selftests/safeloader.sh
@@ -1,5 +1,10 @@
 #!/bin/bash -e
 
+# This script tests the "avocado.core.safeloader" module by processing
+# either one specific file (the first command line parameter given to
+# this script) or all unittest-like files in the selftests directories
+
+INPUT_FILE="${1:-}"
 SAFELOADER_OUTPUT=$(mktemp /tmp/safeloader-XXXXXX)
 DEFAULT_LOADER_OUTPUT=$(mktemp /tmp/safeloader-XXXXXX)
 
@@ -18,6 +23,10 @@ function check()
     diff -u $SAFELOADER_OUTPUT $DEFAULT_LOADER_OUTPUT;
 }
 
-for input_file in $(grep -c -m1 -E 'import unittest$' selftests/unit{,/plugin,/utils}/test_*.py selftests/functional{,/plugin}/test_*.py | grep -v -E ':0$' | cut -d ':' -f 1); do
-    check input_file
-done
+if [ -n "$INPUT_FILE" ]; then
+    check "$INPUT_FILE";
+else
+    for input_file in $(grep -c -m1 -E 'import unittest$' selftests/unit{,/plugin,/utils}/test_*.py selftests/functional{,/plugin}/test_*.py | grep -v -E ':0$' | cut -d ':' -f 1); do
+        check "$input_file"
+    done
+fi
