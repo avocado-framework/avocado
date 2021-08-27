3a214eee9c761b98a8106986d48622d025487797
Cleber Rosa <crosa@redhat.com>
2021-08-26 23:46:50 -0400

    selftests/functional/test_basic.py: support nrunner
    To support nrunner, it's necessary to read the log files from the
    actual test log (and not the job log).
    
    Signed-off-by: Cleber Rosa <crosa@redhat.com>

--- selftests/functional/test_basic.py
+++ selftests/functional/test_basic.py
@@ -549,16 +549,23 @@ class RunnerOperationTest(TestCaseTmpDir):
                          % result)
 
     def test_runner_test_parameters(self):
-        cmd_line = ('%s --show=test run --disable-sysinfo --job-results-dir %s '
+        cmd_line = ('%s run --disable-sysinfo --job-results-dir %s '
                     '-p "sleep_length=0.01" -- examples/tests/sleeptest.py '
                     % (AVOCADO, self.tmpdir.name))
         result = process.run(cmd_line, ignore_status=True)
         expected_rc = exit_codes.AVOCADO_ALL_OK
         self.assertEqual(result.exit_status, expected_rc,
                          "Avocado did not return rc %d:\n%s" % (expected_rc, result))
+
+        json_path = os.path.join(self.tmpdir.name, 'latest', 'results.json')
+        with open(json_path) as json_file:
+            result_json = json.load(json_file)
+        with open(result_json['tests'][0]['logfile'], 'r+b') as test_log_file:
+            test_log = test_log_file.read()
+
         self.assertIn(b"PARAMS (key=sleep_length, path=*, default=1) => '0.01'",
-                      result.stdout)
-        self.assertIn(b"Sleeping for 0.01 seconds", result.stdout)
+                      test_log)
+        self.assertIn(b"Sleeping for 0.01 seconds", test_log)
 
     def test_other_loggers(self):
         with script.TemporaryScript(
