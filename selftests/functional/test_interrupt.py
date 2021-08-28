f88488130a03b4ecfb6a561433b43fe747eda609
Beraldo Leal <bleal@redhat.com>
2021-08-28 17:35:39 -0300

    nrunner: fix nrunner test_interrupt processes
    nrunners does not support yet this kind of operation because of its
    architecture. Part of #4911
    
    Signed-off-by: Beraldo Leal <bleal@redhat.com>

--- selftests/functional/test_interrupt.py
+++ selftests/functional/test_interrupt.py
@@ -201,7 +201,7 @@ class InterruptTest(TestCaseTmpDir):
         good_test.save()
         self.test_module = good_test.path
         os.chdir(BASEDIR)
-        cmd = ('%s run %s --disable-sysinfo --job-results-dir %s ' %
+        cmd = ('%s run --test-runner=runner %s --disable-sysinfo --job-results-dir %s ' %
                (AVOCADO, self.test_module, self.tmpdir.name))
         proc = subprocess.Popen(cmd.split(),
                                 stdout=subprocess.PIPE,
