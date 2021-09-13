cffa6eeb626e974f5392f202c0116c45591402df
Ana Guerrero Lopez <anguerre@redhat.com>
2021-09-13 14:35:18 +0200

    opt_plugins/varianter_cit: run tests independently
    Add the command 'test' to setup.py
    
    Reference: https://github.com/avocado-framework/avocado/issues/4940
    
    Signed-off-by: Ana Guerrero Lopez <anguerre@redhat.com>

--- optional_plugins/varianter_cit/setup.py
+++ optional_plugins/varianter_cit/setup.py
@@ -13,10 +13,46 @@
 #          Bestoun S. Ahmed <bestoon82@gmail.com>
 #          Cleber Rosa <crosa@redhat.com>
 
+import glob
 
-from setuptools import find_packages, setup
+from setuptools import Command, find_packages, setup
 
 VERSION = open("VERSION", "r").read().strip()
+
+
+class Test(Command):
+    """Run tests"""
+
+    description = "Run tests"
+    user_options = []
+
+    def run(self):
+        # This must go there otherwise avocado must be installed for all targets
+        from avocado.core.job import Job
+        from avocado.core.suite import TestSuite
+        suites = []
+        pattern = 'tests/*'
+        config_check = {
+            'run.references': glob.glob(pattern),
+            'run.test_runner': 'nrunner',
+            'run.ignore_missing_references': True,
+            'job.output.testlogs.statuses': ['FAIL']
+            }
+        suites.append(TestSuite.from_config(config_check, "PLUGIN_varianter-cit"))
+
+        config = {'core.show': ['app'],
+                  'run.test_runner': 'nrunner'}
+
+        with Job(config, suites) as j:
+            exit_code = j.run()
+        return exit_code
+
+    def initialize_options(self):
+        """Set default values for options."""
+
+    def finalize_options(self):
+        """Post-process options."""
+
 
 setup(name='avocado-framework-plugin-varianter-cit',
       description='Varianter with combinatorial capabilities',
@@ -28,6 +64,7 @@ setup(name='avocado-framework-plugin-varianter-cit',
       include_package_data=True,
       install_requires=['avocado-framework==%s' % VERSION],
       test_suite='tests',
+      cmdclass={'test': Test},
       entry_points={
           'avocado.plugins.cli': [
               'varianter_cit = avocado_varianter_cit:VarianterCitCLI',
