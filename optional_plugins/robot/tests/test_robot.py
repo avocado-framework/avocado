3a1bf78d0536fda7ce6acc65512a17524f9dcba0
Cleber Rosa <crosa@redhat.com>
2021-10-19 13:15:11 -0400

    Robot: add resolver test
    Signed-off-by: Cleber Rosa <crosa@redhat.com>

--- optional_plugins/robot/tests/test_robot.py
+++ optional_plugins/robot/tests/test_robot.py
@@ -4,6 +4,8 @@ import unittest
 import avocado_robot
 import pkg_resources
 
+from avocado.core.resolver import ReferenceResolutionResult
+
 
 def python_module_available(module_name):
     '''
@@ -45,3 +47,24 @@ class Loader(unittest.TestCase):
         self.assertIs(sleep_klass, avocado_robot.RobotTest)
         self.assertEqual(sleep_params['name'],
                          "%s:Avocado.Sleep" % ROBOT_AVOCADO)
+
+
+class Resolver(unittest.TestCase):
+    @unittest.skipUnless(python_module_available('robotframework'),
+                         'robotframework python module missing')
+    @unittest.skipUnless(python_module_available('avocado-framework-plugin-robot'),
+                         'avocado-framework-plugin-robot python module missing')
+    @unittest.skipUnless(os.path.isfile(ROBOT_AVOCADO),
+                         'Robot test file not found at "%s"' % ROBOT_AVOCADO)
+    def test_resolver(self):
+        res = avocado_robot.RobotResolver().resolve(ROBOT_AVOCADO)
+        self.assertEqual(res.result, ReferenceResolutionResult.SUCCESS)
+        self.assertEqual(len(res.resolutions), 2)
+        nosleep = res.resolutions[0]
+        self.assertEqual(nosleep.kind, 'robot')
+        self.assertEqual(nosleep.uri,
+                         os.path.join(THIS_DIR, 'avocado.robot:Avocado.NoSleep'))
+        sleep = res.resolutions[1]
+        self.assertEqual(nosleep.kind, 'robot')
+        self.assertEqual(sleep.uri,
+                         os.path.join(THIS_DIR, 'avocado.robot:Avocado.Sleep'))
