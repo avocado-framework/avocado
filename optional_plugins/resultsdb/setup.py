7d814b44095bd5a840da48eb14369788b461432d
Ana Guerrero Lopez <anguerre@redhat.com>
2021-11-09 10:16:09 +0100

    optional_plugins/resultsdb: set versioned requires
    Fixes: https://github.com/avocado-framework/avocado/issues/5105
    
    Signed-off-by: Ana Guerrero Lopez <anguerre@redhat.com>

--- optional_plugins/resultsdb/setup.py
+++ optional_plugins/resultsdb/setup.py
@@ -26,7 +26,7 @@ setup(name='avocado-framework-plugin-resultsdb',
       packages=find_packages(),
       include_package_data=True,
       install_requires=['avocado-framework==%s' % VERSION,
-                        'resultsdb-api'],
+                        'resultsdb-api==2.1.3'],
       entry_points={
           'avocado.plugins.cli': [
               'resultsdb = avocado_resultsdb:ResultsdbCLI',
