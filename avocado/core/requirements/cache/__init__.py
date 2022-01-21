d24408d62a2493374b5dd50b0f126bf8f332499e
Ana Guerrero Lopez <anguerre@redhat.com>
2022-01-21 12:57:06 +0100

    avocado/core/requirements/cache/__init__.py: changes for PEP420
    Remove current __init__.py and update headers to work
    directly with the sqlite based backend, this is the only
    available implementation anyway.
    
    Signed-off-by: Ana Guerrero Lopez <anguerre@redhat.com>

--- avocado/core/requirements/cache/__init__.py
+++
@@ -1,3 +0,0 @@
-# The sqlite based backend is the only implementation
-from avocado.core.requirements.cache.backends.sqlite import (get_requirement,
-                                                             set_requirement)
