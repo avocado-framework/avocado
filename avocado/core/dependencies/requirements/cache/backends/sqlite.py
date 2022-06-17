e53daf7a0d945ea6b4251b673642bbef3f6c6344
Jan Richter <jarichte@redhat.com>
2022-06-17 11:27:52 +0200

    Requirements cache database updates
    This adds few methods for manipulating with requirements cache. This
    will be useful for spawners which will use requirements cache.
    
    Signed-off-by: Jan Richter <jarichte@redhat.com>

--- avocado/core/dependencies/requirements/cache/backends/sqlite.py
+++ avocado/core/dependencies/requirements/cache/backends/sqlite.py
@@ -24,6 +24,9 @@ from avocado.core.data_dir import get_datafile_path
 #: The location of the requirements cache database
 CACHE_DATABASE_PATH = get_datafile_path('cache', 'requirements.sqlite')
 
+sqlite3.register_adapter(bool, int)
+sqlite3.register_converter("BOOLEAN", lambda v: bool(int(v)))
+
 #: The definition of the database schema
 SCHEMA = [
     'CREATE TABLE IF NOT EXISTS requirement_type (requirement_type TEXT UNIQUE)',
@@ -41,6 +44,7 @@ SCHEMA = [
      'environment TEXT,'
      'requirement_type TEXT,'
      'requirement TEXT,'
+     'saved BOOLEAN,'
      'FOREIGN KEY(environment_type) REFERENCES environment(environment_type),'
      'FOREIGN KEY(environment) REFERENCES environment(environment),'
      'FOREIGN KEY(requirement_type) REFERENCES requirement_type(requirement_type)'
@@ -51,6 +55,7 @@ SCHEMA = [
 
 
 def _create_requirement_cache_db():
+    os.makedirs(os.path.dirname(CACHE_DATABASE_PATH), exist_ok=True)
     with sqlite3.connect(CACHE_DATABASE_PATH) as conn:
         cursor = conn.cursor()
         for entry in SCHEMA:
@@ -59,7 +64,7 @@ def _create_requirement_cache_db():
 
 
 def set_requirement(environment_type, environment,
-                    requirement_type, requirement):
+                    requirement_type, requirement, saved=True):
     if not os.path.exists(CACHE_DATABASE_PATH):
         _create_requirement_cache_db()
 
@@ -71,18 +76,24 @@ def set_requirement(environment_type, environment,
         cursor.execute(sql, (environment_type, environment))
         sql = "INSERT OR IGNORE INTO requirement_type VALUES (?)"
         cursor.execute(sql, (requirement_type, ))
-        sql = "INSERT OR IGNORE INTO requirement VALUES (?, ?, ?, ?)"
+        sql = "INSERT OR IGNORE INTO requirement VALUES (?, ?, ?, ?, ?)"
         cursor.execute(sql, (environment_type, environment,
-                             requirement_type, requirement))
-    conn.commit()
+                             requirement_type, requirement, saved))
+        conn.commit()
 
 
-def get_requirement(environment_type, environment,
-                    requirement_type, requirement):
+def is_requirement_in_cache(environment_type, environment,
+                            requirement_type, requirement):
+    """Checks if requirement is in cache.
+
+       :rtype: True if requirement is in cache
+               False if requirement is not in cache
+               None if requirement is in cache but it is not saved yet.
+    """
     if not os.path.exists(CACHE_DATABASE_PATH):
         return False
 
-    sql = ("SELECT COUNT(*) FROM requirement WHERE ("
+    sql = ("SELECT r.saved FROM requirement r WHERE ("
            "environment_type = ? AND "
            "environment = ? AND "
            "requirement_type = ? AND "
@@ -94,5 +105,196 @@ def get_requirement(environment_type, environment,
                                       requirement_type, requirement))
         row = result.fetchone()
         if row is not None:
-            return row[0] == 1
+            if row[0]:
+                return True
+            return None
     return False
+
+
+def is_environment_prepared(environment):
+    """Checks if environment has all requirements saved."""
+
+    if not os.path.exists(CACHE_DATABASE_PATH):
+        return False
+
+    sql = ("SELECT COUNT(*) FROM requirement r JOIN "
+           "environment e ON e.environment = r.environment "
+           "WHERE (r.environment = ? AND "
+           "r.saved = 0)")
+
+    with sqlite3.connect(CACHE_DATABASE_PATH,
+                         detect_types=sqlite3.PARSE_DECLTYPES) as conn:
+        cursor = conn.cursor()
+        result = cursor.execute(sql, (environment,))
+
+        row = result.fetchone()
+        if row is not None:
+            return row[0] == 0
+    return False
+
+
+def update_environment(environment_type, old_environment, new_environment):
+    """Updates environment information for each requirement in one environment.
+
+    It will remove the old environment and add the new one to the cache.
+
+    :param environment_type: Type of fetched environment
+    :type environment_type: str
+    :param old_environment: Environment which should be updated
+    :type environment: str
+    :param new_environment: Environment, which will be a reimbursement for the
+                            old one.
+    :type environment: str
+    """
+    if not os.path.exists(CACHE_DATABASE_PATH):
+        return False
+
+    with sqlite3.connect(CACHE_DATABASE_PATH) as conn:
+        cursor = conn.cursor()
+        sql = "INSERT OR IGNORE INTO environment VALUES (?, ?)"
+        cursor.execute(sql, (environment_type, new_environment))
+
+        sql = ("UPDATE requirement SET environment = ? WHERE ("
+               "environment_type = ? AND "
+               "environment = ? )")
+
+        cursor.execute(sql, (new_environment, environment_type,
+                             old_environment))
+
+        sql = ("DELETE FROM environment WHERE ("
+               "environment_type = ? AND "
+               "environment = ? )")
+
+        cursor.execute(sql, (environment_type, old_environment))
+        conn.commit()
+
+
+def update_requirement_status(environment_type, environment, requirement_type,
+                              requirement, new_status):
+    """Updates status of selected requirement in cache.
+
+    The status has two values, save=True or not_save=False.
+
+    :param environment_type: Type of fetched environment
+    :type environment_type: str
+    :param environment: Environment where the requirement is
+    :type environment: str
+    :param requirement_type: Type of the requirement in environment
+    :type requirement_type: str
+    :param requirement: Name of requirement
+    :type requirement: str
+    :param new_status: Requirement status which will be updated
+    :type new_status: bool
+    """
+
+    if not os.path.exists(CACHE_DATABASE_PATH):
+        return False
+
+    sql = ("UPDATE requirement SET saved = ? WHERE ("
+           "environment_type = ? AND "
+           "environment = ? AND "
+           "requirement_type = ? AND "
+           "requirement = ?)")
+
+    with sqlite3.connect(CACHE_DATABASE_PATH) as conn:
+        cursor = conn.cursor()
+        cursor.execute(sql, (new_status, environment_type, environment,
+                             requirement_type, requirement))
+        conn.commit()
+
+    return True
+
+
+def delete_environment(environment_type, environment):
+    """Deletes environment with all its requirements from cache.
+
+    :param environment_type: Type of environment
+    :type environment_type: str
+    :param environment: Environment which will be deleted
+    :type environment: str
+    """
+
+    if not os.path.exists(CACHE_DATABASE_PATH):
+        return False
+
+    with sqlite3.connect(CACHE_DATABASE_PATH) as conn:
+        sql = ("DELETE FROM requirement WHERE ("
+               "environment_type = ? AND "
+               "environment = ? )")
+        cursor = conn.cursor()
+        cursor.execute(sql, (environment_type, environment))
+        sql = ("DELETE FROM environment WHERE ("
+               "environment_type = ? AND "
+               "environment = ? )")
+        cursor.execute(sql, (environment_type, environment))
+        conn.commit()
+
+
+def delete_requirement(environment_type, environment, requirement_type,
+                       requirement):
+    """Deletes requirement from cache.
+
+    :param environment_type: Type of environment
+    :type environment_type: str
+    :param environment: Environment where the requirement is.
+    :type environment: str
+    :param requirement_type: Type of the requirement in environment
+    :type requirement_type: str
+    :param requirement: Name of requirement which will be deleted
+    :type requirement: str
+    """
+
+    if not os.path.exists(CACHE_DATABASE_PATH):
+        return False
+
+    with sqlite3.connect(CACHE_DATABASE_PATH) as conn:
+        sql = ("DELETE FROM requirement WHERE ("
+               "environment_type = ? AND "
+               "environment = ? AND "
+               "requirement_type = ? AND "
+               "requirement = ?)")
+        cursor = conn.cursor()
+        cursor.execute(sql, (environment_type, environment, requirement_type,
+                             requirement))
+        conn.commit()
+
+
+def get_all_environments_with_requirement(environment_type, requirement_type,
+                                          requirement):
+    """Fetches all environments with selected requirement from cache.
+
+    :param environment_type: Type of fetched environment
+    :type environment_type: str
+    :param requirement_type: Type of the requirement in environment
+    :type requirement_type: str
+    :param requirement: Name of requirement
+    :type requirement: str
+    :return: Dict with all environments which has selected requirements.
+
+    """
+    requirements = {}
+    if not os.path.exists(CACHE_DATABASE_PATH):
+        return requirements
+
+    environment_select = ("SELECT e.environment FROM requirement r JOIN "
+                          "environment e ON e.environment = r.environment "
+                          "WHERE (r.environment_type = ? AND "
+                          "r.requirement_type = ? AND "
+                          "r.requirement = ?)")
+    sql = (f"SELECT r.environment, r.requirement_type, r.requirement "
+           f"FROM requirement AS r, ({environment_select}) AS e "
+           f"WHERE r.environment = e.environment")
+
+    with sqlite3.connect(CACHE_DATABASE_PATH,
+                         detect_types=sqlite3.PARSE_DECLTYPES) as conn:
+        cursor = conn.cursor()
+        result = cursor.execute(sql, (environment_type,
+                                      requirement_type,
+                                      requirement))
+
+        for row in result.fetchall():
+            if row[0] in requirements:
+                requirements[row[0]].append((row[1], row[2]))
+            else:
+                requirements[row[0]] = [(row[1], row[2])]
+    return requirements
--- selftests/functional/test_requirements_cache.py
+++ selftests/functional/test_requirements_cache.py
@@ -11,12 +11,25 @@ ENTRIES = [
      'bash'),
     ('podman',
      'cd34d13b2980d0a9d438f754b2e94f85443076d0ebe1b0db09a0439f35feca5e',
+     'package',
+     'hello'),
+    ('podman',
+     'ad34d13b2980d0a9d438f754b2e94f85443076d0ebe1b0db09a0439f35feca5e',
+     'package',
+     'hello'),
+    ('podman',
+     'cd34d13b2980d0a9d438f754b2e94f85443076d0ebe1b0db09a0439f35feca5e',
      'core',
      'avocado'),
     ('local',
      'localhost.localdomain',
      'core',
-     'avocado')
+     'avocado'),
+    ('podman',
+     'pd34d13b2980d0a9d438f754b2e94f85443076d0ebe1b0db09a0439f35feca5e',
+     'package',
+     'foo',
+     0),
     ]
 
 
@@ -27,13 +40,53 @@ class Cache(TestCaseTmpDir):
                 'avocado.core.dependencies.requirements.cache.backends.sqlite.CACHE_DATABASE_PATH',
                 os.path.join(self.tmpdir.name,
                              'requirements.sqlite')):
-            for entry in ENTRIES:
+            for entry in ENTRIES[:-1]:
                 cache.set_requirement(*entry)
-                self.assertTrue(cache.get_requirement(*entry))
+                self.assertTrue(cache.is_requirement_in_cache(*entry))
+            entry = ENTRIES[-1]
+            cache.set_requirement(*entry)
+            self.assertIsNone(cache.is_requirement_in_cache(entry[0],
+                                                            entry[1],
+                                                            entry[2],
+                                                            entry[3]))
+            self.assertFalse(cache.is_requirement_in_cache('local',
+                                                           'localhost.localdomain',
+                                                           'package',
+                                                           'foo'))
 
     def test_empty(self):
         with unittest.mock.patch(
                 'avocado.core.dependencies.requirements.cache.backends.sqlite.CACHE_DATABASE_PATH',
                 os.path.join(self.tmpdir.name,
                              'requirements.sqlite')):
-            self.assertFalse(cache.get_requirement(*ENTRIES[0]))
+            self.assertFalse(cache.is_requirement_in_cache(*ENTRIES[0]))
+
+    def test_is_environment_prepared(self):
+        with unittest.mock.patch(
+                'avocado.core.dependencies.requirements.cache.backends.sqlite.CACHE_DATABASE_PATH',
+                os.path.join(self.tmpdir.name,
+                             'requirements.sqlite')):
+            for entry in ENTRIES:
+                cache.set_requirement(*entry)
+            self.assertFalse(cache.is_environment_prepared(
+                "pd34d13b2980d0a9d438f754b2e94f85443076d0ebe1b0db09a0439f35feca5e"))
+            self.assertTrue(cache.is_environment_prepared(
+                "cd34d13b2980d0a9d438f754b2e94f85443076d0ebe1b0db09a0439f35feca5e"))
+
+    def test_get_all_environments_with_requirement(self):
+        with unittest.mock.patch(
+                'avocado.core.dependencies.requirements.cache.backends.sqlite.CACHE_DATABASE_PATH',
+                os.path.join(self.tmpdir.name,
+                             'requirements.sqlite')):
+            for entry in ENTRIES:
+                cache.set_requirement(*entry)
+            all_requirements = cache.get_all_environments_with_requirement(
+                'podman', 'package', 'hello')
+            expected_data = {
+                'ad34d13b2980d0a9d438f754b2e94f85443076d0ebe1b0db09a0439f35feca5e':
+                [('package', 'hello')],
+                'cd34d13b2980d0a9d438f754b2e94f85443076d0ebe1b0db09a0439f35feca5e':
+                [('core', 'avocado'),
+                 ('package', 'bash'),
+                 ('package', 'hello')]}
+            self.assertEqual(all_requirements, expected_data)
