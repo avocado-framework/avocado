3cfbbb85bd028c71a99a65952f634d7683f10903
Jan Richter <jarichte@redhat.com>
2022-06-17 11:27:52 +0200

    Requirements cache database updates
    This adds few methods for manipulating with requirements cache. This
    will be useful for spawners which will use requirements cache.
    
    Signed-off-by: Jan Richter <jarichte@redhat.com>

--- avocado/core/dependencies/requirements/cache/__init__.py
+++ avocado/core/dependencies/requirements/cache/__init__.py
@@ -1,3 +1,6 @@
 # The sqlite based backend is the only implementation
 from avocado.core.dependencies.requirements.cache.backends.sqlite import (
-    get_requirement, set_requirement)
+    delete_environment, delete_requirement,
+    get_all_environments_with_requirement, is_environment_prepared,
+    is_requirement_in_cache, set_requirement, update_environment,
+    update_requirement_status)
