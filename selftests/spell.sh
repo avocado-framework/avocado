edb06473fd4c12b67bd03c607107a2d688369609
Cleber Rosa <crosa@redhat.com>
2022-07-07 19:32:41 -0400

    selftests/spell.sh: support both pylint-3 and pylint
    This is already done in selftests/lint.sh, and allows the test to run
    properly on systems that the pylint script named with the Python
    version suffix only.
    
    Signed-off-by: Cleber Rosa <crosa@redhat.com>

--- selftests/spell.sh
+++ selftests/spell.sh
@@ -1,4 +1,6 @@
 #!/bin/sh -e
 echo "** Running spell check..."
 
-pylint -j 1 --errors-only --disable=all --enable=spelling --spelling-dict=en_US --spelling-private-dict-file=spell.ignore *
+PYLINT=$(which pylint-3 2>/dev/null || which pylint)
+
+${PYLINT} -j 1 --errors-only --disable=all --enable=spelling --spelling-dict=en_US --spelling-private-dict-file=spell.ignore *
