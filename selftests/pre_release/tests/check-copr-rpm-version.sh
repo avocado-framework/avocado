73d6dabc1729c9a8860cfcea7e288b1a607edc00
Cleber Rosa <crosa@redhat.com>
2022-07-11 12:14:55 -0400

    COPR version check: allow the package NVR to be specified
    This is useful for debugging and to check previous builds.
    
    Signed-off-by: Cleber Rosa <crosa@redhat.com>

--- selftests/pre_release/tests/check-copr-rpm-version.sh
+++ selftests/pre_release/tests/check-copr-rpm-version.sh
@@ -1,5 +1,8 @@
 #!/bin/sh -e
 
+# This script accepts a package NVR as its first argument.  If not given,
+# it will default to a NVR based on the info of the repository given
+
 # Put here the name of your GIT remote that tracks the official
 # repo, that is, https://github.com/avocado-framework/avocado
 # (or the git:// url version of the same repo)
@@ -14,7 +17,8 @@ SHORT_COMMIT=$(git rev-parse --short=9 $ORIGIN/master)
 RPM_RELEASE_NUMBER=$(grep -E '^Release:\s([0-9]+)' python-avocado.spec | sed -E 's/Release:\s([0-9]+).*/\1/')
 DISTRO_VERSION=36
 
-RPM_NVR="python3-avocado-${VERSION}-${RPM_RELEASE_NUMBER}.${COMMIT_DATE}git${SHORT_COMMIT}.fc${DISTRO_VERSION}"
+DEFAULT_RPM_NVR="python3-avocado-${VERSION}-${RPM_RELEASE_NUMBER}.${COMMIT_DATE}git${SHORT_COMMIT}.fc${DISTRO_VERSION}"
+RPM_NVR="${1:-$DEFAULT_RPM_NVR}"
 
 PODMAN=$(which podman 2>/dev/null || which docker)
 PODMAN_IMAGE=quay.io/avocado-framework/check-copr-rpm-version
