2eb51609f6584738070bd0d65d0f69af61130b88
Jan Richter <jarichte@redhat.com>
2022-11-04 10:59:41 +0100

    Add python 3.11 support
    Reference: #5503
    Signed-off-by: Jan Richter <jarichte@redhat.com>

--- setup.py
+++ setup.py
@@ -336,6 +336,7 @@ if __name__ == "__main__":
             "Programming Language :: Python :: 3.8",
             "Programming Language :: Python :: 3.9",
             "Programming Language :: Python :: 3.10",
+            "Programming Language :: Python :: 3.11",
         ],
         packages=find_packages(exclude=("selftests*",)),
         include_package_data=True,
