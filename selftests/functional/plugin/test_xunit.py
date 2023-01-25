3a8476bd094b89f2b56405b8a09bd34ec625fc14
Jan Richter <jarichte@redhat.com>
2023-01-25 18:06:41 +0100

    Add variant information to the XUnit test case name
    Reference: #5589
    Signed-off-by: Jan Richter <jarichte@redhat.com>

--- selftests/functional/plugin/test_xunit.py
+++ selftests/functional/plugin/test_xunit.py
@@ -80,3 +80,30 @@ class JsonResultTest(TestCaseTmpDir):
         for child in els[0].childNodes:
             if child.nodeType is child.CDATA_SECTION_NODE:
                 self.assertIn("Traceback (most recent call last):", child.nodeValue)
+
+    def test_variant(self):
+        cmd_line = (
+            f"{AVOCADO} run examples/tests/passtest.py "
+            "--mux-yaml examples/yaml_to_mux/simple_vars.yaml "
+            f"--job-results-dir {self.tmpdir.name} --disable-sysinfo "
+            "--max-parallel-tasks=1"
+        )
+        process.run(cmd_line, ignore_status=True)
+        xunit_path = path.join(self.tmpdir.name, "latest", "results.xml")
+
+        with open(xunit_path, "rb") as fp:
+            xml = fp.read()
+        try:
+            dom = minidom.parseString(xml)
+        except Exception as details:
+            raise ValueError(f"Error parsing XML: '{details}'.\nXML Contents:\n{xml}")
+        self.assertTrue(dom)
+        els = dom.getElementsByTagName("testcase")
+        self.assertEqual(
+            els[0].attributes["name"].value,
+            "examples/tests/passtest.py:PassTest.test;run-first-febe",
+        )
+        self.assertEqual(
+            els[1].attributes["name"].value,
+            "examples/tests/passtest.py:PassTest.test;run-second-bafe",
+        )
