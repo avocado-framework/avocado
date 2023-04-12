3d4cd60a58e0d8ed2717b3c5876075e8eb895a26
Jan Richter <jarichte@redhat.com>
2023-04-12 10:49:18 +0200

    BUILTIN_STREAMS description
    We have predefined streams which can be used by `--show` option to show
    user the predefined outputs. Let's add a dictionary which will translate
    these options to real avocado logging streams. This will make much
    easier manipulation with those streams and options.
    
    Signed-off-by: Jan Richter <jarichte@redhat.com>

--- avocado/core/streams.py
+++ avocado/core/streams.py
@@ -1,9 +1,15 @@
 #: Builtin special keywords to enable set of logging streams
 BUILTIN_STREAMS = {
+    "app": "avocado.app",
+    "test": "avocado.test",
+    "job": "avocado.job",
+    "early": "avocado",
+}
+
+BUILTIN_STREAMS_DESCRIPTION = {
     "app": "application output",
     "test": "test output",
     "job": "job output",
-    "debug": "tracebacks and other debugging info",
     "early": ("early logging of other streams, including test " "(very verbose)"),
 }
 
