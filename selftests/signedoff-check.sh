ead9f6b9a5e4b1a71b7852400dccd03580b0f3a8
Yihuang Yu <yihyu@redhat.com>
2023-04-18 15:05:48 +0800

    Test signedoff-check

--- selftests/signedoff-check.sh
+++ selftests/signedoff-check.sh
@@ -3,6 +3,8 @@
 echo "** Running signedoff-check..."
 
 AUTHOR="$(git log --no-merges -1 --pretty='format:%aN <%aE>')"
+HEADER=$(git log --no-merges -1 --format='%s')
+echo "The commit header is: '${HEADER}"
 git log --no-merges -1 --pretty=format:%B | grep -i "Signed-off-by: $AUTHOR"
 if [ $? != 0 ]; then
     echo "The commit message does not contain author's signature (Signed-off-by: $AUTHOR)"
