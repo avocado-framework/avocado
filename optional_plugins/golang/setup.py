02108c24fdabeb5383cdb4aea49c0b1c35858d44
Cleber Rosa <crosa@redhat.com>
2023-05-31 16:45:54 -0400

    Golang plugin: move documentation to the plugin itself
    The documentation for the Golang (and other) plugins is currently
    hosted in the main Avocado tree.  It's certainly not the best location
    for many reasons:
    
     * The documentation is not available the Python package (such as when
       browsing pypi)
     * The documentation is not available when browsing the sources on Git
       forges (such as GitHub).
     * Developers hacking the code won't easily find the documentation.
    
    This moves the documentation to the plugin directory itself, while
    still keeping it in the "Plugins" documentation section.
    
    Signed-off-by: Cleber Rosa <crosa@redhat.com>

--- optional_plugins/golang/setup.py
+++ optional_plugins/golang/setup.py
@@ -13,6 +13,8 @@
 # Copyright: Red Hat Inc. 2017
 # Author: Amador Pahim <apahim@redhat.com>
 
+import os
+
 from setuptools import setup
 
 # Handle systems with setuptools < 40
@@ -23,12 +25,23 @@ except ImportError:
 else:
     packages = find_namespace_packages(include=["avocado_golang"])
 
-VERSION = open("VERSION", "r", encoding="utf-8").read().strip()
+BASE_PATH = os.path.dirname(__file__)
+with open(os.path.join(BASE_PATH, "VERSION"), "r", encoding="utf-8") as version_file:
+    VERSION = version_file.read().strip()
+
+
+def get_long_description():
+    with open(os.path.join(BASE_PATH, "README.rst"), "rt", encoding="utf-8") as readme:
+        readme_contents = readme.read()
+    return readme_contents
+
 
 setup(
     name="avocado-framework-plugin-golang",
-    description="Avocado Plugin for Execution of Golang tests",
     version=VERSION,
+    description="Avocado Plugin for Execution of Golang tests",
+    long_description=get_long_description(),
+    long_description_content_type="text/x-rst",
     author="Avocado Developers",
     author_email="avocado-devel@redhat.com",
     url="http://avocado-framework.github.io/",
