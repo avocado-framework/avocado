6ad3c77b26ef72b9ce5cb9ef7a166fb49903d7c3
Cleber Rosa <crosa@redhat.com>
2023-07-10 16:25:28 -0400

    avocado/core/utils/loader.py: remove unnecessary TestError
    Before nrunner, all test conditions (including errors) were
    "communicated" by means of specialized class inheriting from
    avocado.core.test.Test.
    
    With nrunner, the conditions are communicated using messages, and that
    is true for errors.
    
    The "avocado-instrumented" runner already protects against exceptions
    while trying to load/import test modules, so this extra handling is
    completely unnecessary.
    
    Signed-off-by: Cleber Rosa <crosa@redhat.com>

--- avocado/core/utils/loader.py
+++ avocado/core/utils/loader.py
@@ -4,21 +4,6 @@ import os
 import sys
 
 from avocado.core import test
-from avocado.utils import stacktrace
-
-
-class TestError(test.Test):
-    """
-    Generic test error.
-    """
-
-    def __init__(self, *args, **kwargs):
-        exception = kwargs.pop("exception")
-        test.Test.__init__(self, *args, **kwargs)
-        self.exception = exception
-
-    def test(self):
-        self.error(self.exception)
 
 
 def load_test(test_factory):
@@ -43,13 +28,6 @@ def load_test(test_factory):
     try:
         sys.path.insert(0, test_module_dir)
         test_module = importlib.import_module(module_name)
-    except:  # pylint: disable=W0702
-        # On load_module exception we fake the test class and pass
-        # the exc_info as parameter to be logged.
-        test_parameters["methodName"] = "test"
-        exception = stacktrace.prepare_exc_info(sys.exc_info())
-        test_parameters["exception"] = exception
-        return TestError(**test_parameters)
     finally:
         if test_module_dir in sys.path:
             sys.path.remove(test_module_dir)
