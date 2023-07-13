4e6ebfb590ce5285f19c7cc322078c27db017510
chanxguqing <changxuqing@uniontech.com>
2023-07-13 17:36:20 +0800

    In the above code, we use the super() function to call the init method of the parent class, which ensures the consistency of the code when inheriting.
    Signed-off-by: chanxguqing <changxuqing@uniontech.com>

--- avocado/utils/pmem.py
+++ avocado/utils/pmem.py
@@ -28,9 +28,8 @@ class PMemException(Exception):
     """
 
     def __init__(self, additional_text=None):  # pylint: disable=W0231
-        super().__init__()
+        super().__init__()  # add
         self.additional_text = additional_text
-
 
     def __str__(self):
         return f"Command failed.\ninfo: {self.additional_text}"
