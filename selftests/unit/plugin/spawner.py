f3b76127f577b6944e78aed696533785bdb8f39a
Cleber Rosa <crosa@redhat.com>
2023-10-19 18:27:17 -0400

    Process Spawner: introduce handle class
    Some operations to manage the lifecycle of a process running a task
    depends on the process itself, and others on the asyncio Task waiting
    for the process to finish.
    
    This introduces a class that holds together the process itself and the
    asyncio Task that waits for the process to finish in a more convenient
    locality.
    
    Signed-off-by: Cleber Rosa <crosa@redhat.com>

--- selftests/unit/plugin/spawner.py
+++ selftests/unit/plugin/spawner.py
@@ -1,10 +1,11 @@
+import asyncio
 import unittest
 
 from avocado.core.nrunner.runnable import Runnable
 from avocado.core.nrunner.task import Task
 from avocado.core.spawners.mock import MockRandomAliveSpawner, MockSpawner
 from avocado.core.task.runtime import RuntimeTask
-from avocado.plugins.spawners.process import ProcessSpawner
+from avocado.plugins.spawners.process import ProcessSpawner, ProcessSpawnerHandle
 
 
 class Process(unittest.TestCase):
@@ -21,6 +22,43 @@ class Process(unittest.TestCase):
     def test_never_spawned(self):
         self.assertFalse(self.spawner.is_task_alive(self.runtime_task))
         self.assertFalse(self.spawner.is_task_alive(self.runtime_task))
+
+
+class MockProcessFinishQuickly:
+    async def wait(self):
+        return 0
+
+
+class MockProcessNeverFinish:
+    async def wait(self):
+        await asyncio.sleep(float("inf"))
+
+
+class ProcessHandle(unittest.TestCase):
+    def test_finishes(self):
+        handle = ProcessSpawnerHandle(MockProcessFinishQuickly())
+
+        async def await_wait_task():
+            _ = handle.wait_task
+
+        loop = asyncio.get_event_loop()
+        loop.run_until_complete(await_wait_task())
+        self.assertIsInstance(handle.wait_task, asyncio.Task)
+        self.assertTrue(handle.wait_task.done())
+        self.assertEqual(handle.wait_task.result(), 0)
+
+    def test_never_finishes(self):
+        handle = ProcessSpawnerHandle(MockProcessNeverFinish())
+
+        async def await_wait_task():
+            _ = handle.wait_task
+
+        loop = asyncio.get_event_loop()
+        loop.run_until_complete(asyncio.wait_for(await_wait_task(), 2.0))
+        self.assertIsInstance(handle.wait_task, asyncio.Task)
+        self.assertFalse(handle.wait_task.done())
+        with self.assertRaises(asyncio.InvalidStateError):
+            handle.wait_task.result()
 
 
 class Mock(Process):
