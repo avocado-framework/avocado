4a109a3698a7ffa896e2253780b9cf0743779108
Vaishnavi Bhat <vaishnavi@linux.vnet.ibm.com>
2023-10-20 13:13:30 +0530

    Adding check if the test interface tested is a bonding slave
    The patch is to test if the interface is a slave interface
    or a regular one. when mac address is passed as test param, return
    only for a regular interface or a bond master interface.
    
    Signed-off-by: Vaishnavi Bhat <vaishnavi@linux.vnet.ibm.com>

--- avocado/utils/network/hosts.py
+++ avocado/utils/network/hosts.py
@@ -74,8 +74,9 @@ class Host:
     def get_interface_by_hwaddr(self, mac):
         """Return an interface that has a specific mac."""
         for interface in self.interfaces:
-            if mac in interface.get_hwaddr():
-                return interface
+            if not interface.is_bond_slave():
+                if mac in interface.get_hwaddr():
+                    return interface
 
     def get_all_hwaddr(self):
         """Get a list of all mac address in the host
