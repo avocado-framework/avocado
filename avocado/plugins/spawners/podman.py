aef3e8e466647f3799fd346bb87a7af6e96043fb
Cleber Rosa <crosa@redhat.com>
2024-03-11 13:44:13 -0400

    Podman spawner: tag images created by the spawner due to requirements
    The podman spawner will create images with requirements so that they
    can be reused by other tests.  But those images won't have any
    meaningful tags associated with them.  That is, users doing a "podman
    images" command will only see "<UNKNOWN>".
    
    With this change, a somewhat meaningful description is attempted to be
    used as tag.
    
    For instance, when running:
    
     $ avocado run --spawner=podman \
       -- examples/tests/passtest_with_dependency.py
    
    Which has:
    
       :avocado: dependency={"type": "package", "name": "hello"}
    
    As a docstring directive, under a Fedora 38 machine, the
    image will be tagged:
    
       localhost/avocado_generated_fedora_38_package_hello
    
    The prefix is configurable and should also allow users to filter/clean
    images created/cached by Avocado.
    
    Signed-off-by: Cleber Rosa <crosa@redhat.com>

--- avocado/plugins/spawners/podman.py
+++ avocado/plugins/spawners/podman.py
@@ -65,6 +65,18 @@ class PodmanSpawnerInit(Init):
             section=section, key="avocado_spawner_egg", help_msg=help_msg, default=None
         )
 
+        help_msg = (
+            "Prefix to use when tagging images that are created by the "
+            "Podman spawner."
+        )
+
+        settings.register_option(
+            section=section,
+            key="image_tag_prefix",
+            help_msg=help_msg,
+            default="avocado_generated",
+        )
+
 
 class PodmanCLI(CLI):
 
@@ -411,6 +423,22 @@ class PodmanSpawner(DeploymentSpawner, SpawnerMixin):
             return False
         return True
 
+    async def _create_and_tag_image(self, runtime_task):
+        _, stdout, _ = await self.podman.execute(
+            "commit", "-q", runtime_task.spawner_handle
+        )
+        image_id = stdout.decode().strip()
+        tag_prefix = self.config.get("spawner.podman.image_tag_prefix")
+        base_image = self.config.get("spawner.podman.image")
+        kind = runtime_task.task.runnable.kind
+        name = runtime_task.task.runnable.kwargs.get("name")
+        tag = f"{tag_prefix}_{base_image}_{kind}_{name}".replace(":", "_")
+        try:
+            _, _, _ = await self.podman.execute("tag", image_id, tag)
+        except PodmanException as ex:
+            LOG.warning("Could not tag image %s with %s: %s", image_id, tag, ex)
+        return image_id
+
     async def update_requirement_cache(
         self, runtime_task, result
     ):  # pylint: disable=W0221
@@ -418,10 +446,7 @@ class PodmanSpawner(DeploymentSpawner, SpawnerMixin):
         if result in STATUSES_NOT_OK:
             cache.delete_environment(self.environment, environment_id)
             return
-        _, stdout, _ = await self.podman.execute(
-            "commit", "-q", runtime_task.spawner_handle
-        )
-        image_id = stdout.decode().strip()
+        image_id = await self._create_and_tag_image(runtime_task)
         cache.update_environment(self.environment, environment_id, image_id)
         cache.update_requirement_status(
             self.environment,
