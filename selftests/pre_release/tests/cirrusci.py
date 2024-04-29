7d54877168b06d7d17803b106f7ed56c5f67230b
Cleber Rosa <crosa@redhat.com>
2024-04-29 11:51:34 -0400

    Cirrus CI: use URIs specific to 92lts
    Without those specifiers, tests will fail and badges will present a
    fail state because Cirrus is no longer used on the latest branches.
    
    Signed-off-by: Cleber Rosa <crosa@redhat.com>

--- selftests/pre_release/tests/cirrusci.py
+++ selftests/pre_release/tests/cirrusci.py
@@ -7,7 +7,7 @@ from avocado.utils import download
 class CirrusCI(unittest.TestCase):
 
     def test(self):
-        url = 'https://api.cirrus-ci.com/github/avocado-framework/avocado.json'
+        url = 'https://api.cirrus-ci.com/github/avocado-framework/avocado.json?branch=92lts'
         http_response = download.url_open(url)
         self.assertEqual(http_response.code, 200)
         content = http_response.read()
