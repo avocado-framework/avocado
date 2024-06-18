09420d551d80835c8a93ae793df16bd4aee5bd7c
Yihuang Yu <yihyu@redhat.com>
2024-06-18 15:48:12 +0800

    chore: Migrate settings of linter and formatter to pyproject.toml
    pyproject.toml can be an entry to maintain configurations of linter and
    formatter, which easier to manage their configurations.
    
    Signed-off-by: Yihuang Yu <yihyu@redhat.com>

--- selftests/isort.sh
+++ selftests/isort.sh
@@ -1,3 +1,3 @@
 #!/bin/sh -e
 
-isort --check-only --profile black .
+isort --check-only .
--- selftests/spell.sh
+++ selftests/spell.sh
@@ -3,4 +3,4 @@ echo "** Running spell check..."
 
 PYLINT=$(which pylint-3 2>/dev/null || which pylint)
 
-${PYLINT} -j 1 --errors-only --disable=all --enable=spelling --spelling-dict=en_US --spelling-private-dict-file=spell.ignore *
+${PYLINT} -j 1 --errors-only --disable=all --enable=spelling *
