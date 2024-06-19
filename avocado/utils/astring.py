679d6225864222bd951e1325fce5e2786567892f
changxuqing <changxuqing@uniontech.com>
2024-06-19 16:14:02 +0800

    The operations of converting characters to ASCII values and generating a list of bits can be combined into one loop, reducing the number of passes.
    Signed-off-by: changxuqing <changxuqing@uniontech.com>

--- avocado/utils/astring.py
+++ avocado/utils/astring.py
@@ -66,16 +66,13 @@ def string_to_bitlist(data):
 
     :param data: String to be transformed
     """
-    data = [ord(c) for c in data]
+    ord_ = ord
     result = []
+    append = result.append
     for ch in data:
-        i = 7
-        while i >= 0:
-            if ch & (1 << i) != 0:
-                result.append(1)
-            else:
-                result.append(0)
-            i -= 1
+        ascii_value = ord_(ch)
+        for i in range(7, -1, -1):
+            append((ascii_value >> i) & 1)
     return result
 
 
