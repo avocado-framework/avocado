7b44b3b075aff7ce43304cb18858b9bf78c75dde
Jan Richter <jarichte@redhat.com>
2024-06-26 10:58:19 +0200

    pylint-3 removal
    Avocado is using pylint-3 instead of default pylint. This is a leftover
    from the time when avocado supported python2. Unfortunately, the
    pylint-3 has slightly different behaviour than pylint, therefore we need
    to update it to have more accurate testing.
    
    Signed-off-by: Jan Richter <jarichte@redhat.com>

--- selftests/spell.sh
+++ selftests/spell.sh
@@ -1,6 +1,6 @@
 #!/bin/sh -e
 echo "** Running spell check..."
 
-PYLINT=$(which pylint-3 2>/dev/null || which pylint)
+PYLINT=$(which pylint 2>/dev/null || python3 -m pylint)
 
 ${PYLINT} -j 1 --errors-only --disable=all --enable=spelling --spelling-dict=en_US --spelling-private-dict-file=spell.ignore *
