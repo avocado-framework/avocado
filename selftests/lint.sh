c430c2f2bb96391d738fc607c891afc597ed96bb
Jan Richter <jarichte@redhat.com>
2024-06-26 10:58:19 +0200

    pylint-3 removal
    Avocado is using pylint-3 instead of default pylint. This is a leftover
    from the time when avocado supported python2. Unfortunately, the
    pylint-3 has slightly different behaviour than pylint, therefore we need
    to update it to have more accurate testing. Lets use python3 -m pylint
    
    Signed-off-by: Jan Richter <jarichte@redhat.com>

--- selftests/lint.sh
+++ selftests/lint.sh
@@ -1,9 +1,7 @@
 #!/bin/sh -e
 echo "** Running lint..."
 
-PYLINT=$(which pylint-3 2>/dev/null || which pylint)
-
 # Those are files from our main packages, we should follow the .pylintrc file with all
 # enabled by default. Some are disabled, we are working to reduce this list.
 FILES=$(git ls-files '*.py')
-${PYLINT} ${PYLINT_OPTIONS} ${FILES}
+python3 -m pylint ${PYLINT_OPTIONS} ${FILES}
