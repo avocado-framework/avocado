avocado-framework/avocado

commit df6b1daf94a6f9e0f21ec55c5f1292b66eb7450b

cannot import name 'packaging' from 'pkg_resources' fix
In the setuptools>=70 the `pkg_resources` is deprecated and some of its
features might cause troubles. These changes have impact on avocado CI
with python3.12. This commit uses directly the `packaging` package
instead of `pkg_resources` which solves the CI issues.

Reference:
https://setuptools.pypa.io/en/stable/pkg_resources.html#package-discovery-and-resource-access-using-pkg-resources
Signed-off-by: Jan Richter <jarichte@redhat.com>

avocado/utils/kernel.py
@@ -23,7 +23,7 @@ import os
 import shutil
 import tempfile
 
-from pkg_resources import packaging
+from packaging.version import parse
 
 from avocado.utils import archive, asset, build, distro, process
 
@@ -207,6 +207,6 @@ def check_version(version):
     :type version: string
     :param version: version to be compared with current kernel version
     """
-    os_version = packaging.version.parse(os.uname()[2])
-    version = packaging.version.parse(version)
+    os_version = parse(os.uname()[2])
+    version = parse(version)
     assert os_version > version, "Old kernel"

commit 58738b61a8bd6af542ff82a2ccccb49551c48c1b

Backwards compatibility for kernel utils module
This is a fix of kerne utils compatibility issue with setuptools=>70
which has been introduced in df6b1da.

Reference: #5988
Signed-off-by: Jan Richter <jarichte@redhat.com>

avocado/utils/kernel.py
@@ -23,7 +23,10 @@ import os
 import shutil
 import tempfile
 
-from packaging.version import parse
+try:
+    import packaging
+except ImportError:
+    from pkg_resources import packaging
 
 from avocado.utils import archive, asset, build, distro, process
 
@@ -207,6 +210,6 @@ def check_version(version):
     :type version: string
     :param version: version to be compared with current kernel version
     """
-    os_version = parse(os.uname()[2])
-    version = parse(version)
+    os_version = packaging.version.parse(os.uname()[2])
+    version = packaging.version.parse(version)
     assert os_version > version, "Old kernel"