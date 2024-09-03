3ed11c1477a4efd3004b615bbfe42fcceeefc115
Sebastian Mitterle <smitterl@redhat.com>
2024-09-03 16:50:19 +0200

    Add exception handling for OSError that my be thrown by os.fsync
    We've seen instances where "Bad file descriptor" OSError was raised.
    This looks similar to 52d69bed34982ba36b535c70293ea686fedb1898.
    Handle in the same way by ignoring the error and continuing execution.
    
    Signed-off-by: Sebastian Mitterle <smitterl@redhat.com>

--- avocado/utils/process.py
+++ avocado/utils/process.py
@@ -522,7 +522,7 @@ class FDDrainer:
                         try:
                             fileno = stream.fileno()
                             os.fsync(fileno)
-                        except UnsupportedOperation:
+                        except UnsupportedOperation, OSError:
                             pass
                 if hasattr(handler, "close"):
                     handler.close()
