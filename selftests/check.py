41edcced833141af98887959c727090a67545860
Cleber Rosa <crosa@redhat.com>
2024-09-12 09:44:41 -0400

    Static checks: use lint check from avocado-static-checks
    This switches to the script provided by the avocado-static-checks
    repo, while using the local pylintrc configuration (set through the
    use of "avocado-static-checks.conf".
    
    In the near future, at least the "avocado/utils" directory needs to
    made compliant with the standard pylint configuration provided by
    avocado-static-checks, so that code can be easily moved to autils.
    
    Fixes: https://github.com/avocado-framework/avocado/issues/5990
    Signed-off-by: Cleber Rosa <crosa@redhat.com>

--- selftests/check.py
+++ selftests/check.py
@@ -601,6 +601,7 @@ def create_suites(args):  # pylint: disable=W0621
             "static-checks/check-import-order"
         )
         config_check_static["resolver.references"].append("static-checks/check-style")
+        config_check_static["resolver.references"].append("static-checks/check-lint")
         suites.append(TestSuite.from_config(config_check_static, "static-checks"))
 
     # ========================================================================
