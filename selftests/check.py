f452aa5d2bc249a79c2bf6dd63686cf0d3bc352f
Cleber Rosa <crosa@redhat.com>
2024-11-25 02:35:10 -0500

    avocado.utils.kernel: fix kernel version parsing
    This fixes two problems with the current version parse mechanism:
    
     1. Recent setuptools' version parse function, which has dropped
        "LegacyVersion" support, raises an InvalidVersion exception
        for common kernel version info as presented by the system and
        returned by os.uname()
    
     2. The import of the packaging module is broken on systems with no
        setuptools and only the "packaging" module.
    
    This introduces a simple but custom version parse that solves both and
    simplifies dependencies.
    
    Fixes: https://github.com/avocado-framework/avocado/issues/6058
    Signed-off-by: Cleber Rosa <crosa@redhat.com>

--- selftests/check.py
+++ selftests/check.py
@@ -27,7 +27,7 @@ TEST_SIZE = {
     "job-api-7": 1,
     "nrunner-interface": 70,
     "nrunner-requirement": 28,
-    "unit": 678,
+    "unit": 681,
     "jobs": 11,
     "functional-parallel": 314,
     "functional-serial": 7,
