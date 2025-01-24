a601e5ad12f8f355267d25a8dfe7c31196880134
Jan Richter <jarichte@redhat.com>
2025-01-24 16:57:58 +0100

    Seltests / Containers / CI: Bump Fedora to 40
    Because Fedora 38 is at the end of its life-cycle let's update the used
    Fedora images to 40 before the public package repositories will be
    removed.
    
    Signed-off-by: Jan Richter <jarichte@redhat.com>

--- selftests/functional/utils/podman.py
+++ selftests/functional/utils/podman.py
@@ -6,21 +6,21 @@ class PodmanTest(Test):
     def test_python_version(self):
         """
         :avocado: dependency={"type": "package", "name": "podman", "action": "check"}
-        :avocado: dependency={"type": "podman-image", "uri": "fedora:38"}
+        :avocado: dependency={"type": "podman-image", "uri": "fedora:40"}
         :avocado: tags=slow
         """
         podman = Podman()
-        result = podman.get_python_version("fedora:38")
-        self.assertEqual(result, (3, 11, "/usr/bin/python3"))
+        result = podman.get_python_version("fedora:40")
+        self.assertEqual(result, (3, 12, "/usr/bin/python3"))
 
     def test_container_info(self):
         """
         :avocado: dependency={"type": "package", "name": "podman", "action": "check"}
-        :avocado: dependency={"type": "podman-image", "uri": "fedora:38"}
+        :avocado: dependency={"type": "podman-image", "uri": "fedora:40"}
         :avocado: tags=slow
         """
         podman = Podman()
-        _, stdout, _ = podman.execute("create", "fedora:38", "/bin/bash")
+        _, stdout, _ = podman.execute("create", "fedora:40", "/bin/bash")
         container_id = stdout.decode().strip()
         result = podman.get_container_info(container_id)
         self.assertEqual(result["Id"], container_id)
@@ -35,21 +35,21 @@ class AsyncPodmanTest(Test):
     async def test_python_version(self):
         """
         :avocado: dependency={"type": "package", "name": "podman", "action": "check"}
-        :avocado: dependency={"type": "podman-image", "uri": "fedora:38"}
+        :avocado: dependency={"type": "podman-image", "uri": "fedora:40"}
         :avocado: tags=slow
         """
         podman = AsyncPodman()
-        result = await podman.get_python_version("fedora:38")
-        self.assertEqual(result, (3, 11, "/usr/bin/python3"))
+        result = await podman.get_python_version("fedora:40")
+        self.assertEqual(result, (3, 12, "/usr/bin/python3"))
 
     async def test_container_info(self):
         """
         :avocado: dependency={"type": "package", "name": "podman", "action": "check"}
-        :avocado: dependency={"type": "podman-image", "uri": "fedora:38"}
+        :avocado: dependency={"type": "podman-image", "uri": "fedora:40"}
         :avocado: tags=slow
         """
         podman = AsyncPodman()
-        _, stdout, _ = await podman.execute("create", "fedora:38", "/bin/bash")
+        _, stdout, _ = await podman.execute("create", "fedora:40", "/bin/bash")
         container_id = stdout.decode().strip()
         result = await podman.get_container_info(container_id)
         self.assertEqual(result["Id"], container_id)
