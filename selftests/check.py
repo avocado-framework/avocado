dba6f365d79b879c88c35367fcc247a994bae08e
Cleber Rosa <crosa@redhat.com>
2025-08-25 18:56:38 -0400

    GDB utils: increase coverage by making example test functional
    There's a functional GDB test that is being shipped only as an
    example test.  We can make it run during the selftests to
    increase the module coverage (and avoid regression as could be
    observed in the last few commits).
    
    Signed-off-by: Cleber Rosa <crosa@redhat.com>

--- selftests/check.py
+++ selftests/check.py
@@ -29,7 +29,7 @@ TEST_SIZE = {
     "nrunner-requirement": 28,
     "unit": 739,
     "jobs": 11,
-    "functional-parallel": 318,
+    "functional-parallel": 335,
     "functional-serial": 7,
     "optional-plugins": 0,
     "optional-plugins-golang": 2,
