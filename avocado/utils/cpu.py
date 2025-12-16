avocado-framework/avocado

commit 7cc27dc2424ba23f5c9330276da1df70f323f56e

Add/extend cpu_model numbers for the Zen arch
The patch has the following changes incorporated:-

  *Add the cpu_model numbers range (0x60, 0x6F) for detecting Zen5
  CPUs in the get_x86_amd_zen().

  *Extend the cpu_model dictionary to have Zen6 numbers in the
  get_x86_amd_zen().

  *Add function description for the function get_x86_amd_zen().

Referred the cpu_model numbers for Zen5 from [1] and for Zen6 from [2].

[1] https://github.com/torvalds/linux/blob/master/arch/x86/kernel/cpu/amd.c#L515

[2] https://github.com/torvalds/linux/blob/master/arch/x86/kernel/cpu/amd.c#L518

Signed-off-by: Suneeth D <Suneeth.D@amd.com>

avocado/utils/cpu.py
@@ -302,6 +302,7 @@ def get_model():
 
 def get_x86_amd_zen(family=None, model=None):
     """
+    Get the AMD Zen architecture's version of the x86_AMD CPU
     :param family: AMD family
     :type family: int
     :param model: AMD model
@@ -317,7 +318,7 @@ def get_x86_amd_zen(family=None, model=None):
             2: [(0x30, 0x4F), (0x60, 0x7F), (0x90, 0x91), (0xA0, 0xAF)],
         },
         0x19: {3: [(0x00, 0x0F), (0x20, 0x5F)], 4: [(0x10, 0x1F), (0x60, 0xAF)]},
-        0x1A: {5: [(0x00, 0x0F), (0x20, 0x2F), (0x40, 0x4F), (0x70, 0x7F)]},
+        0x1A: {5: [(0x00, 0x0F), (0x20, 0x2F), (0x40, 0x4F), (0x60, 0x7F)], 6: [(0x50, 0x5f), (0x80, 0xAF), (0xc0, 0xcf)]},
     }
     if not family:
         family = get_family()

commit 6ad82b141bd91da4eaa5333e6516f87e1e45aa69

The black style fix
The commit 7cc27dc introduced style issue which needs to be fixed

Reference:7cc27dc
Signed-off-by: Jan Richter <jarichte@redhat.com>

avocado/utils/cpu.py
@@ -318,7 +318,10 @@ def get_x86_amd_zen(family=None, model=None):
             2: [(0x30, 0x4F), (0x60, 0x7F), (0x90, 0x91), (0xA0, 0xAF)],
         },
         0x19: {3: [(0x00, 0x0F), (0x20, 0x5F)], 4: [(0x10, 0x1F), (0x60, 0xAF)]},
-        0x1A: {5: [(0x00, 0x0F), (0x20, 0x2F), (0x40, 0x4F), (0x60, 0x7F)], 6: [(0x50, 0x5f), (0x80, 0xAF), (0xc0, 0xcf)]},
+        0x1A: {
+            5: [(0x00, 0x0F), (0x20, 0x2F), (0x40, 0x4F), (0x60, 0x7F)],
+            6: [(0x50, 0x5F), (0x80, 0xAF), (0xC0, 0xCF)],
+        },
     }
     if not family:
         family = get_family()