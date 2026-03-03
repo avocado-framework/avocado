12b4e28ebb4806e72a38a68d784bb41947894bbc
Maram Srimannarayana Murthy <msmurthy@linux.vnet.ibm.com>
2026-03-03 18:19:06 +0530

    Added example of return slots formats in doc string

--- avocado/utils/pci.py
+++ avocado/utils/pci.py
@@ -190,7 +190,8 @@ def get_slot_from_sysfs(full_pci_address):
     :param full_pci_address: Full PCI address including domain (0000:03:00.0)
 
     :return: Removed port related details using re, only returns till
-             physical slot of the adapter.
+             physical slot of the adapter
+             Examples: U78CD.001.FZHAK92-P2-C3, U50EE.001.WZS0011-P3-C20-R1.
     """
     if not os.path.isfile(f"/sys/bus/pci/devices/{full_pci_address}/devspec"):
         return None
